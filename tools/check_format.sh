9c968b0d52a3c54ee33c6324a72a006cdf9573c9
Michael Rebello <mrebello@lyft.com>
2019-06-07 14:11:32 -0700

    run the formatter
    Signed-off-by: Michael Rebello <mrebello@lyft.com>

--- tools/check_format.sh
+++ tools/check_format.sh
@@ -16,6 +16,6 @@ fi
 # TODO(mattklein123): We don't need envoy_package() in various files. Somehow fix in upstream
 #                     checker.
 envoy/tools/check_format.py \
-    --add-excluded-prefixes ./envoy/ ./envoy_build_config/extensions_build_config.bzl ./WORKSPACE ./examples/objective-c/ \
+    --add-excluded-prefixes ./envoy/ ./envoy_build_config/extensions_build_config.bzl ./WORKSPACE ./dist/Envoy.framework/ \
     --skip_envoy_build_rule_check "$ENVOY_FORMAT_ACTION"
 envoy/tools/format_python_tools.sh check
