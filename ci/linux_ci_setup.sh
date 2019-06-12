33794dccbe41db411b5a5739149c0c0ef8e23485
Jose Nino <jnino@lyft.com>
2019-06-12 12:34:58 -0700

    ci: setup android on azure pipelines
    Signed-off-by: Jose Nino <jnino@lyft.com>

---
+++ ci/linux_ci_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+# Setup basic requirements and install them.
+apt-get update
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y wget software-properties-common make cmake git \
+  unzip bc libtool ninja-build automake zip time golang gdb strace \
+  apt-transport-https
+# clang 8.
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+apt-add-repository "deb https://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main"
+apt-get update
+apt-get install -y clang-8 clang-format-8 clang-tidy-8 lld-8 libc++-8-dev libc++abi-8-dev
+# gcc-7
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt update
+apt install -y g++-7
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 1000
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 1000
+update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 1000
+update-alternatives --config gcc
+update-alternatives --config g++
+update-alternatives --config gcov
+# Bazel and related dependencies.
+apt-get install -y openjdk-8-jdk curl
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+apt-get update
+apt-get install -y bazel
+apt-get install -y aspell
+rm -rf /var/lib/apt/lists/*
+
+export PATH=/usr/lib/llvm-8/bin:$PATH
+export CC=clang
+export CXX=clang++
