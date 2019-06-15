13cff957037dd2f64ff244f82083e5979e3ee354
Jose Nino <jnino@lyft.com>
2019-06-15 07:32:34 -0700

    less deps
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -6,21 +6,20 @@ set -e
 sudo apt-get update
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y wget software-properties-common make cmake git \
-  unzip bc libtool ninja-build automake zip time golang \
+  unzip bc libtool ninja-build automake zip time \
   apt-transport-https
 
 # clang 8.
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb https://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main"
 sudo apt-get update
-sudo apt-get install -y clang-8 clang-format-8 clang-tidy-8 lld-8 libc++-8-dev libc++abi-8-dev
+sudo apt-get install -y clang-8 lld-8 libc++-8-dev libc++abi-8-dev
 ls -lh /usr/bin/clang*
 
-# Bazel and related dependencies.
-sudo apt-get install -y openjdk-8-jdk curl
+# Bazel.
+sudo apt-get install -y curl
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y bazel
-sudo apt-get install -y aspell
 sudo rm -rf /var/lib/apt/lists/*
