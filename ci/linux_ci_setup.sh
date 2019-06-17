e9176d74ea5253d42b3b6989602053e2157f007a
Jose Nino <jnino@lyft.com>
2019-06-17 13:09:12 -0700

    try bazel 0.26.1
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -21,15 +21,12 @@ sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-8 100
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-8 100
 
 # Bazel.
-sudo apt-get install -y curl
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y bazel
-sudo rm -rf /var/lib/apt/lists/*
+wget -c https://github.com/bazelbuild/bazel/releases/download/0.26.1/bazel-0.26.1-installer-linux-x86_64.sh
+chmod +x bazel-0.26.1-installer-linux-x86_64.sh
+./bazel-0.26.1-installer-linux-x86_64.sh
 
 # ndk 20
-wget -c https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
-unzip android-ndk-r20-linux-x86_64.zip
-sudo rm -rf /usr/local/lib/android/sdk/ndk-bundle
-sudo mv android-ndk-r20 /usr/local/lib/android/sdk/ndk-bundle
+# wget -c https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
+# unzip android-ndk-r20-linux-x86_64.zip
+# sudo rm -rf /usr/local/lib/android/sdk/ndk-bundle
+# sudo mv android-ndk-r20 /usr/local/lib/android/sdk/ndk-bundle
