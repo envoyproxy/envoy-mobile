a184c3c26124297a9e93438c0f0d5ca2f3c42be3
Alan Chiu <achiu@lyft.com>
2019-06-16 17:06:28 -0700

    oops
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- ci/kotlin_lint.sh
+++ ci/kotlin_lint.sh
@@ -2,6 +2,6 @@
 
 version="1.0.0-RC15"
 detekt_jar="detekt-cli-$version-all.jar"
-wget -q "https://github.com/arturbosch/detekt/releases/download/$version/$detekt_jar" detekt-cli.jar
+wget -q "https://github.com/arturbosch/detekt/releases/download/$version/$detekt_jar"
 
 java -jar ${detekt_jar} --build-upon-default-config -c .kotlinlint.yml -i examples/kotlin/hello_world
