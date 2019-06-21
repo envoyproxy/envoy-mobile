046827f2187e91c20e88a086fbd5c6e3b06f3fe0
Michael Rebello <mrebello@lyft.com>
2019-06-21 13:26:34 -0700

    envoyobjc
    Signed-off-by: Michael Rebello <mrebello@lyft.com>

---
+++ examples/objective-c/xcode_variant/EnvoyObjc/EnvoyObjc/AppDelegate.mm
@@ -0,0 +1,40 @@
+#import "AppDelegate.h"
+#import <Envoy/Envoy.h>
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate ()
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    UIViewController *controller = [ViewController new];
+    UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:controller];
+    self.window = [[UIWindow alloc] initWithFrame: [UIScreen mainScreen].bounds];
+    [self.window setRootViewController:navigation];
+    [self.window makeKeyAndVisible];
+
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
+    [NSThread detachNewThreadSelector:@selector(startEnvoy) toTarget:self withObject:nil];
+    NSLog(@"Finished launching!");
+    return YES;
+}
+
+- (void)startEnvoy {
+    NSString *configFile = [[NSBundle mainBundle] pathForResource:@"config" ofType:@"yaml"];
+    NSString *configYaml = [NSString stringWithContentsOfFile:configFile encoding:NSUTF8StringEncoding error:NULL];
+    NSLog(@"Loading config:\n%@", configYaml);
+
+    // Initialize the server's main context under a try/catch loop and simply return EXIT_FAILURE
+    // as needed. Whatever code in the initialization path that fails is expected to log an error
+    // message so the user can diagnose.
+    try {
+        run_envoy(configYaml.UTF8String);
+    } catch (NSException *e) {
+        NSLog(@"Error starting Envoy: %@", e);
+        exit(EXIT_FAILURE);
+    }
+}
+
+@end
