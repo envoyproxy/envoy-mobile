bee1a4e00b5fde445ac3051e2721d6207c7e6e8f
Alan Chiu <achiu@lyft.com>
2019-06-24 10:58:22 -0700

    hack a sticky service together
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- examples/kotlin/hello_world/MainActivity.kt
+++ examples/kotlin/hello_world/MainActivity.kt
@@ -1,15 +1,18 @@
 package io.envoyproxy.envoymobile.helloenvoykotlin
 
 import android.app.Activity
+import android.app.Service
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.os.IBinder
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 
+import android.util.Log
 import java.io.IOException
 import java.io.InputStream
 import java.net.HttpURLConnection
@@ -22,27 +25,10 @@ private const val REQUEST_HANDLER_THREAD_NAME = "hello_envoy_kt"
 private const val ENDPOINT = "http://0.0.0.0:9001/api.lyft.com/static/demo/hello_world.txt"
 private const val ENVOY_SERVER_HEADER = "server"
 
-class MainActivity : Activity() {
-  private lateinit var recyclerView: RecyclerView
+class PersistentService : Service() {
   private val thread = HandlerThread(REQUEST_HANDLER_THREAD_NAME)
-  private lateinit var envoy: Envoy
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-
-    Envoy.load(baseContext)
- 
-    // Create Envoy instance with config.
-    envoy = Envoy(baseContext, loadEnvoyConfig(baseContext, R.raw.config))
-
-    recyclerView = findViewById(R.id.recycler_view) as RecyclerView
-    recyclerView.layoutManager = LinearLayoutManager(this)
-
-    val adapter = ResponseRecyclerViewAdapter()
-    recyclerView.adapter = adapter
-    val dividerItemDecoration = DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
-    recyclerView.addItemDecoration(dividerItemDecoration)
+  override fun onCreate() {
     thread.start()
     val handler = Handler(thread.looper)
 
@@ -51,7 +37,7 @@ class MainActivity : Activity() {
       override fun run() {
         try {
           val response = makeRequest()
-          recyclerView.post({ adapter.add(response) })
+          MainActivity.callback(response)
         } catch (e: IOException) {
           Log.d("MainActivity", "exception making request.", e)
         }
@@ -62,29 +48,71 @@ class MainActivity : Activity() {
     }, TimeUnit.SECONDS.toMillis(1))
   }
 
-  override fun onDestroy() {
-    super.onDestroy()
-    thread.quit()
+  override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    return START_STICKY
   }
 
-  private fun makeRequest(): Response {
-    val url = URL(ENDPOINT)
-    // Open connection to the envoy thread listening locally on port 9001
-    val connection = url.openConnection() as HttpURLConnection
-    val status = connection.responseCode
-    if (status != 200) {
-      throw IOException("non 200 status: $status")
+  override fun onBind(intent: Intent): IBinder? {
+    return null
+  }
+
+  override fun onDestroy() {
+
+  }
+}
+
+private fun makeRequest(): Response {
+  val url = URL(ENDPOINT)
+  // Open connection to the envoy thread listening locally on port 9001
+  val connection = url.openConnection() as HttpURLConnection
+  val status = connection.responseCode
+  if (status != 200) {
+    throw IOException("non 200 status: $status")
+  }
+
+  val serverHeaderField = connection.headerFields[ENVOY_SERVER_HEADER]
+  val inputStream = connection.inputStream
+  val body = deserialize(inputStream)
+  inputStream.close()
+  return Response(body, serverHeaderField?.joinToString(separator = ", ") ?: "")
+}
+
+private fun deserialize(inputStream: InputStream): String {
+  return inputStream.bufferedReader().use { reader -> reader.readText() }
+}
+
+class MainActivity : Activity() {
+  private lateinit var recyclerView: RecyclerView
+  private lateinit var envoy: Envoy
+
+  companion object {
+    var callback: (Response) -> Unit = {}
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Intent(this, PersistentService::class.java).also { intent ->
+      startService(intent)
     }
 
-    val serverHeaderField = connection.headerFields[ENVOY_SERVER_HEADER]
-    val inputStream = connection.inputStream
-    val body = deserialize(inputStream)
-    inputStream.close()
-    return Response(body, serverHeaderField?.joinToString(separator = ", ") ?: "")
-  }
+    setContentView(R.layout.activity_main)
 
-  private fun deserialize(inputStream: InputStream): String {
-    return inputStream.bufferedReader().use { reader -> reader.readText() }
+    Envoy.load(baseContext)
+
+    // Create Envoy instance with config.
+    envoy = Envoy(baseContext, loadEnvoyConfig(baseContext, R.raw.config))
+
+    recyclerView = findViewById(R.id.recycler_view) as RecyclerView
+    recyclerView.layoutManager = LinearLayoutManager(this)
+
+    val adapter = ResponseRecyclerViewAdapter()
+    recyclerView.adapter = adapter
+    val dividerItemDecoration = DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
+    recyclerView.addItemDecoration(dividerItemDecoration)
+
+    callback = { response ->
+      recyclerView.post({ adapter.add(response) })
+    }
   }
 
   private fun loadEnvoyConfig(context: Context, configResourceId: Int): String {
