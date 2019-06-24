a2df0e13d66cb26a37b36d47c846533c9ba90511
Alan Chiu <achiu@lyft.com>
2019-06-24 11:06:49 -0700

    hack control
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- examples/kotlin/control/MainActivity.kt
+++ examples/kotlin/control/MainActivity.kt
@@ -1,10 +1,13 @@
 package io.envoyproxy.envoymobile.hellocontrolkotlin
 
 import android.app.Activity
+import android.app.Service
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.os.IBinder
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -19,21 +22,12 @@ import java.util.concurrent.TimeUnit
 private const val REQUEST_HANDLER_THREAD_NAME = "hello_envoy_kt"
 private const val ENDPOINT = "http://s3.amazonaws.com/api.lyft.com/static/demo/hello_world.txt"
 
-class MainActivity : Activity() {
-  private lateinit var recyclerView: RecyclerView
+private const val ENVOY_SERVER_HEADER = "server"
+
+class PersistentService : Service() {
   private val thread = HandlerThread(REQUEST_HANDLER_THREAD_NAME)
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-
-    recyclerView = findViewById(R.id.recycler_view) as RecyclerView
-    recyclerView.layoutManager = LinearLayoutManager(this)
-
-    val adapter = ResponseRecyclerViewAdapter()
-    recyclerView.adapter = adapter
-    val dividerItemDecoration = DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
-    recyclerView.addItemDecoration(dividerItemDecoration)
+  override fun onCreate() {
     thread.start()
     val handler = Handler(thread.looper)
 
@@ -42,7 +36,7 @@ class MainActivity : Activity() {
       override fun run() {
         try {
           val response = makeRequest()
-          recyclerView.post({ adapter.add(response) })
+          MainActivity.callback(response)
         } catch (e: IOException) {
           Log.d("MainActivity", "exception making request.", e)
         }
@@ -53,28 +47,66 @@ class MainActivity : Activity() {
     }, TimeUnit.SECONDS.toMillis(1))
   }
 
+  override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    return START_STICKY
+  }
+
+  override fun onBind(intent: Intent): IBinder? {
+    return null
+  }
+
   override fun onDestroy() {
-    super.onDestroy()
-    thread.quit()
-  }
 
-  private fun makeRequest(): Response {
-    val url = URL(ENDPOINT)
-    // Open connection to the envoy thread listening locally on port 9001
-    val connection = url.openConnection() as HttpURLConnection
-    val status = connection.responseCode
-    if (status != 200) {
-      throw IOException("non 200 status: $status")
-    }
-
-    val inputStream = connection.inputStream
-    val body = deserialize(inputStream)
-    inputStream.close()
-    return Response(body, "")
-  }
-
-  private fun deserialize(inputStream: InputStream): String {
-    return inputStream.bufferedReader().use { reader -> reader.readText() }
   }
 }
 
+private fun makeRequest(): Response {
+  val url = URL(ENDPOINT)
+  // Open connection to the envoy thread listening locally on port 9001
+  val connection = url.openConnection() as HttpURLConnection
+  val status = connection.responseCode
+  if (status != 200) {
+    throw IOException("non 200 status: $status")
+  }
+
+  val serverHeaderField = connection.headerFields[ENVOY_SERVER_HEADER]
+  val inputStream = connection.inputStream
+  val body = deserialize(inputStream)
+  inputStream.close()
+  return Response(body, serverHeaderField?.joinToString(separator = ", ")
+      ?: "")
+}
+
+private fun deserialize(inputStream: InputStream): String {
+  return inputStream.bufferedReader().use { reader -> reader.readText() }
+}
+
+
+class MainActivity : Activity() {
+  private lateinit var recyclerView: RecyclerView
+
+  companion object {
+    var callback: (Response) -> Unit = {}
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Intent(this, PersistentService::class.java).also { intent ->
+      startService(intent)
+    }
+
+    setContentView(R.layout.activity_main)
+
+    recyclerView = findViewById(R.id.recycler_view) as RecyclerView
+    recyclerView.layoutManager = LinearLayoutManager(this)
+
+    val adapter = ResponseRecyclerViewAdapter()
+    recyclerView.adapter = adapter
+    val dividerItemDecoration = DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
+    recyclerView.addItemDecoration(dividerItemDecoration)
+
+    callback = { response ->
+      recyclerView.post({ adapter.add(response) })
+    }
+  }
+}
