162faa96e37158c9fd96f6e181daf68310e658b8
Jose Nino <jnino@lyft.com>
2019-06-27 17:06:40 -0700

    logcat
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- examples/kotlin/shared/ResponseViewHolder.kt
+++ examples/kotlin/shared/ResponseViewHolder.kt
@@ -1,8 +1,10 @@
 package io.envoyproxy.envoymobile.shared
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+
 
 class ResponseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
   private val countTextView: TextView = itemView.findViewById(R.id.response_text_view_count) as TextView
@@ -15,6 +17,7 @@ class ResponseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         { success ->
           responseTextView.text = responseTextView.resources.getString(R.string.title_string, success.title)
           headerTextView.text = headerTextView.resources.getString(R.string.header_string, success.header)
+          Log.d("ResponseViewHolder", "successful response!")
         },
         { failure ->
           responseTextView.text = responseTextView.resources.getString(R.string.title_string, failure.message)
