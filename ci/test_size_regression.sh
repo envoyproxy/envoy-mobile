40b018bcbea87c6f390bed1e8c7c2c0b358deb92
Jose Nino <jnino@lyft.com>
2019-07-02 09:09:15 -0700

    linux stat
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- ci/test_size_regression.sh
+++ ci/test_size_regression.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Checks the absolute size, and the relative increase of a file.
-SIZE1=$(stat -f "%z" "$1")
-SIZE2=$(stat -f "%z" "$2")
+SIZE1=$(stat -c "%s" "$1")
+SIZE2=$(stat -c "%s" "$2")
 PERC=$(bc <<< "scale=2; ($SIZE2 - $SIZE1)/$SIZE1 * 100")
 echo "The new binary is $PERC % different in size compared to master"
 
