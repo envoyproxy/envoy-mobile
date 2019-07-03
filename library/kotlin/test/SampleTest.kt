58bc2ee24dd60b0d6e5ba1d1d56dfaf223954f2f
Alan Chiu <achiu@lyft.com>
2019-07-03 16:23:46 -0700

    sample tests
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/kotlin/test/SampleTest.kt
+++ library/kotlin/test/SampleTest.kt
@@ -1,8 +1,12 @@
-package io.envoyproxy.envoymobile
+package library.kotlin.test
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class SampleTest {
 
+  @Test
+  fun sampleTest() {
+    assertThat(true).isTrue()
+  }
 }
