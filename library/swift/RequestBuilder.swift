a7a32dce9ae0779832932415d33d0684d3c4e4aa
Michael Rebello <mrebello@lyft.com>
2019-07-11 14:34:00 -0700

    remove convenience init
    https://envoyproxy.slack.com/archives/CKQ2LK23G/p1562880255143900
    
    Signed-off-by: Michael Rebello <mrebello@lyft.com>

--- library/swift/RequestBuilder.swift
+++ library/swift/RequestBuilder.swift
@@ -36,20 +36,6 @@ public final class RequestBuilder: NSObject {
     self.method = method
   }
 
-  /// Convenience initializer accepting individual URL components.
-  public convenience init?(scheme: String, host: String, path: String, method: RequestMethod) {
-    var components = URLComponents()
-    components.scheme = scheme
-    components.host = host
-    components.path = path
-
-    guard let url = components.url else {
-      return nil
-    }
-
-    self.init(url: url, method: method)
-  }
-
   // MARK: - Builder functions
 
   @discardableResult
