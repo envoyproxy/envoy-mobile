1b888b939d00af0ec5dfb990c3e0b90470fcf165
Jose Nino <jnino@lyft.com>
2019-07-11 14:43:54 -0700

    checkpoint: use AsyncStream interface
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/http/async_stream_manager.cc
+++ library/common/http/async_stream_manager.cc
@@ -1,17 +1,17 @@
 #include "library/common/http/async_stream_manager.h"
 
-namespace EnvoyMobile {
+namespace Envoy {
 namespace Http {
 
-envoy_stream_t AsyncStreamManager::createStream(envoy_observer observer) {
-  stream_.emplace(current_stream_id_, AsyncStream(observer));
+envoy_stream_t MobileAsyncStreamManager::createStream(envoy_observer observer) {
+  stream_.emplace(current_stream_id_, MobileAsyncStream(observer));
   return current_stream_id_++;
 }
 
-std::shared_ptr<AsyncStream> AsyncStreamManager::getStream(envoy_stream_t stream_id) const {
+std::shared_ptr<MobileAsyncStream> MobileAsyncStreamManager::getStream(envoy_stream_t stream_id) const {
   const auto async_stream_it = stream_.find(stream_id);
-  return (async_stream_it != stream_.end())
-    ? std::make_shared<AsyncStream>(async_stream_it->second) : nullptr;
+  return (async_stream_it != stream_.end()) ? std::make_shared<MobileAsyncStream>(async_stream_it->second)
+                                            : nullptr;
 }
 
 } // namespace Http
--- library/common/http/async_stream_manager.h
+++ library/common/http/async_stream_manager.h
@@ -3,21 +3,21 @@
 #include <atomic>
 #include <unordered_map>
 
-#include "library/common/types.h"
 #include "library/common/http/async_stream.h"
+#include "library/common/types.h"
 
-namespace EnvoyMobile {
+namespace Envoy {
 namespace Http {
 
-class AsyncStreamManager {
+class MobileAsyncStreamManager {
 public:
-  AsyncStreamManager();
+  MobileAsyncStreamManager();
 
   envoy_stream_t createStream(envoy_observer);
-  std::shared_ptr<AsyncStream> getStream(envoy_stream_t stream_id) const;
+  std::shared_ptr<MobileAsyncStream> getStream(envoy_stream_t stream_id) const;
 
 private:
-  std::unordered_map<envoy_stream_t, AsyncStream> stream_;
+  std::unordered_map<envoy_stream_t, MobileAsyncStream> stream_;
   std::atomic<envoy_stream_t> current_stream_id_;
 };
 
