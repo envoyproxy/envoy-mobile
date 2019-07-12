4a372607a9810b286d850ac94d1cf455341e5471
Jose Nino <jnino@lyft.com>
2019-07-11 17:38:31 -0700

    checkpoint: plumbed AsyncClient all the way through
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/main_interface.cc
+++ library/common/main_interface.cc
@@ -16,10 +16,11 @@
 
 // NOLINT(namespace-envoy)
 
-static Envoy::Http::MobileAsyncStreamManager stream_manager_;
+static std::unique_ptr<Envoy::MainCommon> main_common_;
+static std::unique_ptr<Envoy::Http::MobileAsyncStreamManager> stream_manager_;
 
 envoy_stream start_stream(envoy_observer observer) {
-  return {ENVOY_SUCCESS, stream_manager_.createStream(observer)};
+  return {ENVOY_SUCCESS, stream_manager_->createStream(observer)};
 }
 
 // Note I did not deal with HeaderMap or Buffer (below) ownership yet. Meaning this functions still
@@ -29,7 +30,7 @@ envoy_stream start_stream(envoy_observer observer) {
 // Note as well that this functions did not take ownership of the passed in envoy_headers or
 // envoy_data. The caller still has ownership of them.
 envoy_status_t send_headers(envoy_stream_t stream_id, envoy_headers headers, bool end_stream) {
-  auto stream = stream_manager_.getStream(stream_id);
+  auto stream = stream_manager_->getStream(stream_id);
   if (stream) {
     const auto header_map = Envoy::Http::Utility::transformHeaders(headers);
     stream->sendHeaders(*header_map, end_stream);
@@ -40,7 +41,7 @@ envoy_status_t send_headers(envoy_stream_t stream_id, envoy_headers headers, boo
 }
 
 envoy_status_t send_data(envoy_stream_t stream_id, envoy_data data, bool end_stream) {
-  auto stream = stream_manager_.getStream(stream_id);
+  auto stream = stream_manager_->getStream(stream_id);
   if (stream) {
     const auto buffer = Envoy::Http::Utility::transformData(data);
     stream->sendData(*buffer, end_stream);
@@ -50,19 +51,19 @@ envoy_status_t send_data(envoy_stream_t stream_id, envoy_data data, bool end_str
   }
 }
 
-envoy_status_t send_metadata(envoy_stream_t stream_id, envoy_headers metadata, bool end_stream) {
-  auto stream = stream_manager_.getStream(stream_id);
-  if (stream) {
-    const auto metadata_map = Envoy::Http::Utility::transformHeaders(metadata);
-    stream->sendMetadata(*metadata_map, end_stream);
-    return ENVOY_SUCCESS;
-  } else {
-    return ENVOY_FAILURE;
-  }
-}
+// envoy_status_t send_metadata(envoy_stream_t stream_id, envoy_headers metadata, bool end_stream) {
+//   auto stream = stream_manager_->getStream(stream_id);
+//   if (stream) {
+//     const auto metadata_map = Envoy::Http::Utility::transformHeaders(metadata);
+//     stream->sendMetadata(*metadata_map, end_stream);
+//     return ENVOY_SUCCESS;
+//   } else {
+//     return ENVOY_FAILURE;
+//   }
+// }
 
 envoy_status_t send_trailers(envoy_stream_t stream_id, envoy_headers trailers) {
-  auto stream = stream_manager_.getStream(stream_id);
+  auto stream = stream_manager_->getStream(stream_id);
   if (stream) {
     const auto trailers_map = Envoy::Http::Utility::transformHeaders(trailers);
     stream->sendTrailers(*trailers_map);
@@ -72,18 +73,18 @@ envoy_status_t send_trailers(envoy_stream_t stream_id, envoy_headers trailers) {
   }
 }
 
-envoy_status_t locally_close_stream(envoy_stream_t stream_id) {
-  auto stream = stream_manager_.getStream(stream_id);
-  if (stream) {
-    stream->locally_close();
-    return ENVOY_SUCCESS;
-  } else {
-    return ENVOY_FAILURE;
-  }
-}
+// envoy_status_t locally_close_stream(envoy_stream_t stream_id) {
+//   auto stream = stream_manager_->getStream(stream_id);
+//   if (stream) {
+//     stream->locally_close();
+//     return ENVOY_SUCCESS;
+//   } else {
+//     return ENVOY_FAILURE;
+//   }
+// }
 
 envoy_status_t reset_stream(envoy_stream_t stream_id) {
-  auto stream = stream_manager_.getStream(stream_id);
+  auto stream = stream_manager_->getStream(stream_id);
   if (stream) {
     stream->reset();
     return ENVOY_SUCCESS;
@@ -92,12 +93,10 @@ envoy_status_t reset_stream(envoy_stream_t stream_id) {
   }
 }
 
-static Envoy::Http::AsyncClient* api_async_client_;
-static std::unique_ptr<Envoy::MainCommon> main_common_;
-
 void setup() {
-  api_async_client_ =
-      &main_common_->server()->clusterManager().httpAsyncClientForCluster("hello_world_api");
+  Envoy::Http::AsyncClient& api_async_client =
+      main_common_->server()->clusterManager().httpAsyncClientForCluster("hello_world_api");
+  stream_manager_ = std::make_unique<Envoy::Http::MobileAsyncStreamManager>(api_async_client);
 }
 
 /**
