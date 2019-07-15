5715eae540fb8b839d9a15347363b5a3f57b2a23
Mike Schore <mike.schore@gmail.com>
2019-07-15 16:45:06 -0700

    very hacky wip
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/common/http/header_utility.cc
+++ library/common/http/header_utility.cc
@@ -18,7 +18,20 @@ HeaderMapPtr transformHeaders(envoy_headers headers) {
 }
 
 envoy_headers transformHeaders(HeaderMapPtr&& header_map) {
-  
+  auto headers = new envoy_header[header_map.size()];
+  int i = 0;
+  header_map.iterate(
+      [&headers, &i](const HeaderEntry& header, void*) -> HeaderMap::Iterate {
+        const absl::string_view header_name = header.key().getStringView();
+        const absl::string_view header_value = header.value().getStringView();
+        envoy_string name = { strdup(header_name.data()), header_name.size() };
+        envoy_string value = { strdup(header_value.data)), header_value.size() };
+        headers[i++] = { name, value };
+        return HeaderMap::Iterate::Continue;
+      },
+      nullptr
+  );
+  return { header_map.size(), headers };
 }
 
 } // namespace Utility
