0488337f2ce279c81982c60e72d8f98afa763d3f
Jose Nino <jnino@lyft.com>
2019-07-15 17:08:38 -0700

    some fixes
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/http/header_utility.cc
+++ library/common/http/header_utility.cc
@@ -1,37 +1,37 @@
 #include "library/common/http/header_utility.h"
 
+#include "common/http/header_map_impl.h"
+
 namespace Envoy {
 namespace Http {
 namespace Utility {
 
-static inline std::string ConvertString(envoy_string s) {
-  return std::string(s.data, s.length);
-}
+static inline std::string convertString(envoy_string s) { return std::string(s.data, s.length); }
 
 HeaderMapPtr transformHeaders(envoy_headers headers) {
-  Http::HeaderMapPtr transformed_headers(new Http::HeaderMapImpl());
-  for (int i = 0; i < headers.size; i++) {
-    transformed_headers->addCopy(LowerCaseString(ConvertString(headers[i].name)),
-                                 ConvertString(headers[i].value));
+  Http::HeaderMapPtr transformed_headers = std::make_unique<HeaderMapImpl>();
+  for (int i = 0; i < headers.length; i++) {
+    // FIXME: advance the pointer
+    transformed_headers->addCopy(LowerCaseString(convertString(headers.headers->name)),
+                                 convertString(headers.headers->value));
   }
   return transformed_headers;
 }
 
 envoy_headers transformHeaders(HeaderMapPtr&& header_map) {
-  auto headers = new envoy_header[header_map.size()];
+  auto headers = new envoy_header[header_map->size()];
   int i = 0;
-  header_map.iterate(
+  header_map->iterate(
       [&headers, &i](const HeaderEntry& header, void*) -> HeaderMap::Iterate {
         const absl::string_view header_name = header.key().getStringView();
         const absl::string_view header_value = header.value().getStringView();
-        envoy_string name = { strdup(header_name.data()), header_name.size() };
+        envoy_string name = {strdup(header_name.data()), header_name.size()};
         envoy_string value = { strdup(header_value.data)), header_value.size() };
-        headers[i++] = { name, value };
+        headers[i++] = {name, value};
         return HeaderMap::Iterate::Continue;
       },
-      nullptr
-  );
-  return { header_map.size(), headers };
+      nullptr);
+  return {header_map.size(), headers};
 }
 
 } // namespace Utility
