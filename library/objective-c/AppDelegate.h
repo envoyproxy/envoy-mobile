e626822cd7d18320a8c425bd6cc6d66726977462
Mike Schore <mike.schore@gmail.com>
2019-07-17 12:08:33 -0700

    revive old harness
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/objective-c/AppDelegate.h
+++ library/objective-c/AppDelegate.h
@@ -1,3 +1,4 @@
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- library/objective-c/main.mm
+++ library/objective-c/main.mm
@@ -1,11 +1,8 @@
 #import "AppDelegate.h"
-#include "absl/debugging/symbolize.h"
 
 
 int main(int argc, char * argv[]) {
   @autoreleasepool {
-    absl::InitializeSymbolizer(argv[0]);
-
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
