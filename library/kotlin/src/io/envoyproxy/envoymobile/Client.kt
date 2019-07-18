7e1bef8c4a06f79fef4c6e6aaf19216718e96a03
Alan Chiu <achiu@lyft.com>
2019-07-18 13:29:27 -0700

    more
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/kotlin/src/io/envoyproxy/envoymobile/Client.kt
+++ library/kotlin/src/io/envoyproxy/envoymobile/Client.kt
@@ -12,20 +12,17 @@ interface StreamCallback {
   fun onMetadata(metadata: Map<String, List<String>>, endStream: Boolean)
   fun onTrailers(trailers: Map<String, List<String>>)
   fun onError(error: Error)
-
-  fun onCanceled() // TODO: onInterrupt()?
+  fun onCanceled()
   fun onCompletion()
-
 }
 
 interface StreamEmitter {
-  fun sendHeaders(headers: Map<String, List<String>>): Boolean
-  fun sendData(byteBuffer: ByteBuffer, endStream: Boolean): Boolean
-  fun sendMetadata(metadata: Map<String, List<String>>, endStream: Boolean): Boolean
-  fun sendTrailers(trailers: Map<String, List<String>>): Boolean
-
-  fun close(): Boolean
-  fun interrupt(): Boolean
+  fun isActive(): Boolean
+  fun sendData(byteBuffer: ByteBuffer, endStream: Boolean): StreamEmitter
+  fun sendMetadata(metadata: Map<String, List<String>>, endStream: Boolean): StreamEmitter
+  
+  fun close(trailers: Map<String, List<String>>)
+  fun cancel()
 }
 
 class Error(
@@ -33,41 +30,3 @@ class Error(
     val message: String,
     val cause: Throwable? = null
 )
-
-fun demo(client: Client) {
-  val streamEmitter = client.startStream(request(), object : StreamCallback {
-    override fun onHeaders(statusCode: Int, headers: Map<String, List<String>>) {
-      TODO("not implemented")
-    }
-
-    override fun onData(byteBuffer: ByteBuffer, endStream: Boolean) {
-      TODO("not implemented")
-    }
-
-    override fun onMetadata(metadata: Map<String, List<String>>, endStream: Boolean) {
-      TODO("not implemented")
-    }
-
-    override fun onTrailers(trailers: Map<String, List<String>>) {
-      TODO("not implemented")
-    }
-
-    override fun onError(error: Error) {
-      TODO("not implemented")
-    }
-
-    override fun onCanceled() {
-      TODO("not implemented")
-    }
-
-    override fun onCompletion() {
-      TODO("not implemented")
-    }
-  })
-
-
-}
-
-fun request(): Request {
-  TODO("lazily done for demo purposes")
-}
