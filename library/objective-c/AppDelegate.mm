d01da82dfee2befb6c6163de5bafdd0079898319
Mike Schore <mike.schore@gmail.com>
2019-07-19 14:18:44 -0700

    update harness
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/objective-c/AppDelegate.mm
+++ library/objective-c/AppDelegate.mm
@@ -1,4 +1,6 @@
 #import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
 #include "library/objective-c/EnvoyEngine.h"
 
 @interface AppDelegate () <NSURLConnectionDelegate>
@@ -10,6 +12,11 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [NSThread detachNewThreadSelector:@selector(envoyRunner) toTarget:self withObject:nil];
   [NSThread detachNewThreadSelector:@selector(envoyHarness) toTarget:self withObject:nil];
+
+  UIViewController *controller = [UITableViewController new];
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  [self.window setRootViewController:controller];
+  [self.window makeKeyAndVisible];
   NSLog(@"Finished launching!");
   return YES;
 }
@@ -22,22 +29,22 @@
                                                       error:NULL];
   NSLog(@"Loading config: %@", configYaml);
 
-  [EnvoyEngine runWithConfig:configYaml];
+  [EnvoyEngine runWithConfig:configYaml logLevel:@"debug"];
 }
 
 - (void)envoyHarness {
   sleep(5);
   [EnvoyEngine setupEnvoy];
-  NSString *url = @"http://0.0.0.0:9001/ping";
-  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+  //NSString *url = @"http://0.0.0.0:9001/ping";
+  //NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
   sleep(1);
   while (true) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-      NSLog(@"issuing request: %@", url);
-      [[NSURLConnection alloc] initWithRequest:request delegate:self];
-    });
+    //dispatch_async(dispatch_get_main_queue(), ^{
+    //  NSLog(@"issuing request: %@", url);
+    //  [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    //});
     [EnvoyEngine makeRequest];
-    sleep(5);
+    sleep(1);
   }
 }
 
