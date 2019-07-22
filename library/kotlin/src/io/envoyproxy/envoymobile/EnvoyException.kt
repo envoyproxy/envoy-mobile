da5199346b079213ee47a7e55cacd73302e47799
Alan Chiu <achiu@lyft.com>
2019-07-22 12:40:43 -0700

    errorCodeg
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/kotlin/src/io/envoyproxy/envoymobile/EnvoyException.kt
+++ library/kotlin/src/io/envoyproxy/envoymobile/EnvoyException.kt
@@ -5,12 +5,12 @@ import java.io.IOException
 /**
  * An Envoy Exception class for describing what error may have occurred.
  *
- * @param code internal error code associated with the exception that occurred.
+ * @param errorCode internal error code associated with the exception that occurred.
  * @param message a description of what exception that occurred.
  * @param cause an optional cause for the exception.
  */
 class EnvoyException(
-    val code: Int,
+    val errorCode: Int,
     override val message: String,
     override val cause: Throwable? = null
 ) : IOException(message, cause)
