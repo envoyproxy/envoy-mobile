07029ac28fac87ba22038b279828c3e79f164867
Michael Rebello <mrebello@lyft.com>
2019-07-22 12:19:54 -0700

    s/code/errorCode
    Signed-off-by: Michael Rebello <mrebello@lyft.com>

--- library/swift/src/Error.swift
+++ library/swift/src/Error.swift
@@ -3,14 +3,14 @@ import Foundation
 @objcMembers
 public final class Error: NSObject, Swift.Error {
   /// Error code associated with the exception that occurred.
-  public let code: Int
+  public let errorCode: Int
   /// A description of what exception that occurred.
   public let message: String
   /// Optional cause for the error.
   public let cause: Swift.Error?
 
-  init(code: Int, message: String, cause: Swift.Error?) {
-    self.code = code
+  init(errorCode: Int, message: String, cause: Swift.Error?) {
+    self.errorCode = errorCode
     self.message = message
     self.cause = cause
   }
