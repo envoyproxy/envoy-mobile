59d5b301003999773872e7bd1347d23b7061c6bd
Michael Rebello <mrebello@lyft.com>
2019-07-22 11:18:35 -0700

    docstrings
    Signed-off-by: Michael Rebello <mrebello@lyft.com>

--- library/swift/src/StreamCallback.swift
+++ library/swift/src/StreamCallback.swift
@@ -3,37 +3,37 @@ import Foundation
 /// Callback interface for receiving stream events.
 @objc
 public protocol StreamCallback {
-  /// Invoked when response headers are read.
+  /// Called when response headers are received by the stream.
   ///
   /// - parameter headers:    The headers of the response.
   /// - parameter statusCode: The status code of the response.
   func onHeaders(_ headers: [String: [String]], statusCode: Int)
 
-  /// Invoked when response body is read.
+  /// Called when a data frame is received by the stream.
   ///
   /// - parameter data:      Bytes in the response.
   /// - parameter endStream: True if the stream is complete.
   func onData(_ data: Data, endStream: Bool)
 
-  /// Invoked when response metadata is read.
+  /// Called when response metadata is received by the stream.
   ///
   /// - parameter metadata:  The metadata of the response.
   /// - parameter endStream: True if the stream is complete.
   func onMetadata(_ metadata: [String: [String]], endStream: Bool)
 
-  /// Invoked when response trailers are read.
+  /// Called when response trailers are received by the stream.
   ///
   /// - parameter trailers: The trailers of the response.
   func onTrailers(_ trailers: [String: [String]])
 
-  /// Invoked when there is an internal Envoy error with the stream.
+  /// Called when an internal Envoy exception occurs with the stream.
   ///
   /// - parameter error: The error that occurred with the stream.
   func onError(_ error: Error)
 
-  /// Invoked when the stream is canceled.
+  /// Called when the stream is canceled.
   func onCanceled()
 
-  /// Invoked when the stream is completed.
+  /// Called when the stream has been completed.
   func onCompletion()
 }
