a859ea86f40da4e5999dd98614d34374ecc7a32f
Jose Nino <jnino@lyft.com>
2019-07-23 14:15:45 -0700

    comments
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/buffer/utility.cc
+++ library/common/buffer/utility.cc
@@ -6,8 +6,10 @@ namespace Envoy {
 namespace Buffer {
 namespace Utility {
 
+// TODO: implement this https://github.com/lyft/envoy-mobile/issues/284.
 Buffer::InstancePtr transformData(envoy_data) { return nullptr; }
 
+// TODO: implement this https://github.com/lyft/envoy-mobile/issues/284.
 envoy_data transformData(Buffer::Instance&) { return {0, nullptr}; }
 
 } // namespace Utility
