44852340ca32b23ad6762f2cb74a4e277dfaebbd
Jose Nino <jnino@lyft.com>
2019-07-23 15:46:07 -0700

    fmt
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/include/c_types.h
+++ library/common/include/c_types.h
@@ -93,7 +93,8 @@ extern "C" { // function pointers
  * Called when all headers get received on the async HTTP stream.
  * @param headers, the headers received.
  * @param end_stream, whether the response is headers-only.
- * @param context, contains the necessary state to carry out platform-specific dispatch and execution.
+ * @param context, contains the necessary state to carry out platform-specific dispatch and
+ * execution.
  */
 typedef void (*on_headers)(envoy_headers headers, bool end_stream, void* context);
 /**
@@ -101,27 +102,31 @@ typedef void (*on_headers)(envoy_headers headers, bool end_stream, void* context
  * This callback can be invoked multiple times if the data gets streamed.
  * @param data, the data received.
  * @param end_stream, whether the data is the last data frame.
- * @param context, contains the necessary state to carry out platform-specific dispatch and execution.
+ * @param context, contains the necessary state to carry out platform-specific dispatch and
+ * execution.
  */
 typedef void (*on_data)(envoy_data data, bool end_stream, void* context);
 /**
  * Called when all trailers get received on the async HTTP stream.
  * Note that end stream is implied when on_trailers is called.
  * @param metadata, the trailers received.
- * @param context, contains the necessary state to carry out platform-specific dispatch and execution.
+ * @param context, contains the necessary state to carry out platform-specific dispatch and
+ * execution.
  */
 typedef void (*on_metadata)(envoy_headers metadata, void* context);
 /**
  * Called when all trailers get received on the async HTTP stream.
  * Note that end stream is implied when on_trailers is called.
  * @param trailers, the trailers received.
- * @param context, contains the necessary state to carry out platform-specific dispatch and execution.
+ * @param context, contains the necessary state to carry out platform-specific dispatch and
+ * execution.
  */
 typedef void (*on_trailers)(envoy_headers trailers, void* context);
 /**
  * Called when the async HTTP stream has an error.
  * @param envoy_error, the error received/caused by the async HTTP stream.
- * @param context, contains the necessary state to carry out platform-specific dispatch and execution.
+ * @param context, contains the necessary state to carry out platform-specific dispatch and
+ * execution.
  */
 typedef void (*on_error)(envoy_error error, void* context);
 
