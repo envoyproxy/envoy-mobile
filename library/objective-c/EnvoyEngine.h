193e578c4fce9efa3a1c56956c82e2ddd254badb
Michael Rebello <me@michaelrebello.com>
2019-07-22 18:06:45 -0700

    wip: objective-c shim/bridging updates
    Signed-off-by: Michael Rebello <me@michaelrebello.com>

--- library/objective-c/EnvoyEngine.h
+++ library/objective-c/EnvoyEngine.h
@@ -11,4 +11,24 @@
 /// and will not yield.
 + (int)runWithConfig:(NSString *)config logLevel:(NSString *)logLevel;
 
+typedef NSArray<NSDictionary<NSString *, NSString *> *> EnvoyHeaders;
+
+typedef void (^OnHeadersClosure)(EnvoyHeaders *, BOOL);
+
+typedef void (^OnDataClosure)(NSData *, BOOL);
+
+typedef void (^OnTrailersClosure)(EnvoyHeaders *, BOOL);
+
+@end
+
+
+
+typedef enum {} EnvoyErrorCode;
+
+@interface EnvoyError: NSError
+
+@property (nonatomic, strong) NSString *message;
+
+@property (nonatomic, assign) EnvoyErrorCode error_code;
+
 @end
