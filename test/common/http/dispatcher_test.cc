b38dfbceb91c59c499f8a2caac9f325fe4401b09
Jose Nino <jnino@lyft.com>
2019-07-29 14:53:03 -0700

    nice. I like this now.
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/common/http/dispatcher_test.cc
+++ test/common/http/dispatcher_test.cc
@@ -41,7 +41,10 @@ public:
 
   Stats::MockIsolatedStatsStore stats_store_;
   MockAsyncClientCallbacks callbacks_;
+  MockAsyncClientStreamCallbacks stream_callbacks_;
   NiceMock<Upstream::MockClusterManager> cm_;
+  NiceMock<MockStreamEncoder> stream_encoder_;
+  StreamDecoder* response_decoder_{};
   NiceMock<Event::MockDispatcher> event_dispatcher_;
   NiceMock<Runtime::MockLoader> runtime_;
   NiceMock<Runtime::MockRandomGenerator> random_;
@@ -52,53 +55,81 @@ public:
   envoy_observer observer_;
 };
 
-TEST_F(DispatcherTest, SendHeaders) {
-  std::vector<std::pair<std::string, std::string>> header_strings = {
-      {":method", "GET"}, {":scheme", "https"}, {":authority", "api.lyft.com"}, {":path", "/ping"}};
+TEST_F(DispatcherTest, BasicStreamHeadersOnly) {
+  // Setup observer to handle the response headers.
+  envoy_observer observer;
+  bool on_headers_called = false;
+  observer.context = &on_headers_called;
+  observer.on_headers_f = [](envoy_headers c_headers, bool end_stream, void* context) -> void {
+    ASSERT_TRUE(end_stream);
+    HeaderMapPtr response_headers = Utility::transformHeaders(c_headers);
+    EXPECT_EQ(response_headers->Status()->value().getStringView(), "200");
+    bool* on_headers_called = static_cast<bool*>(context);
+    *on_headers_called = true;
+  };
 
-  envoy_header* header_array = new envoy_header[header_strings.size()];
+  // Grab the response decoder in order to dispatch responses on the stream.
+  EXPECT_CALL(cm_.conn_pool_, newStream(_, _))
+      .WillOnce(Invoke([&](StreamDecoder& decoder,
+                           ConnectionPool::Callbacks& callbacks) -> ConnectionPool::Cancellable* {
+        callbacks.onPoolReady(stream_encoder_, cm_.conn_pool_.host_);
+        response_decoder_ = &decoder;
+        return nullptr;
+      }));
 
-  for (size_t i = 0; i < header_strings.size(); i++) {
-    header_array[i] = {
-        envoyString(header_strings[i].first),
-        envoyString(header_strings[i].second),
-    };
-  }
+  // Build a set of request headers.
+  TestHeaderMapImpl headers;
+  HttpTestUtility::addDefaultHeaders(headers);
+  envoy_headers c_headers = Utility::transformHeaders(headers);
 
-  envoy_headers headers = {header_strings.size(), header_array};
-  NiceMock<MockAsyncClientStream> underlying_stream;
-
+  // Create a stream.
   EXPECT_CALL(cm_, httpAsyncClientForCluster("egress_cluster"))
       .WillOnce(ReturnRef(cm_.async_client_));
-  EXPECT_CALL(cm_.async_client_, start(_, _)).WillOnce(Return(&underlying_stream));
-  envoy_stream_t stream_id = http_dispatcher_.startStream(observer_);
+  EXPECT_CALL(cm_.async_client_, start(_, _))
+      .WillOnce(
+          WithArg<0>(Invoke([&](AsyncClient::StreamCallbacks& callbacks) -> AsyncClient::Stream* {
+            return client_.start(callbacks, AsyncClient::StreamOptions());
+          })));
+  envoy_stream_t stream_id = http_dispatcher_.startStream(observer);
 
+  // Send request headers.
   Event::PostCb post_cb;
   EXPECT_CALL(event_dispatcher_, post(_)).WillOnce(SaveArg<0>(&post_cb));
-  http_dispatcher_.sendHeaders(stream_id, headers, true);
+  http_dispatcher_.sendHeaders(stream_id, c_headers, true);
 
   EXPECT_CALL(event_dispatcher_, isThreadSafe()).Times(2).WillRepeatedly(Return(true));
-  EXPECT_CALL(underlying_stream, sendHeaders(_, true)).Times(1);
+  EXPECT_CALL(stream_encoder_, encodeHeaders(_, true));
   post_cb();
-}
 
-TEST_F(DispatcherTest, SendHeadersNoStream) {
-  envoy_headers headers;
-  Event::PostCb post_cb;
-  EXPECT_CALL(event_dispatcher_, post(_)).WillOnce(SaveArg<0>(&post_cb));
-  http_dispatcher_.sendHeaders(0, headers, true);
+  // Decode response headers. decodeHeaders with true will bubble up to onHeaders, which will in
+  // turn cause closeRemote. Because closeLocal has already been called, cleanup will happen; hence
+  // the second call to isThreadSafe.
+  // TODO: find a way to make the fact that the stream is correctly closed more explicit.
+  EXPECT_CALL(event_dispatcher_, isThreadSafe()).Times(2).WillRepeatedly(Return(true));
+  response_decoder_->decode100ContinueHeaders(
+      HeaderMapPtr(new TestHeaderMapImpl{{":status", "100"}}));
+  response_decoder_->decodeHeaders(HeaderMapPtr(new TestHeaderMapImpl{{":status", "200"}}), true);
 
-  EXPECT_CALL(event_dispatcher_, isThreadSafe()).Times(1).WillRepeatedly(Return(true));
-  // FIXME better proof that sendHeaders was not called.
-  post_cb();
+  EXPECT_EQ(
+      1UL,
+      cm_.thread_local_cluster_.cluster_.info_->stats_store_.counter("upstream_rq_200").value());
+  EXPECT_EQ(1UL, cm_.thread_local_cluster_.cluster_.info_->stats_store_
+                     .counter("internal.upstream_rq_200")
+                     .value());
+
+  // Ensure that the on_headers_f on the observer was called.
+  ASSERT_TRUE(on_headers_called);
 }
 
 TEST_F(DispatcherTest, ResetStream) {
   envoy_observer observer;
-
-  observer.on_error_f = [](envoy_error actual_error, void*) -> void {
+  bool on_error_called = false;
+  observer.context = &on_error_called;
+  observer.on_error_f = [](envoy_error actual_error, void* context) -> void {
     envoy_error expected_error = {ENVOY_STREAM_RESET, {0, nullptr}};
     ASSERT_EQ(actual_error.error_code, expected_error.error_code);
+    bool* on_error_called = static_cast<bool*>(context);
+    *on_error_called = true;
   };
 
   EXPECT_CALL(cm_, httpAsyncClientForCluster("egress_cluster"))
@@ -116,6 +147,9 @@ TEST_F(DispatcherTest, ResetStream) {
 
   EXPECT_CALL(event_dispatcher_, isThreadSafe()).Times(4).WillRepeatedly(Return(true));
   post_cb();
+
+  // Ensure that the on_error_f on the observer was called.
+  ASSERT_TRUE(on_error_called);
 }
 
 } // namespace Http
