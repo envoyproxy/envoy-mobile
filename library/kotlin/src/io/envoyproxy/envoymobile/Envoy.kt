7e83143e5ed3a095cb9976d9e6ae426d34826a88
Alan Chiu <achiu@lyft.com>
2019-08-21 16:55:14 -0700

    move everything into the same file
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/kotlin/src/io/envoyproxy/envoymobile/Envoy.kt
+++ library/kotlin/src/io/envoyproxy/envoymobile/Envoy.kt
@@ -1,6 +1,7 @@
 package io.envoyproxy.envoymobile
 
 import io.envoyproxy.envoymobile.engine.EnvoyEngine
+import io.envoyproxy.envoymobile.engine.types.EnvoyObserver
 import java.nio.ByteBuffer
 
 /**
@@ -74,4 +75,214 @@ class Envoy constructor(
   override fun send(request: Request, body: ByteBuffer?, responseHandler: ResponseHandler): CancelableStream {
     return send(request, body, emptyMap(), responseHandler)
   }
+}
+
+// For some reason we have an issue building Envoy.kt when the below interfaces/classes are not in this file
+// Issue tracked:
+
+interface Client {
+  /**
+   * For starting a stream.
+   *
+   * @param request the request for opening a stream.
+   * @param responseHandler the callback for receiving stream events.
+   * @return the emitter for streaming data outward.
+   */
+  fun send(request: Request, responseHandler: ResponseHandler): StreamEmitter
+
+
+  /**
+   * Convenience function for sending a unary request.
+   *
+   * @param request  The request to send.
+   * @param data Serialized data to send as the body of the request.
+   * @param trailers Trailers to send with the request.
+   * @param responseHandler the callback for receiving stream events.
+   * @return CancelableStream, a cancelable request.
+   */
+  fun send(request: Request, data: ByteBuffer?, trailers: Map<String, List<String>>,
+           responseHandler: ResponseHandler): CancelableStream
+
+  /**
+   * Convenience function for sending a unary request.
+   *
+   * @param request The request to send.
+   * @param body Serialized data to send as the body of the request.
+   * @param responseHandler the callback for receiving stream events.
+   * @return CancelableStream, a cancelable request.
+   */
+  fun send(request: Request, body: ByteBuffer?,
+           responseHandler: ResponseHandler): CancelableStream
+}
+
+
+/**
+ * Callback interface for receiving stream events.
+ */
+class ResponseHandler {
+
+  class EnvoyObserverAdapter(
+      internal val responseHandler: ResponseHandler
+  ) : EnvoyObserver {
+
+    override fun onHeaders(headers: Map<String, List<String>>?, endStream: Boolean) {
+      val statusCode = headers!![":status"]?.first()?.toIntOrNull() ?: 0
+      responseHandler.onHeadersClosure(headers, statusCode)
+    }
+
+    override fun onData(byteBuffer: ByteBuffer?, endStream: Boolean) {
+      responseHandler.onDataClosure(byteBuffer, endStream)
+    }
+
+    override fun onMetadata(metadata: Map<String, List<String>>?) {
+      responseHandler.onMetadataClosure(metadata!!)
+    }
+
+    override fun onTrailers(trailers: Map<String, List<String>>?) {
+      responseHandler.onTrailersClosure(trailers!!)
+    }
+
+    override fun onError() {
+      responseHandler.onErrorClosure()
+    }
+
+    override fun onCancel() {
+      responseHandler.onCancelClosure()
+    }
+  }
+
+  internal val underlyingObserver = EnvoyObserverAdapter(this)
+
+  private var onHeadersClosure: (headers: Map<String, List<String>>, statusCode: Int) -> Unit = { _, _ -> Unit }
+  private var onDataClosure: (byteBuffer: ByteBuffer?, endStream: Boolean) -> Unit = { _, _ -> Unit }
+  private var onMetadataClosure: (metadata: Map<String, List<String>>) -> Unit = { Unit }
+  private var onTrailersClosure: (trailers: Map<String, List<String>>) -> Unit = { Unit }
+  private var onErrorClosure: () -> Unit = { Unit }
+  private var onCancelClosure: () -> Unit = { Unit }
+
+  /**
+   * Specify a callback for when response headers are received by the stream.
+   * If `endStream` is `true`, the stream is complete.
+   *
+   * @param closure: Closure which will receive the headers, status code,
+   *                 and flag indicating if the stream is headers-only.
+   * @param statusCode the status code of the response.
+   * @return ResponseHandler, this ResponseHandler.
+   */
+  fun onHeaders(closure: (headers: Map<String, List<String>>, statusCode: Int) -> Unit): ResponseHandler {
+    this.onHeadersClosure = closure
+    return this
+  }
+
+  /**
+   * Specify a callback for when a data frame is received by the stream.
+   * If `endStream` is `true`, the stream is complete.
+   *
+   * @param closure: Closure which will receive the data,
+   *                 and flag indicating if the stream is complete.
+   * @return ResponseHandler, this ResponseHandler.
+   */
+  fun onData(closure: (byteBuffer: ByteBuffer?, endStream: Boolean) -> Unit): ResponseHandler {
+    this.onDataClosure = closure
+    return this
+  }
+
+  /**
+   * Called when response metadata is received by the stream.
+   *
+   * @param metadata the metadata of a response.
+   * @param endStream true if the stream is complete.
+   * @return ResponseHandler, this ResponseHandler.
+   */
+  fun onMetadata(closure: (metadata: Map<String, List<String>>) -> Unit): ResponseHandler {
+    this.onMetadataClosure = closure
+    return this
+  }
+
+  /**
+   * Specify a callback for when trailers are received by the stream.
+   * If the closure is called, the stream is complete.
+   *
+   * @param closure: Closure which will receive the trailers.
+   * @return ResponseHandler, this ResponseHandler.
+   */
+  fun onTrailers(closure: (trailers: Map<String, List<String>>) -> Unit): ResponseHandler {
+    this.onTrailersClosure = closure
+    return this
+  }
+
+  /**
+   * Specify a callback for when an internal Envoy exception occurs with the stream.
+   * If the closure is called, the stream is complete.
+   *
+   * @param closure: Closure which will be called when an error occurs.
+   * @return ResponseHandler, this ResponseHandler.
+   */
+  fun onError(closure: () -> Unit): ResponseHandler {
+    this.onErrorClosure = closure
+    return this
+  }
+
+  /**
+   * Specify a callback for when the stream is canceled.
+   * If the closure is called, the stream is complete.
+   *
+   * @param closure: Closure which will be called when the stream is canceled.
+   * @return ResponseHandler, this ResponseHandler.
+   */
+  fun onCanceled(closure: () -> Unit): ResponseHandler {
+    this.onCancelClosure = closure
+    return this
+  }
+}
+
+
+/**
+ * Interface for a stream that may be canceled.
+ */
+interface CancelableStream {
+  /**
+   * Cancel and end the associated stream.
+   * @throws EnvoyException when there is an exception canceling the stream or sending trailers.
+   */
+  @Throws(EnvoyException::class)
+  fun cancel()
+}
+
+/**
+ * Interface allowing for sending/emitting data on an Envoy stream.
+ */
+interface StreamEmitter : CancelableStream {
+
+  /**
+   * For sending data to an associated stream.
+   *
+   * @param byteBuffer the byte buffer data to send to the stream.
+   * @throws IllegalStateException when the stream is not active
+   * @throws EnvoyException when there is an exception sending data.
+   * @return this stream emitter.
+   */
+  @Throws(EnvoyException::class)
+  fun sendData(byteBuffer: ByteBuffer): StreamEmitter
+
+  /**
+   * For sending a map of metadata to an associated stream.
+   *
+   * @param metadata the metadata to send over the stream.
+   * @throws IllegalStateException when the stream is not active.
+   * @throws EnvoyException when there is an exception sending metadata.
+   * @return this stream emitter.
+   */
+  @Throws(EnvoyException::class)
+  fun sendMetadata(metadata: Map<String, List<String>>): StreamEmitter
+
+  /**
+   * For ending an associated stream and sending trailers.
+   *
+   * @param trailers to send with ending a stream. If no trailers are needed, empty map will be the default.
+   * @throws IllegalStateException when the stream is not active.
+   * @throws EnvoyException when there is an exception ending the stream or sending trailers.
+   */
+  @Throws(EnvoyException::class)
+  fun close(trailers: Map<String, List<String>> = emptyMap())
 }
