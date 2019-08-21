envoyproxy/envoy-mobile

commit 1d1af0b7ec27e7f921b00c92bf527595d6b05e56

kotlin: update ResponseHandler interface (#357)
Update ResponseHandler interface and update the java types to be consistent with the objc layer

Signed-off-by: Alan Chiu <achiu@lyft.com>

For an explanation of how to fill out the fields, please see the relevant section
in [PULL_REQUESTS.md](https://github.com/envoyproxy/envoy/blob/master/PULL_REQUESTS.md)

Description: kotlin: update ResponseHandler interface
Risk Level: low
Testing: unit
Docs Changes: n/a
Release Notes: n/a
[Optional Fixes #Issue]
[Optional Deprecated:]

library/java/src/io/envoyproxy/envoymobile/engine/EnvoyHTTPStream.java
@@ -1,8 +1,10 @@
 package io.envoyproxy.envoymobile.engine;
 
-import io.envoyproxy.envoymobile.engine.types.EnvoyData;
-import io.envoyproxy.envoymobile.engine.types.EnvoyHeaders;
 import io.envoyproxy.envoymobile.engine.types.EnvoyObserver;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
 
 public class EnvoyHTTPStream {
 
@@ -20,7 +22,7 @@ public class EnvoyHTTPStream {
    * @param headers,   the headers to send.
    * @param endStream, supplies whether this is headers only.
    */
-  public void sendHeaders(EnvoyHeaders headers, boolean endStream) {
+  public void sendHeaders(Map<String, List<String>> headers, boolean endStream) {
     JniLibrary.sendHeaders(streamHandle, headers, endStream);
   }
 
@@ -30,7 +32,7 @@ public class EnvoyHTTPStream {
    * @param data,      the data to send.
    * @param endStream, supplies whether this is the last data in the streamHandle.
    */
-  public void sendData(EnvoyData data, boolean endStream) {
+  public void sendData(ByteBuffer data, boolean endStream) {
     JniLibrary.sendData(streamHandle, data, endStream);
   }
 
@@ -39,7 +41,7 @@ public class EnvoyHTTPStream {
    *
    * @param metadata, the metadata to send.
    */
-  public void sendMetadata(EnvoyHeaders metadata) {
+  public void sendMetadata(Map<String, List<String>> metadata) {
     JniLibrary.sendMetadata(streamHandle, metadata);
   }
 
@@ -49,7 +51,7 @@ public class EnvoyHTTPStream {
    *
    * @param trailers, the trailers to send.
    */
-  public void sendTrailers(EnvoyHeaders trailers) {
+  public void sendTrailers(Map<String, List<String>> trailers) {
     JniLibrary.sendTrailers(streamHandle, trailers);
   }
 
@@ -59,5 +61,7 @@ public class EnvoyHTTPStream {
    *
    * @return Success, unless the streamHandle has already been canceled.
    */
-  public int resetStream() { return JniLibrary.resetStream(streamHandle); }
+  public int resetStream() {
+    return JniLibrary.resetStream(streamHandle);
+  }
 }

library/java/src/io/envoyproxy/envoymobile/engine/JniLibrary.java
@@ -1,8 +1,10 @@
 package io.envoyproxy.envoymobile.engine;
 
-import io.envoyproxy.envoymobile.engine.types.EnvoyData;
-import io.envoyproxy.envoymobile.engine.types.EnvoyHeaders;
 import io.envoyproxy.envoymobile.engine.types.EnvoyObserver;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
 
 class JniLibrary {
 
@@ -61,7 +63,8 @@ class JniLibrary {
    * @param endStream, supplies whether this is headers only.
    * @return int, the resulting status of the operation.
    */
-  protected static native int sendHeaders(long stream, EnvoyHeaders headers, boolean endStream);
+  protected static native int sendHeaders(long stream, Map<String, List<String>> headers,
+                                          boolean endStream);
 
   /**
    * Send data over an open HTTP stream. This method can be invoked multiple times.
@@ -71,7 +74,7 @@ class JniLibrary {
    * @param endStream, supplies whether this is the last data in the stream.
    * @return int, the resulting status of the operation.
    */
-  protected static native int sendData(long stream, EnvoyData data, boolean endStream);
+  protected static native int sendData(long stream, ByteBuffer data, boolean endStream);
 
   /**
    * Send metadata over an HTTP stream. This method can be invoked multiple times.
@@ -80,7 +83,7 @@ class JniLibrary {
    * @param metadata, the metadata to send.
    * @return int, the resulting status of the operation.
    */
-  protected static native int sendMetadata(long stream, EnvoyHeaders metadata);
+  protected static native int sendMetadata(long stream, Map<String, List<String>> metadata);
 
   /**
    * Send trailers over an open HTTP stream. This method can only be invoked once per stream.
@@ -90,7 +93,7 @@ class JniLibrary {
    * @param trailers, the trailers to send.
    * @return int, the resulting status of the operation.
    */
-  protected static native int sendTrailers(long stream, EnvoyHeaders trailers);
+  protected static native int sendTrailers(long stream, Map<String, List<String>> trailers);
 
   /**
    * Detach all observers from a stream and send an interrupt upstream if supported by transport.

library/java/src/io/envoyproxy/envoymobile/engine/types/BUILD
@@ -5,11 +5,8 @@ load("//bazel:kotlin_lib.bzl", "envoy_mobile_java_library")
 envoy_mobile_java_library(
     name = "envoy_c_types_lib",
     srcs = [
-        "EnvoyData.java",
         "EnvoyError.java",
         "EnvoyErrorCode.java",
-        "EnvoyHeader.java",
-        "EnvoyHeaders.java",
         "EnvoyObserver.java",
         "EnvoyStatus.java",
     ],

library/java/src/io/envoyproxy/envoymobile/engine/types/EnvoyData.java
@@ -1,11 +1,0 @@
-package io.envoyproxy.envoymobile.engine.types;
-
-public class EnvoyData {
-  public final long length;
-  public final byte[] bytes;
-
-  public EnvoyData(long length, byte[] bytes) {
-    this.length = length;
-    this.bytes = bytes;
-  }
-}

library/java/src/io/envoyproxy/envoymobile/engine/types/EnvoyError.java
@@ -2,9 +2,9 @@ package io.envoyproxy.envoymobile.engine.types;
 
 public class EnvoyError {
   public final EnvoyErrorCode errorCode;
-  public final EnvoyData message;
+  public final String message;
 
-  public EnvoyError(EnvoyErrorCode errorCode, EnvoyData message) {
+  public EnvoyError(EnvoyErrorCode errorCode, String message) {
     this.errorCode = errorCode;
     this.message = message;
   }

library/java/src/io/envoyproxy/envoymobile/engine/types/EnvoyHeader.java
@@ -1,11 +1,0 @@
-package io.envoyproxy.envoymobile.engine.types;
-
-public class EnvoyHeader {
-  public final EnvoyData key;
-  public final EnvoyData value;
-
-  public EnvoyHeader(EnvoyData key, EnvoyData value) {
-    this.key = key;
-    this.value = value;
-  }
-}

library/java/src/io/envoyproxy/envoymobile/engine/types/EnvoyHeaders.java
@@ -1,11 +1,0 @@
-package io.envoyproxy.envoymobile.engine.types;
-
-public class EnvoyHeaders {
-  public final long length;
-  public final EnvoyHeader[] envoyHeaders;
-
-  public EnvoyHeaders(long length, EnvoyHeader[] envoyHeaders) {
-    this.length = length;
-    this.envoyHeaders = envoyHeaders;
-  }
-}

library/java/src/io/envoyproxy/envoymobile/engine/types/EnvoyObserver.java
@@ -1,5 +1,9 @@
 package io.envoyproxy.envoymobile.engine.types;
 
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+
 public interface EnvoyObserver {
   /**
    * Called when all headers get received on the async HTTP stream.
@@ -8,17 +12,17 @@ public interface EnvoyObserver {
    * @param endStream, whether the response is headers-only.
    *                   execution.
    */
-  void onHeaders(EnvoyHeaders headers, boolean endStream);
+  void onHeaders(Map<String, List<String>> headers, boolean endStream);
 
   /**
    * Called when a data frame gets received on the async HTTP stream.
    * This callback can be invoked multiple times if the data gets streamed.
    *
-   * @param data,      the data received.
+   * @param data,      the buffer of the data received.
    * @param endStream, whether the data is the last data frame.
    *                   execution.
    */
-  void onData(EnvoyData data, boolean endStream);
+  void onData(ByteBuffer data, boolean endStream);
 
   /**
    * Called when all metadata get received on the async HTTP stream.
@@ -27,7 +31,7 @@ public interface EnvoyObserver {
    * @param metadata, the metadata received.
    *                  execution.
    */
-  void onMetadata(EnvoyHeaders metadata);
+  void onMetadata(Map<String, List<String>> metadata);
 
   /**
    * Called when all trailers get received on the async HTTP stream.
@@ -36,13 +40,15 @@ public interface EnvoyObserver {
    * @param trailers, the trailers received.
    *                  execution.
    */
-  void onTrailers(EnvoyHeaders trailers);
+  void onTrailers(Map<String, List<String>> trailers);
 
   /**
    * Called when the async HTTP stream has an error.
-   *
-   * @param error, the error received/caused by the async HTTP stream.
-   *               execution.
    */
-  void onError(EnvoyError error);
+  void onError();
+
+  /**
+   * Called when the async HTTP stream is canceled.
+   */
+  void onCancel();
 }

commit ac57a8f934d2d4165baeb19a288d06df1c0a5e9c

java: fix master linter
Broken by #357.

Signed-off-by: Michael Rebello <me@michaelrebello.com>

library/java/src/io/envoyproxy/envoymobile/engine/EnvoyHTTPStream.java
@@ -61,7 +61,5 @@ public class EnvoyHTTPStream {
    *
    * @return Success, unless the streamHandle has already been canceled.
    */
-  public int resetStream() {
-    return JniLibrary.resetStream(streamHandle);
-  }
+  public int resetStream() { return JniLibrary.resetStream(streamHandle); }
 }