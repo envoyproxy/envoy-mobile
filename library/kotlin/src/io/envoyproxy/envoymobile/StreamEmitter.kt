6a0030e9de2aca405313dd78b5a485211301b3b3
Alan Chiu <achiu@lyft.com>
2019-08-21 17:01:09 -0700

    Revert "move everything into the same file"
    This reverts commit 7e83143e5ed3a095cb9976d9e6ae426d34826a88.
    
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/kotlin/src/io/envoyproxy/envoymobile/StreamEmitter.kt
+++ library/kotlin/src/io/envoyproxy/envoymobile/StreamEmitter.kt
@@ -1,53 +1,53 @@
 package io.envoyproxy.envoymobile
 
 import java.nio.ByteBuffer
-//
-///**
-// * Interface for a stream that may be canceled.
-// */
-//interface CancelableStream {
-//  /**
-//   * Cancel and end the associated stream.
-//   * @throws EnvoyException when there is an exception canceling the stream or sending trailers.
-//   */
-//  @Throws(EnvoyException::class)
-//  fun cancel()
-//}
-//
-///**
-// * Interface allowing for sending/emitting data on an Envoy stream.
-// */
-//interface StreamEmitter : CancelableStream {
-//
-//  /**
-//   * For sending data to an associated stream.
-//   *
-//   * @param byteBuffer the byte buffer data to send to the stream.
-//   * @throws IllegalStateException when the stream is not active
-//   * @throws EnvoyException when there is an exception sending data.
-//   * @return this stream emitter.
-//   */
-//  @Throws(EnvoyException::class)
-//  fun sendData(byteBuffer: ByteBuffer): StreamEmitter
-//
-//  /**
-//   * For sending a map of metadata to an associated stream.
-//   *
-//   * @param metadata the metadata to send over the stream.
-//   * @throws IllegalStateException when the stream is not active.
-//   * @throws EnvoyException when there is an exception sending metadata.
-//   * @return this stream emitter.
-//   */
-//  @Throws(EnvoyException::class)
-//  fun sendMetadata(metadata: Map<String, List<String>>): StreamEmitter
-//
-//  /**
-//   * For ending an associated stream and sending trailers.
-//   *
-//   * @param trailers to send with ending a stream. If no trailers are needed, empty map will be the default.
-//   * @throws IllegalStateException when the stream is not active.
-//   * @throws EnvoyException when there is an exception ending the stream or sending trailers.
-//   */
-//  @Throws(EnvoyException::class)
-//  fun close(trailers: Map<String, List<String>> = emptyMap())
-//}
+
+/**
+ * Interface for a stream that may be canceled.
+ */
+interface CancelableStream {
+  /**
+   * Cancel and end the associated stream.
+   * @throws EnvoyException when there is an exception canceling the stream or sending trailers.
+   */
+  @Throws(EnvoyException::class)
+  fun cancel()
+}
+
+/**
+ * Interface allowing for sending/emitting data on an Envoy stream.
+ */
+interface StreamEmitter : CancelableStream {
+
+  /**
+   * For sending data to an associated stream.
+   *
+   * @param byteBuffer the byte buffer data to send to the stream.
+   * @throws IllegalStateException when the stream is not active
+   * @throws EnvoyException when there is an exception sending data.
+   * @return this stream emitter.
+   */
+  @Throws(EnvoyException::class)
+  fun sendData(byteBuffer: ByteBuffer): StreamEmitter
+
+  /**
+   * For sending a map of metadata to an associated stream.
+   *
+   * @param metadata the metadata to send over the stream.
+   * @throws IllegalStateException when the stream is not active.
+   * @throws EnvoyException when there is an exception sending metadata.
+   * @return this stream emitter.
+   */
+  @Throws(EnvoyException::class)
+  fun sendMetadata(metadata: Map<String, List<String>>): StreamEmitter
+
+  /**
+   * For ending an associated stream and sending trailers.
+   *
+   * @param trailers to send with ending a stream. If no trailers are needed, empty map will be the default.
+   * @throws IllegalStateException when the stream is not active.
+   * @throws EnvoyException when there is an exception ending the stream or sending trailers.
+   */
+  @Throws(EnvoyException::class)
+  fun close(trailers: Map<String, List<String>> = emptyMap())
+}
