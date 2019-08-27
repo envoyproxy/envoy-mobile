b008e8e5f055f522cbf0c175c18f4229f731a296
Alan Chiu <achiu@lyft.com>
2019-08-27 10:08:35 -0700

    format
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/java/src/io/envoyproxy/envoymobile/engine/EnvoyConfiguration.java
+++ library/java/src/io/envoyproxy/envoymobile/engine/EnvoyConfiguration.java
@@ -23,8 +23,7 @@ public class EnvoyConfiguration {
       resolvedYAML = configYAML;
     }
     String resolvedConfiguration =
-        resolvedYAML
-            .replace("{{ connect_timeout }}", String.format("%ss", connectTimeoutSeconds))
+        resolvedYAML.replace("{{ connect_timeout }}", String.format("%ss", connectTimeoutSeconds))
             .replace("{{ dns_refresh_rate }}", String.format("%ss", dnsRefreshSeconds))
             .replace("{{ stats_flush_interval }}", String.format("%ss", statsFlushSeconds));
     if (resolvedConfiguration.contains("{{")) {
