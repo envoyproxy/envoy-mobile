6d43b45eb31a2764cad995ea6cf128e32e209822
Jose Nino <jnino@lyft.com>
2019-09-04 13:49:35 -0700

    release
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/objective-c/EnvoyHTTPStreamImpl.m
+++ library/objective-c/EnvoyHTTPStreamImpl.m
@@ -155,6 +155,7 @@ static void ios_on_error(envoy_error error, void *context) {
     NSString *errorMessage = [[NSString alloc] initWithBytes:error.message.bytes
                                                       length:error.message.length
                                                     encoding:NSUTF8StringEncoding];
+    error.message.release(error.message.context);
     callbacks.onError(error.error_code, errorMessage);
   });
 }
