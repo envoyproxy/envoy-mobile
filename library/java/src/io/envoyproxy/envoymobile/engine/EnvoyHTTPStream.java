2a2774613a68e7b90e8c626e10c9a70da08100ab
Mike Schore <mike.schore@gmail.com>
2019-09-05 10:24:19 -0700

    jni: add support for on-heap buffers
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/java/src/io/envoyproxy/envoymobile/engine/EnvoyHTTPStream.java
+++ library/java/src/io/envoyproxy/envoymobile/engine/EnvoyHTTPStream.java
@@ -38,7 +38,13 @@ public class EnvoyHTTPStream {
    * @param endStream, supplies whether this is the last data in the streamHandle.
    */
   public void sendData(ByteBuffer data, boolean endStream) {
-    JniLibrary.sendData(streamHandle, data, endStream);
+    if (data.isDirect()) {
+      JniLibrary.sendData(streamHandle, data, endStream);
+    } else if (data.hasArray()) {
+      JniLibrary.sendData(streamHandle, data.array(), endStream);
+    } else {
+      throw new UnsupportedOperationException("Unsupported ByteBuffer implementation.");
+    }
   }
 
   /**
