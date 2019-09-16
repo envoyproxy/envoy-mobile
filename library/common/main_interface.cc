2656c0c23518a0c2f8dbe43d227d70194f720c33
Mike Schore <mike.schore@gmail.com>
2019-09-16 14:05:12 -0700

    fix format
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/common/main_interface.cc
+++ library/common/main_interface.cc
@@ -54,9 +54,7 @@ envoy_engine_t init_engine() {
   return 1;
 }
 
-envoy_status_t set_preferred_network(envoy_network_t) {
-  return ENVOY_SUCCESS;
-}
+envoy_status_t set_preferred_network(envoy_network_t) { return ENVOY_SUCCESS; }
 
 /*
  * Setup envoy for interaction via the main interface.
