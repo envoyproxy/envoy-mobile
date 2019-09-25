9f4af377b0538d1c59a081e4a955a9d40aeebcb9
Michael Rebello <me@michaelrebello.com>
2019-09-25 15:43:34 -0700

    ios: update preferred networks via reachability
    Adds an implementation of observing reachability on iOS which calls in to the function added in https://github.com/lyft/envoy-mobile/pull/443 to notify Envoy of a change in preferred network selection.
    
    Note that since the core maintains a singleton ref to the preferred network, these observations are also static and are not discarded. This should be addressed as part of implementing shutdown functionality in https://github.com/lyft/envoy-mobile/issues/445.
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>

--- library/objective-c/EnvoyEngineImpl.m
+++ library/objective-c/EnvoyEngineImpl.m
@@ -3,16 +3,22 @@
 #import "library/common/main_interface.h"
 #import "library/common/types/c_types.h"
 
+#import <SystemConfiguration/SystemConfiguration.h>
+
 @implementation EnvoyEngineImpl {
   envoy_engine_t _engineHandle;
 }
+
+#pragma mark - Public interface
 
 - (instancetype)init {
   self = [super init];
   if (!self) {
     return nil;
   }
+
   _engineHandle = init_engine();
+  _ensure_reachability_running();
   return self;
 }
 
@@ -41,6 +47,48 @@
 - (id<EnvoyHTTPStream>)startStreamWithCallbacks:(EnvoyHTTPCallbacks *)callbacks {
   return [[EnvoyHTTPStreamImpl alloc] initWithHandle:init_stream(_engineHandle)
                                            callbacks:callbacks];
+}
+
+#pragma mark - Reachability
+
+static SCNetworkReachabilityRef _reachability_ref;
+
+static void _reachability_callback(SCNetworkReachabilityRef target,
+                                   SCNetworkReachabilityFlags flags, void *info) {
+  if (flags == 0) {
+    return;
+  }
+
+  BOOL isUsingWWAN = flags & kSCNetworkReachabilityFlagsIsWWAN;
+  set_preferred_network(isUsingWWAN ? ENVOY_NET_WWAN : ENVOY_NET_WLAN);
+}
+
+static void _ensure_reachability_running() {
+  if (_reachability_ref) {
+    return;
+  }
+
+  NSString *name = @"io.envoymobile.reachability";
+  SCNetworkReachabilityRef reachability =
+      SCNetworkReachabilityCreateWithName(nil, [name UTF8String]);
+  if (!reachability) {
+    return;
+  }
+
+  _reachability_ref = reachability;
+
+  SCNetworkReachabilityContext context = {0, NULL, NULL, NULL, NULL};
+  if (!SCNetworkReachabilitySetCallback(_reachability_ref, _reachability_callback, &context)) {
+    return;
+  }
+
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  if (!SCNetworkReachabilitySetDispatchQueue(_reachability_ref, queue)) {
+    SCNetworkReachabilitySetCallback(_reachability_ref, NULL, NULL);
+    return;
+  }
+
+  return;
 }
 
 @end
