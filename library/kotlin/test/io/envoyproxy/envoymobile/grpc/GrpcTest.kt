bbfaea505e41fbe6d30f1ed6ddbea38b3639d15a
Alan Chiu <achiu@lyft.com>
2019-09-29 15:43:06 -0700

    fix
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/kotlin/test/io/envoyproxy/envoymobile/grpc/GrpcTest.kt
+++ library/kotlin/test/io/envoyproxy/envoymobile/grpc/GrpcTest.kt
@@ -1,7 +1,5 @@
 package io.envoyproxy.envoymobile.grpc
 
-import io.envoyproxy.envoymobile.Domain
-import io.envoyproxy.envoymobile.EnvoyClientBuilder
 import io.envoyproxy.envoymobile.EnvoyMobileTestGrpc
 import io.envoyproxy.envoymobile.EnvoyMobileTestOuterClass
 import io.grpc.ManagedChannelBuilder
@@ -15,7 +13,6 @@ import org.junit.Test
 class GrpcTest {
 
   private lateinit var server: Server
-  private val envoyClient = EnvoyClientBuilder(Domain("localhost:1234")).build()
 
   @Before
   fun setup() {
