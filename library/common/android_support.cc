fb62c7cd3aad15f7197bc10385422342887980e9
Alan Chiu <achiu@lyft.com>
2019-10-09 16:33:15 -0700

    fixme
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/common/android_support.cc
+++ library/common/android_support.cc
@@ -14,7 +14,7 @@ jint platform_setup(JavaVM* jvm, jobject connectivity_manager) {
 }
 
 void jni_log(const char* tag, const char* text) {
-// FIXME: https://github.com/lyft/envoy-mobile/issues/120
-// For now, this can be used for debugging purposes on Android
-//  __android_log_write(ANDROID_LOG_ERROR, tag, text);
+  // FIXME: https://github.com/lyft/envoy-mobile/issues/120
+  // For now, this can be used for debugging purposes on Android
+  //  __android_log_write(ANDROID_LOG_ERROR, tag, text);
 }
--- library/common/java_support.cc
+++ library/common/java_support.cc
@@ -4,4 +4,6 @@
 
 jint platform_setup(JavaVM*, jobject) { return 0; }
 
-void jni_log(const char* tag, const char* text) {}
+void jni_log(const char* tag, const char* text) {
+  // FIXME: https://github.com/lyft/envoy-mobile/issues/120
+}
