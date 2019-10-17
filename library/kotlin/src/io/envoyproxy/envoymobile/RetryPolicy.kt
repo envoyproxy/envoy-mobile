1228b52d16b87d4922f44b005e2ee4ec798e661d
Michael Rebello <me@michaelrebello.com>
2019-10-17 16:05:01 -0700

    CR
    Signed-off-by: Michael Rebello <me@michaelrebello.com>

--- library/kotlin/src/io/envoyproxy/envoymobile/RetryPolicy.kt
+++ library/kotlin/src/io/envoyproxy/envoymobile/RetryPolicy.kt
@@ -4,9 +4,9 @@ package io.envoyproxy.envoymobile
  * Specifies how a request may be retried, containing one or more rules.
  *
  * @param maxRetryCount Maximum number of retries that a request may be performed.
- * @param retryOn Whitelist of rules used for retrying. Must be <= `totalUpstreamTimeoutMS`
- * or it will be ignored.
+ * @param retryOn Whitelist of rules used for retrying.
  * @param perRetryTimeoutMs Timeout (in milliseconds) to apply to each retry.
+ * Must be <= `totalUpstreamTimeoutMS`.
  * @param totalUpstreamTimeoutMS Total timeout (in milliseconds) that includes all retries.
  * Spans the point at which the entire downstream request has been processed and when the
  * upstream response has been completely processed.
@@ -16,7 +16,13 @@ data class RetryPolicy(
   val retryOn: List<RetryRule>,
   val perRetryTimeoutMs: Long? = null,
   val totalUpstreamTimeoutMS: Long = 15000
-)
+) {
+  init {
+    if (perRetryTimeoutMs != null && perRetryTimeoutMs > totalUpstreamTimeoutMS) {
+      throw IllegalArgumentException("Per-retry timeout must be <= total timeout")
+    }
+  }
+}
 
 /**
  * These are retry rules specified in Envoy's router filter.
