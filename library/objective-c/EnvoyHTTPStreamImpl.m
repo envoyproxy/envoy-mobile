fa17acb8d8b74ae51b03c5c081229d655d967764
Mike Schore <mike.schore@gmail.com>
2019-11-08 10:42:05 -0800

    ios: make stream cleanup (more) thread-safe
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/objective-c/EnvoyHTTPStreamImpl.m
+++ library/objective-c/EnvoyHTTPStreamImpl.m
@@ -137,27 +137,25 @@ static void ios_on_complete(void *context) {
   EnvoyHTTPCallbacks *callbacks = c->callbacks;
   EnvoyHTTPStreamImpl *stream = c->stream;
   dispatch_async(callbacks.dispatchQueue, ^{
-    if (atomic_load(c->canceled)) {
-      return;
-    }
-
+    // FIXME: If the callback queue is not serial, clean up is not currently thread-safe.
     assert(stream);
     [stream cleanUp];
   });
 }
 
 static void ios_on_cancel(void *context) {
+  // This call is atomically gated at the call-site and will only happen once.
+
+  // The cancelation callback does not clean up the stream, since that will race with work Envoy's
+  // main thread may already be doing. Instead we rely on the reset that's dispatched to Envoy to
+  // effect cleanup, with appropriate timing.
   ios_context *c = (ios_context *)context;
   EnvoyHTTPCallbacks *callbacks = c->callbacks;
   EnvoyHTTPStreamImpl *stream = c->stream;
   dispatch_async(callbacks.dispatchQueue, ^{
-    // This call is atomically gated at the call-site and will only happen once.
     if (callbacks.onCancel) {
       callbacks.onCancel();
     }
-
-    assert(stream);
-    [stream cleanUp];
   });
 }
 
@@ -166,11 +164,7 @@ static void ios_on_error(envoy_error error, void *context) {
   EnvoyHTTPCallbacks *callbacks = c->callbacks;
   EnvoyHTTPStreamImpl *stream = c->stream;
   dispatch_async(callbacks.dispatchQueue, ^{
-    if (atomic_load(c->canceled)) {
-      return;
-    }
-
-    if (callbacks.onError) {
+    if (!atomic_load(c->canceled) && callbacks.onError) {
       NSString *errorMessage = [[NSString alloc] initWithBytes:error.message.bytes
                                                         length:error.message.length
                                                       encoding:NSUTF8StringEncoding];
@@ -178,6 +172,7 @@ static void ios_on_error(envoy_error error, void *context) {
       callbacks.onError(error.error_code, errorMessage);
     }
 
+    // FIXME: If the callback queue is not serial, clean up is not currently thread-safe.
     assert(stream);
     [stream cleanUp];
   });
