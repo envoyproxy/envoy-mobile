cd1ec533ea3097dd23ac90ca1e4a1678c91f3756
Mike Schore <mike.schore@gmail.com>
2020-02-13 13:52:44 -0800

    fix integration test and format
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/common/http/header_utility.cc
+++ library/common/http/header_utility.cc
@@ -25,7 +25,7 @@ RequestTrailerMapPtr toRequestTrailers(envoy_headers trailers) {
   RequestTrailerMapPtr transformed_trailers = std::make_unique<RequestTrailerMapImpl>();
   for (envoy_header_size_t i = 0; i < trailers.length; i++) {
     transformed_trailers->addCopy(LowerCaseString(convertToString(trailers.headers[i].key)),
-                                 convertToString(trailers.headers[i].value));
+                                  convertToString(trailers.headers[i].value));
   }
   // The C envoy_headers struct can be released now because the headers have been copied.
   release_envoy_headers(trailers);
