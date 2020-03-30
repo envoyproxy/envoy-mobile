a2e41f175fc2534d0d428c3508e43ed8022c71a9
Jose Nino <jnino@lyft.com>
2020-03-30 16:01:24 -0700

    a word
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/config_template.cc
+++ library/common/config_template.cc
@@ -76,7 +76,7 @@ static_resources:
           # n.b: with mobile clients there are scenarios where all concurrent requests might be
           # retries, e.g when the phone goes offline. Therefore, Envoy Mobile allows all concurrent
           # requests to be retries by setting the concurrent retry budget to 100%.
-          # This configuration uses the default for max concurrent retries (1024) because there is
+          # This configuration uses the default for max concurrent requests (1024) because there is
           # no reasonable scenario where a mobile client should have even close to that many concurrent
           # requests.
           retry_budget:
