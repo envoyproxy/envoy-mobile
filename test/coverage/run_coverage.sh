320c8e980de67372c9f2e02492a0834a8cd59f99
Jose Nino <jnino@lyft.com>
2020-04-07 12:49:33 -0700

    attempt
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/coverage/run_coverage.sh
+++ test/coverage/run_coverage.sh
@@ -3,6 +3,10 @@
 
 set -e
 
+export PATH=/usr/lib/llvm-9/bin:$PATH
+export CC=clang
+export CXX=clang++
+
 export REPOSITORY="@envoy"
 export EXTRA_QUERY_PATHS="//test/..."
 export BUILD_PATH="test/coverage/BUILD"
