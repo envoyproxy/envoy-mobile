aef8e25f0b8f5f8e1a0095db10d78b9eeee29810
Alan Chiu <achiu@lyft.com>
2020-04-22 14:28:07 -0700

    [sonatype] update max retries to be 500
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- dist/sonatype_nexus_upload.py
+++ dist/sonatype_nexus_upload.py
@@ -72,7 +72,7 @@ def _install_locally(version, files):
         shutil.copyfile(file, os.path.join(path, basename))
 
 
-def _urlopen_retried(request, max_retries=20, attempt=1, delay_sec=1):
+def _urlopen_retried(request, max_retries=500, attempt=1, delay_sec=1):
     """
     Retries a request via recursion. Retries happen with a default delay of 1 second. We do not exponentially back off.
     :param request: the request to be made
