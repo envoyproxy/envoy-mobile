6d726c1fcb46ba571b4018925aac34001820a531
Alan Chiu <achiu@lyft.com>
2020-05-14 13:30:10 -0700

    replace request with response
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/swift/src/ResponseHeadersBuilder.swift
+++ library/swift/src/ResponseHeadersBuilder.swift
@@ -3,9 +3,9 @@ import Foundation
 /// Builder used for constructing instances of `ResponseHeaders`.
 @objcMembers
 public final class ResponseHeadersBuilder: HeadersBuilder {
-  /// Build the request headers using the current builder.
+  /// Build the response headers using the current builder.
   ///
-  /// - returns: New instance of request headers.
+  /// - returns: New instance of response headers.
   public func build() -> ResponseHeaders {
     return ResponseHeaders(headers: self.headers)
   }
--- library/swift/src/ResponseTrailersBuilder.swift
+++ library/swift/src/ResponseTrailersBuilder.swift
@@ -3,9 +3,9 @@ import Foundation
 /// Builder used for constructing instances of `ResponseTrailers`.
 @objcMembers
 public final class ResponseTrailersBuilder: HeadersBuilder {
-  /// Build the request trailers using the current builder.
+  /// Build the response trailers using the current builder.
   ///
-  /// - returns: New instance of request trailers.
+  /// - returns: New instance of response trailers.
   public func build() -> ResponseTrailers {
     return ResponseTrailers(headers: self.headers)
   }
