fa3d99b4cc3d04366a680c6897a6821d02058ef9
Jose Nino <jnino@lyft.com>
2020-05-26 13:43:00 -0700

    compression: add response decompression
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/extensions/registry.h
+++ library/common/extensions/registry.h
@@ -8,6 +8,8 @@
 #include "extensions/filters/network/http_connection_manager/config.h"
 #include "extensions/stat_sinks/metrics_service/config.h"
 #include "extensions/transport_sockets/tls/config.h"
+#include "extensions/filters/http/decompressor/config.h"
+#include "extensions/compression/gzip/decompressor/config.h"
 
 namespace Envoy {
 class ExtensionRegistry {
