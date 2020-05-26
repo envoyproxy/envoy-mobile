c24094aa6565d9308272276c04d30835094ab06d
Jose Nino <jnino@lyft.com>
2020-05-26 14:27:23 -0700

    fmt
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/extensions/registry.h
+++ library/common/extensions/registry.h
@@ -3,13 +3,13 @@
 #include "common/upstream/logical_dns_cluster.h"
 
 #include "extensions/clusters/dynamic_forward_proxy/cluster.h"
+#include "extensions/compression/gzip/decompressor/config.h"
+#include "extensions/filters/http/decompressor/config.h"
 #include "extensions/filters/http/dynamic_forward_proxy/config.h"
 #include "extensions/filters/http/router/config.h"
 #include "extensions/filters/network/http_connection_manager/config.h"
 #include "extensions/stat_sinks/metrics_service/config.h"
 #include "extensions/transport_sockets/tls/config.h"
-#include "extensions/filters/http/decompressor/config.h"
-#include "extensions/compression/gzip/decompressor/config.h"
 
 namespace Envoy {
 class ExtensionRegistry {
