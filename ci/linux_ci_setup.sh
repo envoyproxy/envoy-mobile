446d8c6a1b2ea2fbc4b3af56dd38aa8f3e712db4
Alan Chiu <achiu@lyft.com>
2020-06-08 10:37:12 -0700

    again
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -4,24 +4,29 @@ set -e
 
 # Set up basic requirements and install them.
 # workaround https://askubuntu.com/questions/41605/trouble-downloading-packages-list-due-to-a-hash-sum-mismatch-error
+echo "Workaround for hash sum mismatch..."
 sudo rm -rf /var/lib/apt/lists/*
 
 # We have seen problems with heroku's keys.
 # We do not use heroku, but it is pre-installed in the github actions machines.
+echo "Getting heroku keys..."
 curl https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 
 # https://github.com/bazelbuild/bazel/issues/11470#issuecomment-633205152
+echo "Getting bazel-release gpg..."
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 
 sudo apt-get clean
 sudo apt-get update
 
+echo "Installing tools..."
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y wget software-properties-common make cmake git \
   unzip bc libtool ninja-build automake zip time \
   apt-transport-https
 
 # clang 8.
+echo "Installing clang 8..."
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb https://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main"
 sudo apt-get update
@@ -36,9 +41,11 @@ sudo apt-get install gnupg2
 gpg --version
 
 # buildifier
+echo "Installing buildifier..."
 sudo wget -O /usr/local/bin/buildifier https://github.com/bazelbuild/buildtools/releases/download/2.2.1/buildifier
 sudo chmod +x /usr/local/bin/buildifier
 
 # bazelisk
+echo "Installing bazelisk..."
 sudo wget -O /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v0.0.8/bazelisk-linux-amd64
 sudo chmod +x /usr/local/bin/bazel
