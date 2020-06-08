101a0d50f1180f7c19f646626abdcf214bc72f3b
Alan Chiu <achiu@lyft.com>
2020-06-08 12:36:44 -0700

    another
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -17,7 +17,8 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 sudo apt-get clean
-sudo apt-get update
+sudo apt-get update -y
+apt-get install mongodb-org -y
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y wget software-properties-common make cmake git \
