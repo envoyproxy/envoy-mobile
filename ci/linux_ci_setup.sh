d041a26eb0a343db143b58b0e05740ebce20eedf
Alan Chiu <achiu@lyft.com>
2020-06-08 12:29:11 -0700

    try mongo gpg
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -13,6 +13,7 @@ curl https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 # https://github.com/bazelbuild/bazel/issues/11470#issuecomment-633205152
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 sudo apt-get clean
 sudo apt-get update
 
