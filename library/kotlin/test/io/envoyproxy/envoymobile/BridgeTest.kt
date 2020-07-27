d0756cecfad6498eb82b30e547a09f17b2c6169f
Jose Nino <jnino@lyft.com>
2020-07-27 16:38:49 -0700

    test file
    Signed-off-by: Jose Nino <jnino@lyft.com>

---
+++ library/kotlin/test/io/envoyproxy/envoymobile/BridgeTest.kt
@@ -0,0 +1,78 @@
+package io.envoyproxy.envoymobile
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.Mockito.mock
+import java.util.concurrent.Executors
+
+
+private const val REQUEST_HANDLER_THREAD_NAME = "hello_envoy_kt"
+private const val ENVOY_SERVER_HEADER = "server"
+private const val REQUEST_AUTHORITY = "api.lyft.com"
+private const val REQUEST_PATH = "/ping"
+private const val REQUEST_SCHEME = "https"
+
+class BridgeTest {
+  private lateinit var clientBuilder: StreamClientBuilder
+
+  @Test
+  fun `first attempt`() {
+    val config =
+    """
+static_resources:
+  listeners:
+  - name: base_api_listener
+    address:
+      socket_address:
+        protocol: TCP
+        address: 0.0.0.0
+        port_value: 10000
+    api_listener:
+      api_listener:
+        "@type": type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager
+        stat_prefix: hcm
+        route_config:
+          name: api_router
+          virtual_hosts:
+            - name: api
+              domains:
+                - "*"
+              routes:
+                - match:
+                    prefix: "/"
+                  direct_response:
+                    status: 418
+        http_filters:
+          - name: envoy.filters.http.assertion
+            typed_config:
+              "@type": type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion
+              match_config:
+                http_request_headers_match:
+                  headers:
+                    - name: ":authority"
+                      exact_match: not_my_name
+          - name: envoy.router
+            typed_config:
+              "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+    """
+    clientBuilder = StreamClientBuilder(Custom(config))
+
+    clientBuilder.addLogLevel(LogLevel.DEBUG)
+    val streamClient = clientBuilder.build() as EnvoyClient
+
+    val requestHeaders = RequestHeadersBuilder(
+      RequestMethod.GET, REQUEST_SCHEME, REQUEST_AUTHORITY, REQUEST_PATH
+    )
+      .addUpstreamHttpProtocol(UpstreamHttpProtocol.HTTP2)
+      .build()
+    streamClient
+      .newStreamPrototype()
+      .setOnResponseHeaders { responseHeaders, _ ->
+        val status = responseHeaders.httpStatus ?: 0L
+        val message = "received headers with status $status"
+        assertThat(status).isEqualTo(200)
+      }
+      .start(Executors.newSingleThreadExecutor())
+      .sendHeaders(requestHeaders, true)
+  }
+}
