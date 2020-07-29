bc871da0846a6f6de7a4bc245f5766307b982fa3
Michael Rebello <me@michaelrebello.com>
2020-07-29 16:13:40 -0700

    platform: remove some interfaces
    At the moment, `continueRequest()` and `continueResponse()` are not yet wired up. While we plan to implement that in the very near future (next week or so), we do not have plans to add these other "async" operations at the moment. Removing them from the public interface until we're ready to implement them.
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>

--- library/swift/src/filters/RequestFilterCallbacks.swift
+++ library/swift/src/filters/RequestFilterCallbacks.swift
@@ -11,14 +11,4 @@ public protocol RequestFilterCallbacks {
   /// If the request is not complete, the filter will still receive `onData()`/`onTrailers()`
   /// calls.
   func continueRequest()
-
-  /// - returns: The currently buffered data as buffered by the filter or previous ones in the
-  ///            filter chain. Nil if nothing has been buffered yet.
-  func requestBuffer() -> Data?
-
-  /// Adds request trailers. May only be called in `onHeaders()`/`onData()` when
-  /// `endStream = true` in order to guarantee that the client will not send its own trailers.
-  ///
-  /// - parameter trailers: The trailers to add and pass to subsequent filters.
-  func addRequestTrailers(_ trailers: RequestTrailers)
 }
--- library/swift/src/filters/ResponseFilterCallbacks.swift
+++ library/swift/src/filters/ResponseFilterCallbacks.swift
@@ -11,14 +11,4 @@ public protocol ResponseFilterCallbacks {
   /// If the response is not complete, the filter will still receive `onData()`/`onTrailers()`
   /// calls.
   func continueResponse()
-
-  /// - returns: The currently buffered data as buffered by the filter or previous ones in the
-  ///            filter chain. Nil if nothing has been buffered yet.
-  func responseBuffer() -> Data?
-
-  /// Adds response trailers. May only be called in `onHeaders()`/`onData()` when
-  /// `endStream = true` in order to guarantee that the client will not send its own trailers.
-  ///
-  /// - parameter trailers: The trailers to add and pass to subsequent filters.
-  func addResponseTrailers(_ trailers: ResponseTrailers)
 }
