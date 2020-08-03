7f71fe512e505ae8ba15572aeb9e20d7290b3eda
Jingwei Hao <jingweih@lyft.com>
2020-08-03 13:42:43 -0700

    doc for Element
    Signed-off-by: Jingwei Hao <jingweih@lyft.com>

--- library/kotlin/src/io/envoyproxy/envoymobile/Element.kt
+++ library/kotlin/src/io/envoyproxy/envoymobile/Element.kt
@@ -2,6 +2,9 @@ package io.envoyproxy.envoymobile
 
 import java.util.regex.Pattern
 
+/**
+ * Element for stats.
+ */
 class Element(val element: String) {
     init {
         if (!Pattern.compile("^[A-Za-z_]+\$").matcher(element).matches()) {
