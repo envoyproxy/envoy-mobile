e7083453a33fd522988db16c719a6f1d8f88a5d3
Jose Nino <jnino@lyft.com>
2020-08-04 11:31:21 -0700

    comment
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/jni/jni_utility.h
+++ library/common/jni/jni_utility.h
@@ -8,4 +8,5 @@ jint platform_setup(JavaVM* jvm, jobject connectivity_manager);
 // Logging from JNI functions.
 void jni_log(const char* tag, const char* text);
 
+// Get JNIEnv.
 JNIEnv* get_env();
