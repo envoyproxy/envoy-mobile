dc73a5e7208c8134b03d2aa4289cbf7620194627
Mike Schore <mike.schore@gmail.com>
2020-08-13 05:08:22 +0800

    complex filters: add ResumeIteration status
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/kotlin/src/io/envoyproxy/envoymobile/filters/FilterTrailersStatus.kt
+++ library/kotlin/src/io/envoyproxy/envoymobile/filters/FilterTrailersStatus.kt
@@ -17,4 +17,14 @@ sealed class FilterTrailersStatus<T : Headers> {
    * is desired.
    */
   class StopIteration<T : Headers>(val trailers: T) : FilterTrailersStatus<T>()
+
+  /**
+   * Resume previously-stopped iteration, possibly forwarding headers and data, if iteration was
+   * previously stopped during an on*Headers or on*Data invocation.
+   *
+   * It is an error to return ResumeIteration if iteration is not currently stopped, and it is
+   * an error to include headers if headers have already been forwarded to the next filter
+   * (i.e. iteration was stopped during an on*Data invocation instead of on*Headers).
+   */
+  class ResumeIteration<T : Headers>(val headers: Headers?, val data: ByteBuffer?, val trailers: T) : FilterTrailersStatus<T>()
 }
