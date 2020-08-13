4c9950f256e958d467fac99a886b99472bde7959
Jose Nino <jnino@lyft.com>
2020-08-13 13:19:40 -0700

    fmt
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/engine.cc
+++ library/common/engine.cc
@@ -28,8 +28,8 @@ envoy_status_t Engine::run(const std::string config, const std::string log_level
       const std::string name = "envoy";
       const std::string config_flag = "--config-yaml";
       const std::string log_flag = "-l";
-      const char* envoy_argv[] = {name.c_str(), config_flag.c_str(),   config.c_str(),
-                            log_flag.c_str(),    log_level.c_str(), nullptr};
+      const char* envoy_argv[] = {name.c_str(),     config_flag.c_str(), config.c_str(),
+                                  log_flag.c_str(), log_level.c_str(),   nullptr};
 
       main_common_ = std::make_unique<MobileMainCommon>(5, envoy_argv);
       event_dispatcher_ = &main_common_->server()->dispatcher();
