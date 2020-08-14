86af2eb3a6c3ff6178c5af355e2f24ec3f95aa6d
Mike Schore <mike.schore@gmail.com>
2020-08-14 08:27:21 +0800

    further docstring updates
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/swift/src/filters/AsyncRequestFilter.swift
+++ library/swift/src/filters/AsyncRequestFilter.swift
@@ -2,9 +2,22 @@ import Foundation
 
 /// RequestFilter supporting asynchronous resumption.
 public protocol AsyncRequestFilter: RequestFilter {
+  /// Called by the filter manager once to initialize the filter callbacks that the filter should
+  /// use.
+  ///
+  /// - parameter callbacks: The callbacks for this filter to use to interact with the chain.
+  func setRequestFilterCallbacks(_ callbacks: RequestFilterCallbacks)
+
   /// Invoked explicitly in response to an asynchronous resumeRequest() callback when filter
   /// iteration has been stopped. The parameters passed to this invocation will be a snapshot
   /// of any stream state that has not yet been forwarded along the filter chain.
+  ///
+  /// As with other filter invocations, this will be called on Envoy's main thread, and thus
+  /// no additional synchronization is required between this and other invocations.
+  ///
+  /// - param headers: Headers, if `StopIteration` was returned from `onResponseHeaders`.
+  /// - param data: Any data that has been buffered where `StopIterationAndBuffer` was returned.
+  /// - param trailers: Trailers, if `StopIteration` was returned from `onReponseTrailers`. 
   ///
   /// - return: The resumption status including any HTTP entities that will be forwarded.
   func onResumeRequest(
@@ -13,10 +26,4 @@ public protocol AsyncRequestFilter: RequestFilter {
     trailers: RequestTrailers?,
     endStream: Bool
   ) -> FilterResumeStatus<RequestHeaders, RequestTrailers>
-
-  /// Called by the filter manager once to initialize the filter callbacks that the filter should
-  /// use.
-  ///
-  /// - parameter callbacks: The callbacks for this filter to use to interact with the chain.
-  func setRequestFilterCallbacks(_ callbacks: RequestFilterCallbacks)
 }
