736792be9262f6d7defc4dbc12fcbcf09472b3a1
Rafal Augustyniak <raugustyniak@lyft.com>
2020-08-26 17:06:01 -0700

    fix
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

---
+++ library/swift/test/CounterTests.swift
@@ -0,0 +1,18 @@
+import Envoy
+import Foundation
+import XCTest
+
+final class CounterTests: XCTestCase {
+    func testConvenientMethodDelegatesToTheMainMethod() {
+        struct MockCounterImpl: Counter {
+            var wasIncremented = false
+            func increment(count: Int) {
+                self.wasIncremented = true
+            }
+        }
+
+        let counter = MockCounterImpl()
+        counter.increment()
+        XCTAssertTrue(counter.wasIncremented)
+    }
+}
