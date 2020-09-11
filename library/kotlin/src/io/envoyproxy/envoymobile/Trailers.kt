8b9b186bb5809bd87548b373dbf1b2e4d2d7c30c
Mike Schore <mike.schore@gmail.com>
2020-09-11 09:22:36 +0800

    platform: add Trailers type for better classification
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

---
+++ library/kotlin/src/io/envoyproxy/envoymobile/Trailers.kt
@@ -0,0 +1,15 @@
+package io.envoyproxy.envoymobile
+
+/*
+ * Base class representing trailers data structures.
+ * To instantiate new instances see `{Request|Response}TrailersBuilder`.
+ */
+@Suppress("EmptyClassBlock")
+open class Trailers : Headers {
+  /**
+   * Internal constructor used by builders.
+   *
+   * @param trailers: Trailers to set.
+   */
+  protected constructor(trailers: Map<String, List<String>>) : super(trailers)
+}
