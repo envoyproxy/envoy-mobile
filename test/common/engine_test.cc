7c7fe54245e2d20fb69eaa87e7a29f88a0df8be5
Michael Rebello <me@michaelrebello.com>
2020-09-16 09:41:11 -0700

    Update engine_test.cc
    Signed-off-by: Michael Rebello <me@michaelrebello.com>

--- test/common/engine_test.cc
+++ test/common/engine_test.cc
@@ -8,8 +8,8 @@ namespace Envoy {
 class EngineTest : public testing::Test {};
 
 typedef struct {
-  absl::Notification on_engine_running_;
-  absl::Notification on_exit_;
+  absl::Notification on_engine_running;
+  absl::Notification on_exit;
 } engine_test_context;
 
 TEST_F(EngineTest, EarlyExit) {
@@ -33,19 +33,19 @@ TEST_F(EngineTest, EarlyExit) {
   envoy_engine_callbacks callbacks{[](void* context) -> void {
                                      auto* engine_running =
                                          static_cast<engine_test_context*>(context);
-                                     engine_running->on_engine_running_.Notify();
+                                     engine_running->on_engine_running.Notify();
                                    } /*on_engine_running*/,
                                    [](void* context) -> void {
                                      auto* exit = static_cast<engine_test_context*>(context);
-                                     exit->on_exit_.Notify();
+                                     exit->on_exit.Notify();
                                    } /*on_exit*/,
                                    &test_context /*context*/};
 
   run_engine(0, callbacks, config.c_str(), level.c_str());
-  ASSERT_TRUE(test_context.on_engine_running_.WaitForNotificationWithTimeout(absl::Seconds(3)));
+  ASSERT_TRUE(test_context.on_engine_running.WaitForNotificationWithTimeout(absl::Seconds(3)));
 
   terminate_engine(0);
-  ASSERT_TRUE(test_context.on_exit_.WaitForNotificationWithTimeout(absl::Seconds(3)));
+  ASSERT_TRUE(test_context.on_exit.WaitForNotificationWithTimeout(absl::Seconds(3)));
 
   start_stream(0, {});
 }
