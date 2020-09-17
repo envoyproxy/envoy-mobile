62669c930a20c1586d58e3f01a1f556d330fed10
Jose Nino <jnino@lyft.com>
2020-09-16 17:43:41 -0700

    metadata
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/common/main_interface_test.cc
+++ test/common/main_interface_test.cc
@@ -89,9 +89,64 @@ TEST_F(MainInterfaceTest, BasicStream) {
   ASSERT_TRUE(engine_cbs_context.on_exit.WaitForNotificationWithTimeout(absl::Seconds(10)));
 }
 
+TEST_F(MainInterfaceTest, SendMetadata) {
+  // This config is the mininimal envoy mobile config that allows for running the engine.
+  // There is nothing functional about the config, as the created stream is only used for
+  // send_metadata.
+  const std::string config =
+      "{\"admin\":{},\"static_resources\":{\"listeners\":[{\"name\":\"base_api_listener\","
+      "\"address\":{\"socket_address\":{\"protocol\":\"TCP\",\"address\":\"0.0.0.0\",\"port_"
+      "value\":10000}},\"api_listener\":{\"api_listener\":{\"@type\":\"type.googleapis.com/"
+      "envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager\",\"stat_"
+      "prefix\":\"hcm\",\"route_config\":{\"name\":\"api_router\",\"virtual_hosts\":[{\"name\":"
+      "\"api\",\"include_attempt_count_in_response\":true,\"domains\":[\"*\"],\"routes\":[{"
+      "\"match\":{\"prefix\":\"/"
+      "\"},\"route\":{\"cluster_header\":\"x-envoy-mobile-cluster\",\"retry_policy\":{\"retry_back_"
+      "off\":{\"base_interval\":\"0.25s\",\"max_interval\":\"60s\"}}}}]}]},\"http_filters\":[{"
+      "\"name\":\"envoy.router\",\"typed_config\":{\"@type\":\"type.googleapis.com/"
+      "envoy.extensions.filters.http.router.v3.Router\"}}]}}}]},\"layered_runtime\":{\"layers\":[{"
+      "\"name\":\"static_layer_0\",\"static_layer\":{\"overload\":{\"global_downstream_max_"
+      "connections\":50000}}}]}}";
+  const std::string level = "debug";
+  engine_test_context engine_cbs_context{};
+  envoy_engine_callbacks engine_cbs{[](void* context) -> void {
+                                      auto* engine_running =
+                                          static_cast<engine_test_context*>(context);
+                                      engine_running->on_engine_running.Notify();
+                                    } /*on_engine_running*/,
+                                    [](void* context) -> void {
+                                      auto* exit = static_cast<engine_test_context*>(context);
+                                      exit->on_exit.Notify();
+                                    } /*on_exit*/,
+                                    &engine_cbs_context /*context*/};
+  run_engine(0, engine_cbs, config.c_str(), level.c_str());
+
+  ASSERT_TRUE(
+      engine_cbs_context.on_engine_running.WaitForNotificationWithTimeout(absl::Seconds(10)));
+
+  envoy_http_callbacks stream_cbs{[](envoy_headers, bool, void*) -> void* { return nullptr; },
+                                  nullptr /* on_headers */,
+                                  nullptr /* on_data */,
+                                  nullptr /* on_metadata */,
+                                  nullptr /* on_trailers */,
+                                  nullptr /* on_complete */,
+                                  nullptr /* on_cancel */,
+                                  nullptr /* context */};
+
+  envoy_stream_t stream = init_stream(0);
+
+  start_stream(stream, stream_cbs);
+
+  EXPECT_EQ(ENVOY_FAILURE, send_metadata(stream, {}));
+
+  terminate_engine(0);
+
+  ASSERT_TRUE(engine_cbs_context.on_exit.WaitForNotificationWithTimeout(absl::Seconds(10)));
+}
+
 TEST_F(MainInterfaceTest, ResetStream) {
   // This config is the mininimal envoy mobile config that allows for running the engine.
-  // There is nothing functional about the config, as the created stream is immidiately reset.
+  // There is nothing functional about the config, as the created stream is immediately reset.
   const std::string config =
       "{\"admin\":{},\"static_resources\":{\"listeners\":[{\"name\":\"base_api_listener\","
       "\"address\":{\"socket_address\":{\"protocol\":\"TCP\",\"address\":\"0.0.0.0\",\"port_"
