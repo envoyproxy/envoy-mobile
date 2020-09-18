29466ab9fc1114496fa813d8212773f4d8779bc2
Jose Nino <jnino@lyft.com>
2020-09-18 11:57:41 -0700

    fmt
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/common/http/dispatcher_test.cc
+++ test/common/http/dispatcher_test.cc
@@ -1360,7 +1360,8 @@ TEST_F(DispatcherTest, Encode100Continue) {
 
   // Encode 100 continue should blow up.
   TestResponseHeaderMapImpl response_headers{{":status", "200"}};
-  EXPECT_DEATH(response_encoder_->encode100ContinueHeaders(response_headers), "panic: not implemented");
+  EXPECT_DEATH(response_encoder_->encode100ContinueHeaders(response_headers),
+               "panic: not implemented");
 }
 
 TEST_F(DispatcherTest, EncodeMetadata) {
