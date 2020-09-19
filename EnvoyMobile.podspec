envoyproxy/envoy

commit 593bc9e3f352d7150c76bf1004516a3463279d5c

Reduce binary size and build time with split DWARF (#12569)
When envoy build with compilation modes `opt` and `dbg`, the binary size is largely increased due to huge amounts of debug information. By splitting the debug information into two parts at compile time, we can reduce the total size of the object files processed by the linker. This can reduce the `envoy-static` target size from ~1211MB to ~392MB in compilation mode `opt`. Since those two modes are used in the CI scripts, this PR should also improve CI performance.
Risk Level: low
Testing: N/A
Docs Changes: N/A
Release Notes: Updated release notes for new changes to compilation mechanisms. 

Signed-off-by: Muge Chen <mugechen@google.com>

.bazelrc
@@ -23,6 +23,8 @@ build --enable_platform_specific_config
 build:linux --copt=-fPIC
 build:linux --cxxopt=-std=c++17
 build:linux --conlyopt=-fexceptions
+build:linux --fission=dbg,opt
+build:linux --features=per_object_debug_info
 
 # We already have absl in the build, define absl=1 to tell googletest to use absl for backtrace.
 build --define absl=1

bazel/envoy_internal.bzl
@@ -42,9 +42,9 @@ def envoy_copts(repository, test = False):
                "//conditions:default": posix_options,
            }) + select({
                # Bazel adds an implicit -DNDEBUG for opt.
-               repository + "//bazel:opt_build": [] if test else ["-ggdb3"],
+               repository + "//bazel:opt_build": [] if test else ["-ggdb3", "-gsplit-dwarf"],
                repository + "//bazel:fastbuild_build": [],
-               repository + "//bazel:dbg_build": ["-ggdb3"],
+               repository + "//bazel:dbg_build": ["-ggdb3", "-gsplit-dwarf"],
                repository + "//bazel:windows_opt_build": [],
                repository + "//bazel:windows_fastbuild_build": [],
                repository + "//bazel:windows_dbg_build": [],

ci/Dockerfile-envoy-alpine-debug
@@ -2,6 +2,7 @@ FROM frolvlad/alpine-glibc:alpine-3.12_glibc-2.31
 RUN mkdir -p /etc/envoy
 
 ADD linux/amd64/build_release/envoy /usr/local/bin/envoy
+ADD linux/amd64/build_release/envoy.dwp /usr/local/bin/envoy.dwp
 ADD configs/google_com_proxy.v2.yaml /etc/envoy/envoy.yaml
 RUN apk add --no-cache shadow su-exec \
         && addgroup -S envoy && adduser --no-create-home -S envoy -G envoy

ci/build_setup.sh
@@ -9,6 +9,7 @@ export PPROF_PATH=/thirdparty_build/bin/pprof
 [ -z "${NUM_CPUS}" ] && NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
 [ -z "${ENVOY_SRCDIR}" ] && export ENVOY_SRCDIR=/source
 [ -z "${ENVOY_BUILD_TARGET}" ] && export ENVOY_BUILD_TARGET=//source/exe:envoy-static
+[ -z "${ENVOY_BUILD_DEBUG_INFORMATION}" ] && export ENVOY_BUILD_DEBUG_INFORMATION=//source/exe:envoy-static.dwp
 [ -z "${ENVOY_BUILD_ARCH}" ] && export ENVOY_BUILD_ARCH=$(uname -m)
 echo "ENVOY_SRCDIR=${ENVOY_SRCDIR}"
 echo "ENVOY_BUILD_TARGET=${ENVOY_BUILD_TARGET}"

ci/do_ci.sh
@@ -60,12 +60,24 @@ function cp_binary_for_outside_access() {
     "${ENVOY_DELIVERY_DIR}"/"${DELIVERY_LOCATION}"
 }
 
+function cp_debug_info_for_outside_access() {
+  DELIVERY_LOCATION="$1"
+  cp -f \
+    bazel-bin/"${ENVOY_BIN}".dwp \
+    "${ENVOY_DELIVERY_DIR}"/"${DELIVERY_LOCATION}".dwp
+}
+
+
 function cp_binary_for_image_build() {
   # TODO(mattklein123): Replace this with caching and a different job which creates images.
   local BASE_TARGET_DIR="${ENVOY_SRCDIR}${BUILD_ARCH_DIR}"
   echo "Copying binary for image build..."
+  COMPILE_TYPE="$2"
   mkdir -p "${BASE_TARGET_DIR}"/build_"$1"
   cp -f "${ENVOY_DELIVERY_DIR}"/envoy "${BASE_TARGET_DIR}"/build_"$1"
+  if [[ "${COMPILE_TYPE}" == "dbg" || "${COMPILE_TYPE}" == "opt" ]]; then
+    cp -f "${ENVOY_DELIVERY_DIR}"/envoy.dwp "${BASE_TARGET_DIR}"/build_"$1"
+  fi
   mkdir -p "${BASE_TARGET_DIR}"/build_"$1"_stripped
   strip "${ENVOY_DELIVERY_DIR}"/envoy -o "${BASE_TARGET_DIR}"/build_"$1"_stripped/envoy
 
@@ -106,7 +118,16 @@ function bazel_binary_build() {
   # container.
   cp_binary_for_outside_access envoy
 
-  cp_binary_for_image_build "${BINARY_TYPE}"
+  if [[ "${COMPILE_TYPE}" == "dbg" || "${COMPILE_TYPE}" == "opt" ]]; then
+    # Generate dwp file for debugging since we used split DWARF to reduce binary
+    # size
+    bazel build ${BAZEL_BUILD_OPTIONS} -c "${COMPILE_TYPE}" "${ENVOY_BUILD_DEBUG_INFORMATION}" ${CONFIG_ARGS}
+    # Copy the debug information
+    cp_debug_info_for_outside_access envoy
+  fi
+
+  cp_binary_for_image_build "${BINARY_TYPE}" "${COMPILE_TYPE}"
+
 }
 
 CI_TARGET=$1

envoyproxy/envoy-mobile

commit d1b991ec7266dd8666c876dbb9b40228a1718e81

android: fix release builds (#1112)
This adds an argument to counteract build failures we're seeing on Linux Android release builds, believed to be caused in part by https://github.com/envoyproxy/envoy/pull/12569/files#diff-f55b2f358053ad76fb5ac3f776f78ef8R27.

Verified fix with release build: https://github.com/lyft/envoy-mobile/pull/1112/checks?check_run_id=1135915527

I'm also bumping the release in this PR so we can re-tag with a version of the Android binary successfully.

Signed-off-by: Michael Rebello <me@michaelrebello.com>

.bazelrc
@@ -23,9 +23,11 @@ build --define disable_known_issue_asserts=true
 build --define cxxopts=-std=c++17
 # https://github.com/bazelbuild/bazel/issues/10674#issuecomment-658208918
 build --incompatible_objc_compile_info_migration
+# Unset per_object_debug_info. Causes failures on Android Linux release builds.
+build --features=-per_object_debug_info
 
 # Default flags for builds targeting iOS
-# Manual Stamping is necessary in order to get versioning information in the ios
+# Manual stamping is necessary in order to get versioning information in the iOS
 # static framework.
 # More information on stamping can be found here:
 # https://github.com/envoyproxy/envoy/tree/master/bazel#enabling-optional-features

EnvoyMobile.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name = 'EnvoyMobile'
-    s.version = '0.3.1.09182020'
+    s.version = '0.3.1.09192020'
     s.author = 'Lyft, Inc.'
     s.summary = 'Client networking libraries based on the Envoy project'
     s.homepage = 'https://envoy-mobile.github.io'

VERSION
@@ -1,1 +1,1 @@
-0.3.1.09182020
+0.3.1.09192020