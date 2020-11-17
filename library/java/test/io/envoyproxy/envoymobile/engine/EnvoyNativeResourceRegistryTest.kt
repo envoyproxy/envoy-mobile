0828935521b28c8619d864b2a12bb26f681b40f0
Mike Schore <mike.schore@gmail.com>
2020-11-17 20:22:49 +0800

    working test
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/java/test/io/envoyproxy/envoymobile/engine/EnvoyNativeResourceRegistryTest.kt
+++ library/java/test/io/envoyproxy/envoymobile/engine/EnvoyNativeResourceRegistryTest.kt
@@ -8,14 +8,13 @@ import org.junit.Test
 
 class EnvoyNativeResourceRegistryTest {
 
-
   @Test
   fun `release callbacks are invoked when EnvoyNativeResourceWrappers are flagged as unreachable`() {
     val latch = CountDownLatch(1)
     val testHandle: Long = 77
-    run {
-      // Wrapper is within scope function and will become unreachable.
-      val testResourceWrapper = object : EnvoyNativeResourceWrapper {}
+    // Force Wrapper out of scope so the GC picks it up. ¯\_(ツ)_/¯
+    for (i in 0..1 step 2) {
+      var testResourceWrapper: EnvoyNativeResourceWrapper? = object : EnvoyNativeResourceWrapper {}
       val testResourceReleaser = object : EnvoyNativeResourceReleaser {
         override fun release(nativeHandle: Long) {
           assertThat(nativeHandle).isEqualTo(testHandle)
@@ -25,23 +24,18 @@ class EnvoyNativeResourceRegistryTest {
       EnvoyNativeResourceRegistry.globalRegister(testResourceWrapper, testHandle, testResourceReleaser)
     }
 
-    var maxIterations = 10000;
-    while (!latch.await(10, TimeUnit.MILLISECONDS)) {
-      System.runFinalization()
-      System.gc()
-      maxIterations -= 1
-      assertThat(maxIterations > 0).isTrue()
-    }
-    //assertThat(latch.await(2000, TimeUnit.MILLISECONDS)).isTrue()
+    System.runFinalization()
+    System.gc()
+    assertThat(latch.await(2000, TimeUnit.MILLISECONDS)).isTrue()
   }
 
   @Test
   fun `release callbakcs are not invoked when EnvoyNativeResourceWrappers remain reachable`() {
     val latch = CountDownLatch(1)
     val testHandle: Long = 77
-    // Wrapper is outside scope function and will remain reachable.
+    // Wrapper will remain reachable.
     val testResourceWrapper = object : EnvoyNativeResourceWrapper {}
-    run {
+    for (i in 0..1 step 2) {
       val testResourceReleaser = object : EnvoyNativeResourceReleaser {
         override fun release(nativeHandle: Long) {
           // Should be not called.
