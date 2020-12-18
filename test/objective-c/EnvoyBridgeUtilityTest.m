5a3e117fb4dbf6b8b03c4588ad3603b287cdec2a
Mike Schore <mike.schore@gmail.com>
2020-12-19 04:40:21 +0800

    test: set up objc unit tests
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

---
+++ test/objective-c/EnvoyBridgeUtilityTest.m
@@ -0,0 +1,20 @@
+#import <XCTest/XCTest.h>
+
+@interface EnvoyBridgeUtilityTest : XCTestCase
+@end
+
+@implementation EnvoyBridgeUtilityTest
+
+- (void)setUp {
+  [super setUp];
+}
+
+- (void)tearDown {
+  [super tearDown];
+}
+
+- (void)testExample {
+  XCTFail(@"This should fail");
+}
+
+@end
