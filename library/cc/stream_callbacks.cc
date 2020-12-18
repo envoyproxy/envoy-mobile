c769cb86d722ba8db0bd79fea950ab4d44ffb6e5
Cerek Hillen <chillen@lyft.com>
2020-12-18 14:01:49 -0800

    impl stream
    Signed-off-by: Cerek Hillen <chillen@lyft.com>

---
+++ library/cc/stream_callbacks.cc
@@ -0,0 +1,100 @@
+#include "stream_callbacks.h"
+
+#include "bridge_utility.h"
+#include "response_headers_builder.h"
+#include "response_trailers_builder.h"
+
+namespace Envoy {
+namespace Platform {
+
+EnvoyHttpCallbacksAdapter::EnvoyHttpCallbacksAdapter(ExecutorSharedPtr executor,
+                                                     StreamCallbacksSharedPtr callbacks)
+    : executor_(executor), stream_callbacks_(callbacks) {}
+
+envoy_http_callbacks EnvoyHttpCallbacksAdapter::as_envoy_http_callbacks() {
+  envoy_http_callbacks callbacks{
+      .on_headers = &EnvoyHttpCallbacksAdapter::dispatch_on_headers,
+      .on_data = &EnvoyHttpCallbacksAdapter::dispatch_on_data,
+      // on_metadata is not used
+      .on_trailers = &EnvoyHttpCallbacksAdapter::dispatch_on_trailers,
+      .on_error = &EnvoyHttpCallbacksAdapter::dispatch_on_error,
+      .on_complete = &EnvoyHttpCallbacksAdapter::dispatch_on_complete,
+      .on_cancel = &EnvoyHttpCallbacksAdapter::dispatch_on_cancel,
+      .context = this,
+  };
+  return callbacks;
+}
+
+void* EnvoyHttpCallbacksAdapter::dispatch_on_headers(envoy_headers headers, bool end_stream,
+                                                     void* context) {
+  auto self = static_cast<EnvoyHttpCallbacksAdapter*>(context);
+  if (self->stream_callbacks_->on_headers.has_value()) {
+    auto raw_headers = envoy_headers_as_raw_headers(headers);
+    ResponseHeadersBuilder builder;
+    for (const auto& pair : raw_headers) {
+      if (pair.first == ":status") {
+        builder.add_http_status(std::stoi(pair.second[0]));
+      }
+      builder.set(pair.first, pair.second);
+    }
+    self->executor_->execute(
+        [=]() { self->stream_callbacks_->on_headers.value()(builder.build(), end_stream); });
+  }
+  return context;
+}
+
+void* EnvoyHttpCallbacksAdapter::dispatch_on_data(envoy_data data, bool end_stream, void* context) {
+  auto self = static_cast<EnvoyHttpCallbacksAdapter*>(context);
+  if (self->stream_callbacks_->on_error.has_value()) {
+    auto buffer = envoy_data_as_buffer(data);
+    self->executor_->execute(
+        [=]() { self->stream_callbacks_->on_data.value()(buffer, end_stream); });
+  }
+  return context;
+}
+
+void* EnvoyHttpCallbacksAdapter::dispatch_on_trailers(envoy_headers metadata, void* context) {
+  auto self = static_cast<EnvoyHttpCallbacksAdapter*>(context);
+  if (self->stream_callbacks_->on_trailers.has_value()) {
+    auto raw_headers = envoy_headers_as_raw_headers(metadata);
+    ResponseTrailersBuilder builder;
+    for (const auto& pair : raw_headers) {
+      builder.set(pair.first, pair.second);
+    }
+    self->executor_->execute(
+        [=]() { self->stream_callbacks_->on_trailers.value()(builder.build()); });
+  }
+  return context;
+}
+
+void* EnvoyHttpCallbacksAdapter::dispatch_on_error(envoy_error raw_error, void* context) {
+  auto self = static_cast<EnvoyHttpCallbacksAdapter*>(context);
+  if (self->stream_callbacks_->on_error.has_value()) {
+    EnvoyErrorSharedPtr error = std::make_shared<EnvoyError>();
+    error->error_code = raw_error.error_code;
+    error->message = envoy_data_as_string(raw_error.message);
+    error->attempt_count = absl::optional<int>(raw_error.attempt_count);
+
+    self->executor_->execute([=]() { self->stream_callbacks_->on_error.value()(error); });
+  }
+  return context;
+}
+
+void* EnvoyHttpCallbacksAdapter::dispatch_on_complete(void* context) {
+  auto self = static_cast<EnvoyHttpCallbacksAdapter*>(context);
+  if (self->stream_callbacks_->on_complete.has_value()) {
+    self->executor_->execute(self->stream_callbacks_->on_complete.value());
+  }
+  return context;
+}
+
+void* EnvoyHttpCallbacksAdapter::dispatch_on_cancel(void* context) {
+  auto self = static_cast<EnvoyHttpCallbacksAdapter*>(context);
+  if (self->stream_callbacks_->on_cancel.has_value()) {
+    self->executor_->execute(self->stream_callbacks_->on_cancel.value());
+  }
+  return context;
+}
+
+} // namespace Platform
+} // namespace Envoy
