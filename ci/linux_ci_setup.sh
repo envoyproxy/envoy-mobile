643b499c30e261fe647501d5705724175d251193
Jingwei Hao <jingwei.hao@gmail.com>
2021-01-07 19:53:17 -0800

    clang
    Signed-off-by: Jingwei Hao <jingwei.hao@gmail.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+function download_and_check () {
+  local to=$1
+  local url=$2
+  local sha256=$3
+
+  curl -fsSL --output "${to}" "${url}"
+  echo "${sha256}  ${to}" | sha256sum --check
+}
+
 # Set up basic requirements and install them.
 # workaround https://askubuntu.com/questions/41605/trouble-downloading-packages-list-due-to-a-hash-sum-mismatch-error
 sudo rm -rf /var/lib/apt/lists/*
@@ -25,12 +34,23 @@ sudo apt-get install -y wget software-properties-common make cmake git \
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb https://apt.llvm.org/xenial/ llvm-toolchain-xenial-10 main"
 sudo apt-get update
-sudo apt-get install -y clang-10 lld-10 libc++-10-dev libc++abi-10-dev
+sudo apt-get install -y lld-10 libc++-10-dev libc++abi-10-dev
+
+LLVM_VERSION="10.0.0"
+LLVM_DISTRO="x86_64-linux-gnu-ubuntu-18.04"
+LLVM_RELEASE="clang+llvm-${LLVM_VERSION}-${LLVM_DISTRO}"
+LLVM_SHA256SUM="b25f592a0c00686f03e3b7db68ca6dc87418f681f4ead4df4745a01d9be63843"
+LLVM_DOWNLOAD_PREFIX=${LLVM_DOWNLOAD_PREFIX:-https://github.com/llvm/llvm-project/releases/download/llvmorg-}
+download_and_check "${LLVM_RELEASE}.tar.xz" "${LLVM_DOWNLOAD_PREFIX}${LLVM_VERSION}/${LLVM_RELEASE}.tar.xz" "${LLVM_SHA256SUM}"
+tar Jxf "${LLVM_RELEASE}.tar.xz"
+mv "./${LLVM_RELEASE}" /opt/llvm
+sudo chown -R root:root /opt/llvm
+rm "./${LLVM_RELEASE}.tar.xz"
 
 sudo update-alternatives --remove-all clang
 sudo update-alternatives --remove-all clang++
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 100
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 100
+sudo update-alternatives --install /usr/bin/clang clang /opt/llvm/bin/clang-10 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /opt/llvm/bin/clang++ 100
 
 sudo apt-get install gnupg2
 gpg --version
