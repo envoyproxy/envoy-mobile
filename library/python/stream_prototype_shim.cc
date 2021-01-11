48fb7229e98ef6b6970a64c01b9357f8434c541d
Cerek Hillen <chillen@lyft.com>
2021-01-11 09:30:59 -0500

    move closure defn to a template
    Signed-off-by: Cerek Hillen <chillen@lyft.com>

--- library/python/stream_prototype_shim.cc
+++ library/python/stream_prototype_shim.cc
@@ -6,15 +6,20 @@ namespace Envoy {
 namespace Python {
 namespace StreamPrototype {
 
-Platform::StreamPrototype& set_on_headers_shim(Platform::StreamPrototype self,
-                                               Platform::OnHeadersCallback closure) {
-  return self.set_on_headers(
-      [closure](Platform::ResponseHeadersSharedPtr headers, bool end_stream) {
-        py::gil_scoped_acquire acquire;
-        closure(std::move(headers), end_stream);
-      });
+template <typename... Ts> std::function<void(Ts...)> make_shim(std::function<void(Ts...)> closure) {
+  return [closure](Ts... args) {
+    py::gil_scoped_acquire acquire;
+    closure(std::forward<Ts>(args)...);
+  };
 }
 
+Platform::StreamPrototype& set_on_headers_shim(Platform::StreamPrototype& self,
+                                               Platform::OnHeadersCallback closure) {
+  return self.set_on_headers(make_shim(closure));
+}
+
+// set_on_data_shim can't be represented by the make_shim above, because it
+// also constructs a bytes view onto the envoy_data provided.
 Platform::StreamPrototype& set_on_data_shim(Platform::StreamPrototype& self,
                                             OnPyBytesDataCallback closure) {
   return self.set_on_data([closure](envoy_data data, bool end_stream) {
@@ -24,36 +29,24 @@ Platform::StreamPrototype& set_on_data_shim(Platform::StreamPrototype& self,
   });
 }
 
-Platform::StreamPrototype& set_on_trailers_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_trailers_shim(Platform::StreamPrototype& self,
                                                 Platform::OnTrailersCallback closure) {
-  return self.set_on_trailers([closure](Platform::ResponseTrailersSharedPtr trailers) {
-    py::gil_scoped_acquire acquire;
-    closure(std::move(trailers));
-  });
+  return self.set_on_trailers(make_shim(closure));
 }
 
-Platform::StreamPrototype& set_on_error_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_error_shim(Platform::StreamPrototype& self,
                                              Platform::OnErrorCallback closure) {
-  return self.set_on_error([closure](Platform::EnvoyErrorSharedPtr error) {
-    py::gil_scoped_acquire acquire;
-    closure(std::move(error));
-  });
+  return self.set_on_error(make_shim(closure));
 }
 
-Platform::StreamPrototype& set_on_complete_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_complete_shim(Platform::StreamPrototype& self,
                                                 Platform::OnCompleteCallback closure) {
-  return self.set_on_complete([closure]() {
-    py::gil_scoped_acquire acquire;
-    closure();
-  });
+  return self.set_on_complete(make_shim(closure));
 }
 
-Platform::StreamPrototype& set_on_cancel_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_cancel_shim(Platform::StreamPrototype& self,
                                               Platform::OnCancelCallback closure) {
-  return self.set_on_cancel([closure]() {
-    py::gil_scoped_acquire acquire;
-    closure();
-  });
+  return self.set_on_cancel(make_shim(closure));
 }
 
 } // namespace StreamPrototype
--- library/python/stream_prototype_shim.h
+++ library/python/stream_prototype_shim.h
@@ -16,22 +16,22 @@ using OnPyBytesDataCallback = std::function<void(py::bytes bytes, bool end_strea
 
 // each of these shims exist to insert a GIL aqcuisition between the C++
 // callback and the call into Python code.
-Platform::StreamPrototype& set_on_headers_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_headers_shim(Platform::StreamPrototype& self,
                                                Platform::OnHeadersCallback closure);
 
 Platform::StreamPrototype& set_on_data_shim(Platform::StreamPrototype& self,
                                             OnPyBytesDataCallback closure);
 
-Platform::StreamPrototype& set_on_trailers_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_trailers_shim(Platform::StreamPrototype& self,
                                                 Platform::OnTrailersCallback closure);
 
-Platform::StreamPrototype& set_on_error_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_error_shim(Platform::StreamPrototype& self,
                                              Platform::OnErrorCallback closure);
 
-Platform::StreamPrototype& set_on_complete_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_complete_shim(Platform::StreamPrototype& self,
                                                 Platform::OnCompleteCallback closure);
 
-Platform::StreamPrototype& set_on_cancel_shim(Platform::StreamPrototype self,
+Platform::StreamPrototype& set_on_cancel_shim(Platform::StreamPrototype& self,
                                               Platform::OnCancelCallback closure);
 
 } // namespace StreamPrototype
