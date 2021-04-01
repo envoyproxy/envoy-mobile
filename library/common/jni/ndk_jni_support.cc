8bef14076943ec975e9387b7be8e438c05fcc046
Alan Chiu <achiu@lyft.com>
2021-04-01 14:36:28 -0700

    fix
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/common/jni/ndk_jni_support.cc
+++ library/common/jni/ndk_jni_support.cc
@@ -4,8 +4,8 @@
 
 // NOLINT(namespace-envoy)
 
-int jni_log(const char* tag, const char* fmt, ...) {
-  return __android_log_print(ANDROID_LOG_VERBOSE, tag, fmt, ...);
+int jni_log(const char* tag, const char* fmt, void** thr_args) {
+  return __android_log_print(ANDROID_LOG_VERBOSE, tag, fmt, thr_args);
 }
 
 jint java_vm_attach_current_thread(JavaVM* vm, JNIEnv** p_env, void* thr_args) {
