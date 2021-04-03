21608e6b91a81aba91aaea59752d8acf85ca971f
Alan Chiu <achiu@lyft.com>
2021-04-02 17:32:39 -0700

    candidate
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- test/AppTest.kt
+++ test/AppTest.kt
@@ -1,11 +1,14 @@
 package test
 
+import io.envoyproxy.envoymobile.EngineBuilder
+import io.envoyproxy.envoymobile.engine.EnvoyEngine
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class AppTest {
   @Test
   fun `smoke test jni call`() {
-    assertThat(App.f(4321)).isEqualTo(4321)
+//    assertThat(App.f(4321)).isEqualTo(4321)
+    EngineBuilder().build()
   }
 }
