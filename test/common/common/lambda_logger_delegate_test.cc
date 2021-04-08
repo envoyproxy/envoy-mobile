bb49b8afbbf771e71c7bc39fd70a130dc95c775b
Jose Nino <jnino@lyft.com>
2021-04-08 18:23:56 -0500

    fix test
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/common/common/lambda_logger_delegate_test.cc
+++ test/common/common/lambda_logger_delegate_test.cc
@@ -1,6 +1,7 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "library/common/common/lambda_logger_delegate.h"
+#include "library/common/data/utility.h"
 
 using testing::_;
 using testing::HasSubstr;
@@ -12,8 +13,12 @@ TEST(LambdaDelegate, LogCb) {
   std::string expected_msg = "Hello LambdaDelegate";
   std::string actual_msg;
 
-  LambdaDelegate delegate = LambdaDelegate(
-      [&actual_msg](absl::string_view msg) -> void { actual_msg = msg; }, Registry::getSink());
+  LambdaDelegate delegate = LambdaDelegate({[](envoy_data data, void* context) -> void {
+                                              auto* actual_msg = static_cast<std::string*>(context);
+                                              *actual_msg = Data::Utility::copyToString(data);
+                                            },
+                                            &actual_msg},
+                                           Registry::getSink());
 
   ENVOY_LOG_MISC(error, expected_msg);
   EXPECT_THAT(actual_msg, HasSubstr(expected_msg));
