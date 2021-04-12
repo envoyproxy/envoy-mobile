de096af4ec0156c44f2cf2288f55e063aff72381
Jingwei Hao <jingwei.hao@gmail.com>
2021-04-12 18:20:59 -0400

    renaming
    Signed-off-by: Jingwei Hao <jingwei.hao@gmail.com>

--- library/common/jni/jni_utility.h
+++ library/common/jni/jni_utility.h
@@ -40,6 +40,6 @@ envoy_headers to_native_headers(JNIEnv* env, jobjectArray headers);
 
 envoy_headers* to_native_headers_ptr(JNIEnv* env, jobjectArray headers);
 
-envoy_stats_tags to_native_tags(JNIEnv* env, jobjectArray headers);
+envoy_stats_tags to_native_tags(JNIEnv* env, jobjectArray tags);
 
-envoy_map to_envoy_data(JNIEnv* env, jobjectArray jobjArray);
+envoy_map to_native_map(JNIEnv* env, jobjectArray entries);
