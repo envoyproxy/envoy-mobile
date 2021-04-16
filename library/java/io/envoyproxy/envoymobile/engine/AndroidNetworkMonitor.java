d4460fa7f8494fcb5d050337ed944f09cc420803
Alan Chiu <achiu@lyft.com>
2021-04-16 11:38:47 -0700

    pr feedback
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/java/io/envoyproxy/envoymobile/engine/AndroidNetworkMonitor.java
+++ library/java/io/envoyproxy/envoymobile/engine/AndroidNetworkMonitor.java
@@ -24,7 +24,7 @@ import androidx.core.content.ContextCompat;
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class AndroidNetworkMonitor extends BroadcastReceiver {
   private static final String PERMISSION_DENIED_STATS_ELEMENT =
-      "android_access_network_state_denied";
+      "android_permissions.network_state_denied";
   private static final int ENVOY_NET_GENERIC = 0;
   private static final int ENVOY_NET_WWAN = 1;
   private static final int ENVOY_NET_WLAN = 2;
