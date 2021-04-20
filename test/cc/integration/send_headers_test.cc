9adcfb6161f732e10f2518e26e2f92ee4ed65fd4
Cerek Hillen <chillen@lyft.com>
2021-04-20 14:06:55 -0700

    fix python test + fix format
    Signed-off-by: Cerek Hillen <chillen@lyft.com>

--- test/cc/integration/send_headers_test.cc
+++ test/cc/integration/send_headers_test.cc
@@ -3,10 +3,10 @@
 #include "library/cc/engine.h"
 #include "library/cc/engine_builder.h"
 #include "library/cc/envoy_error.h"
+#include "library/cc/log_level.h"
 #include "library/cc/request_headers_builder.h"
 #include "library/cc/request_method.h"
 #include "library/cc/response_headers.h"
-#include "library/cc/log_level.h"
 
 namespace Envoy {
 namespace {
@@ -68,22 +68,25 @@ TEST(TestSendHeaders, CanSendHeaders) {
   Platform::StreamSharedPtr stream;
   auto stream_prototype = engine->stream_client()->new_stream_prototype();
 
-  stream_prototype->set_on_headers([&](Platform::ResponseHeadersSharedPtr headers, bool end_stream) {
-      status.status_code = headers->http_status();
-      status.end_stream = end_stream;
-  });
+  stream_prototype->set_on_headers(
+      [&](Platform::ResponseHeadersSharedPtr headers, bool end_stream) {
+        status.status_code = headers->http_status();
+        status.end_stream = end_stream;
+      });
   stream_prototype->set_on_complete([&]() { stream_complete.Notify(); });
   stream_prototype->set_on_error([&](Platform::EnvoyErrorSharedPtr envoy_error) {
-        (void)envoy_error;
-        stream_complete.Notify();
-      });
+    (void)envoy_error;
+    stream_complete.Notify();
+  });
   stream_prototype->set_on_cancel([&]() { stream_complete.Notify(); });
 
   stream = stream_prototype->start();
 
-  Platform::RequestHeadersBuilder request_headers_builder(Platform::RequestMethod::GET, "https", "example.com", "/");
+  Platform::RequestHeadersBuilder request_headers_builder(Platform::RequestMethod::GET, "https",
+                                                          "example.com", "/");
   auto request_headers = request_headers_builder.build();
-  auto request_headers_ptr = Platform::RequestHeadersSharedPtr(new Platform::RequestHeaders(request_headers));
+  auto request_headers_ptr =
+      Platform::RequestHeadersSharedPtr(new Platform::RequestHeaders(request_headers));
   stream->send_headers(request_headers_ptr, true);
   stream_complete.WaitForNotification();
 
