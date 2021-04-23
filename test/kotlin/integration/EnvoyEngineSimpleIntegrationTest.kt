5d80535e590bcce7de38f11f6bca503ab0e5da9c
Alan Chiu <achiu@lyft.com>
2021-04-23 10:23:41 -0700

    fixup
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- test/kotlin/integration/EnvoyEngineSimpleIntegrationTest.kt
+++ test/kotlin/integration/EnvoyEngineSimpleIntegrationTest.kt
@@ -8,18 +8,14 @@ import org.junit.Test
 class EnvoyEngineSimpleIntegrationTest {
 
   init {
-    System.out.println("~~~~~~~~~~~~~~")
-    System.out.println(System.getProperty("envoy_jni_library_name"))
-    System.out.println("~~~~~~~~~~~~~~")
-    println(System.getProperty("user.dir"))
-    System.out.println("~~~~~~~~~~~~~~")
     JniLibrary.loadTestLibrary()
   }
+
   @Test
   fun `ensure engine build and termination succeeds with no errors`() {
     val engine = EngineBuilder().build()
     Thread.sleep(5000)
     engine.terminate()
-    assertThat(true).isFalse()
+    assertThat(true).isTrue()
   }
 }
