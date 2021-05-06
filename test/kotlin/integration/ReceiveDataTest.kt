4f0fd68b491a7dbca58339832e5733b425a790e9
Alan Chiu <achiu@lyft.com>
2021-05-06 14:13:29 -0700

    fix
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- test/kotlin/integration/ReceiveDataTest.kt
+++ test/kotlin/integration/ReceiveDataTest.kt
@@ -1,11 +1,11 @@
 package test.kotlin.integration
 
 import io.envoyproxy.envoymobile.Custom
-import io.envoyproxy.envoymobile.engine.JniLibrary
 import io.envoyproxy.envoymobile.EngineBuilder
 import io.envoyproxy.envoymobile.RequestHeadersBuilder
 import io.envoyproxy.envoymobile.RequestMethod
 import io.envoyproxy.envoymobile.UpstreamHttpProtocol
+import io.envoyproxy.envoymobile.engine.JniLibrary
 import java.nio.ByteBuffer
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
