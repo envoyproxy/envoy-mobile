bf5252ad75558b8f1de8659d999850746663e919
Mike Schore <mike.schore@gmail.com>
2021-05-13 03:16:14 +0800

    updates for comments
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- test/objective-c/EnvoyBridgeUtilityTest.m
+++ test/objective-c/EnvoyBridgeUtilityTest.m
@@ -11,19 +11,11 @@ typedef NSDictionary<NSString *, NSString *> EnvoyTags;
 
 @implementation EnvoyBridgeUtilityTest
 
-- (void)setUp {
-  [super setUp];
-}
-
-- (void)tearDown {
-  [super tearDown];
-}
-
 - (void)testToNativeData {
   NSString *testString = @"abc";
   NSData *testData = [testString dataUsingEncoding:NSUTF8StringEncoding];
   envoy_data nativeData = toNativeData(testData);
-  XCTAssertTrue(memcmp(nativeData.bytes, testData.bytes, 3) == 0);
+  XCTAssertEqual(memcmp(nativeData.bytes, testData.bytes, 3), 0);
 }
 
 @end
