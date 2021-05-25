ad10e5204ec9273ab783dcbefbb841ca6c88e8e1
Alan Chiu <achiu@lyft.com>
2021-05-24 17:27:05 -0700

    kotlin/swift: include version file in source
    Signed-off-by: Alan Chiu <achiu@lyft.com>

---
+++ library/kotlin/io/envoyproxy/envoymobile/Version.kt
@@ -0,0 +1,4 @@
+package io.envoyproxy.envoymobile
+
+const val ENVOY_MOBILE_VERSION = "0.4.0.04282021"
+const val ENVOY_VERSION = "46b646cbfa87c274700174b142be4525c46bb0d0"
---
+++ library/swift/Version.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+let envoy_mobile_version = "0.4.0.04282021"
+let envoy_version = "46b646cbfa87c274700174b142be4525c46bb0d0"
