e01be02ba1ebc0518fdc694fdaaac9138611b782
Alan Chiu <achiu@lyft.com>
2021-05-24 17:32:35 -0700

    swift public
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- library/swift/Version.swift
+++ library/swift/Version.swift
@@ -1,4 +1,4 @@
 import Foundation
 
-let envoy_mobile_version = "0.4.0.04282021"
-let envoy_version = "46b646cbfa87c274700174b142be4525c46bb0d0"
+public let envoy_mobile_version = "0.4.0.04282021"
+public let envoy_version = "46b646cbfa87c274700174b142be4525c46bb0d0"
