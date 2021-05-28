6ab3e15ceca23381d033113bb20e1830e84cbc07
Snow Pettersen <snowp@lyft.com>
2021-05-28 13:36:37 -0400

    revert java test
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/java/integration/AndroidEnvoyEngineStartUpTest.java
+++ test/java/integration/AndroidEnvoyEngineStartUpTest.java
@@ -1,7 +1,6 @@
 package test.kotlin.integration;
 
 import android.content.Context;
-import java.io.IOException;
 import androidx.test.core.app.ApplicationProvider;
 import io.envoyproxy.envoymobile.AndroidEngineBuilder;
 import io.envoyproxy.envoymobile.Engine;
@@ -9,7 +8,6 @@ import io.envoyproxy.envoymobile.engine.AndroidJniLibrary;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import test.kotlin.integration.TestStatsdServer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,20 +20,9 @@ public class AndroidEnvoyEngineStartUpTest {
   private final Context appContext = ApplicationProvider.getApplicationContext();
 
   @Test
-  public void ensure_engine_starts_and_terminates() throws InterruptedException, IOException {
-    TestStatsdServer server = new TestStatsdServer();
-    server.runAsync(5555);
-
-    Engine engine = new AndroidEngineBuilder(appContext).addStatsdPort(5555).build();
-
-    // Sleep until the server boots.
-    // TODO(snowp): We can probably do better here, block on stat?
-    Thread.sleep(2);
-    engine.flushStats();
-
-    String packet = server.awaitNextPacket();
-
-    assertThat(packet).contains("envoy.pulse.android_permissions.network_state_denied:1|c");
+  public void ensure_engine_starts_and_terminates() throws InterruptedException {
+    Engine engine = new AndroidEngineBuilder(appContext).build();
+    Thread.sleep(1000);
     engine.terminate();
     assertThat(true).isTrue();
   }
