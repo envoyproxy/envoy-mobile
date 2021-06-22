6b4e697b4c43a041c02575c6c723265cbe0657ef
Charles Le Borgne <cleborgne@google.com>
2021-06-22 18:06:27 +0100

    More formatting fixes
    Signed-off-by: Charles Le Borgne <cleborgne@google.com>

--- test/java/org/chromium/net/testing/StrictModeContext.java
+++ test/java/org/chromium/net/testing/StrictModeContext.java
@@ -17,77 +17,73 @@ import java.io.Closeable;
  *
  */
 public final class StrictModeContext implements Closeable {
-    private final StrictMode.ThreadPolicy mThreadPolicy;
-    private final StrictMode.VmPolicy mVmPolicy;
+  private final StrictMode.ThreadPolicy mThreadPolicy;
+  private final StrictMode.VmPolicy mVmPolicy;
 
-    private StrictModeContext(StrictMode.ThreadPolicy threadPolicy, StrictMode.VmPolicy vmPolicy) {
-        mThreadPolicy = threadPolicy;
-        mVmPolicy = vmPolicy;
-    }
+  private StrictModeContext(StrictMode.ThreadPolicy threadPolicy, StrictMode.VmPolicy vmPolicy) {
+    mThreadPolicy = threadPolicy;
+    mVmPolicy = vmPolicy;
+  }
 
-    private StrictModeContext(StrictMode.ThreadPolicy threadPolicy) {
-        this(threadPolicy, null);
-    }
+  private StrictModeContext(StrictMode.ThreadPolicy threadPolicy) { this(threadPolicy, null); }
 
-    private StrictModeContext(StrictMode.VmPolicy vmPolicy) {
-        this(null, vmPolicy);
-    }
+  private StrictModeContext(StrictMode.VmPolicy vmPolicy) { this(null, vmPolicy); }
 
-    /**
-     * Convenience method for disabling all VM-level StrictMode checks with try-with-resources.
-     * Includes everything listed here:
-     *     https://developer.android.com/reference/android/os/StrictMode.VmPolicy.Builder.html
-     */
-    public static StrictModeContext allowAllVmPolicies() {
-        StrictMode.VmPolicy oldPolicy = StrictMode.getVmPolicy();
-        StrictMode.setVmPolicy(StrictMode.VmPolicy.LAX);
-        return new StrictModeContext(oldPolicy);
-    }
+  /**
+   * Convenience method for disabling all VM-level StrictMode checks with try-with-resources.
+   * Includes everything listed here:
+   *     https://developer.android.com/reference/android/os/StrictMode.VmPolicy.Builder.html
+   */
+  public static StrictModeContext allowAllVmPolicies() {
+    StrictMode.VmPolicy oldPolicy = StrictMode.getVmPolicy();
+    StrictMode.setVmPolicy(StrictMode.VmPolicy.LAX);
+    return new StrictModeContext(oldPolicy);
+  }
 
-    /**
-     * Convenience method for disabling all thread-level StrictMode checks with try-with-resources.
-     * Includes everything listed here:
-     *     https://developer.android.com/reference/android/os/StrictMode.ThreadPolicy.Builder.html
-     */
-    public static StrictModeContext allowAllThreadPolicies() {
-        StrictMode.ThreadPolicy oldPolicy = StrictMode.getThreadPolicy();
-        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.LAX);
-        return new StrictModeContext(oldPolicy);
-    }
+  /**
+   * Convenience method for disabling all thread-level StrictMode checks with try-with-resources.
+   * Includes everything listed here:
+   *     https://developer.android.com/reference/android/os/StrictMode.ThreadPolicy.Builder.html
+   */
+  public static StrictModeContext allowAllThreadPolicies() {
+    StrictMode.ThreadPolicy oldPolicy = StrictMode.getThreadPolicy();
+    StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.LAX);
+    return new StrictModeContext(oldPolicy);
+  }
 
-    /**
-     * Convenience method for disabling StrictMode for disk-writes with try-with-resources.
-     */
-    public static StrictModeContext allowDiskWrites() {
-        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
-        return new StrictModeContext(oldPolicy);
-    }
+  /**
+   * Convenience method for disabling StrictMode for disk-writes with try-with-resources.
+   */
+  public static StrictModeContext allowDiskWrites() {
+    StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
+    return new StrictModeContext(oldPolicy);
+  }
 
-    /**
-     * Convenience method for disabling StrictMode for disk-reads with try-with-resources.
-     */
-    public static StrictModeContext allowDiskReads() {
-        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
-        return new StrictModeContext(oldPolicy);
-    }
+  /**
+   * Convenience method for disabling StrictMode for disk-reads with try-with-resources.
+   */
+  public static StrictModeContext allowDiskReads() {
+    StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+    return new StrictModeContext(oldPolicy);
+  }
 
-    /**
-     * Convenience method for disabling StrictMode for slow calls with try-with-resources.
-     */
-    public static StrictModeContext allowSlowCalls() {
-        StrictMode.ThreadPolicy oldPolicy = StrictMode.getThreadPolicy();
-        StrictMode.setThreadPolicy(
-                new StrictMode.ThreadPolicy.Builder(oldPolicy).permitCustomSlowCalls().build());
-        return new StrictModeContext(oldPolicy);
-    }
+  /**
+   * Convenience method for disabling StrictMode for slow calls with try-with-resources.
+   */
+  public static StrictModeContext allowSlowCalls() {
+    StrictMode.ThreadPolicy oldPolicy = StrictMode.getThreadPolicy();
+    StrictMode.setThreadPolicy(
+        new StrictMode.ThreadPolicy.Builder(oldPolicy).permitCustomSlowCalls().build());
+    return new StrictModeContext(oldPolicy);
+  }
 
-    @Override
-    public void close() {
-        if (mThreadPolicy != null) {
-            StrictMode.setThreadPolicy(mThreadPolicy);
-        }
-        if (mVmPolicy != null) {
-            StrictMode.setVmPolicy(mVmPolicy);
-        }
+  @Override
+  public void close() {
+    if (mThreadPolicy != null) {
+      StrictMode.setThreadPolicy(mThreadPolicy);
     }
+    if (mVmPolicy != null) {
+      StrictMode.setVmPolicy(mVmPolicy);
+    }
+  }
 }
