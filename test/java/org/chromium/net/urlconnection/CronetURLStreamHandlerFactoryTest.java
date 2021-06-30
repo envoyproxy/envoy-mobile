c89df1db2d5b8d0ea60a68d1f818bf7f495a503e
Chidera Olibie <colibie@google.com>
2021-06-30 18:32:38 +0000

    added some more tests that work out of the box
    Signed-off-by: Chidera Olibie <colibie@google.com>

---
+++ test/java/org/chromium/net/urlconnection/CronetURLStreamHandlerFactoryTest.java
@@ -0,0 +1,41 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net.urlconnection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.net.testing.Feature;
+import org.chromium.net.testing.CronetTestRule;
+
+/**
+ * Test for CronetURLStreamHandlerFactory.
+ */
+@RunWith(AndroidJUnit4.class)
+@SuppressWarnings("deprecation")
+public class CronetURLStreamHandlerFactoryTest {
+    @Rule
+    public final CronetTestRule mTestRule = new CronetTestRule();
+
+    @Test
+    @SmallTest
+    @Feature({"Cronet"})
+    public void testRequireConfig() throws Exception {
+        mTestRule.startCronetTestFramework();
+        try {
+            new CronetURLStreamHandlerFactory(null);
+            fail();
+        } catch (NullPointerException e) {
+            assertEquals("CronetEngine is null.", e.getMessage());
+        }
+    }
+}
