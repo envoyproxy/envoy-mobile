026a2ef59b2c59ac56151042e95eb493ffd476ba
Jingwei Hao <jingwei.hao@gmail.com>
2021-08-05 08:18:45 -0800

    test
    Signed-off-by: Jingwei Hao <jingwei.hao@gmail.com>

--- test/kotlin/integration/SetEventTrackerTest.kt
+++ test/kotlin/integration/SetEventTrackerTest.kt
@@ -2,55 +2,16 @@ package test.kotlin.integration
 
 import io.envoyproxy.envoymobile.Custom
 import io.envoyproxy.envoymobile.EngineBuilder
+import io.envoyproxy.envoymobile.LogLevel
 import io.envoyproxy.envoymobile.RequestHeadersBuilder
 import io.envoyproxy.envoymobile.RequestMethod
+import io.envoyproxy.envoymobile.ResponseHeaders
+import io.envoyproxy.envoymobile.UpstreamHttpProtocol
 import io.envoyproxy.envoymobile.engine.JniLibrary
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-
-private const val apiListenerType =
-  "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
-private const val assertionFilterType = "type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion"
-private const val config =
-"""
-static_resources:
-  listeners:
-  - name: base_api_listener
-    address:
-      socket_address:
-        protocol: TCP
-        address: 0.0.0.0
-        port_value: 10000
-    api_listener:
-      api_listener:
-        "@type": $apiListenerType
-        stat_prefix: hcm
-        route_config:
-          name: api_router
-          virtual_hosts:
-            - name: api
-              domains:
-                - "*"
-              routes:
-                - match:
-                    prefix: "/"
-                  direct_response:
-                    status: 200
-        http_filters:
-          - name: envoy.filters.http.assertion
-            typed_config:
-              "@type": $assertionFilterType
-              match_config:
-                http_request_headers_match:
-                  headers:
-                    - name: ":authority"
-                      exact_match: example.com
-          - name: envoy.router
-            typed_config:
-              "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
-"""
 
 class SetEventTrackerTest {
 
@@ -60,18 +21,20 @@ class SetEventTrackerTest {
 
   @Test
   fun `set eventTracker`() {
-    val engine = EngineBuilder(Custom(config))
+    val countDownLatch = CountDownLatch(1)
+    val engine = EngineBuilder()
+      .setEventTracker { events ->
+        for (entry in events) {
+          assertThat(entry.key).isEqualTo("foo")
+          assertThat(entry.value).isEqualTo("bar")
+        }
+        countDownLatch.countDown()
+      }
       .addNativeFilter(
         "envoy.filters.http.test_event_tracker",
         "{\"@type\":\"type.googleapis.com/envoymobile.extensions.filters.http.test_event_tracker.TestEventTracker\",\"attributes\":{\"foo\":\"bar\"}}"
       )
-      .setEventTracker { it ->
-        for (entry in it) {
-          assertThat(entry.key).isEqualTo("foo")
-          assertThat(entry.value).isEqualTo("bar")
-        }
-      }
-      .setOnEngineRunning {}
+      .addLogLevel(LogLevel.TRACE)
       .build()
 
     val client = engine.streamClient()
@@ -89,13 +52,15 @@ class SetEventTrackerTest {
       .start()
       .sendHeaders(requestHeaders, true)
 
+    countDownLatch.await(30, TimeUnit.SECONDS)
     engine.terminate()
+    assertThat(countDownLatch.count).isEqualTo(0)
   }
 
   @Test
   fun `engine should continue to run if no eventTracker is set`() {
     val countDownLatch = CountDownLatch(1)
-    val client = EngineBuilder(Custom(config))
+    val client = EngineBuilder()
       .setOnEngineRunning {
         countDownLatch.countDown()
       }
