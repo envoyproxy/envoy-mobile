643344b93477ee0af3a2f6ed11ec73f14d5dc7db
Alan Chiu <achiu@lyft.com>
2021-08-16 16:10:10 -0700

    codespell
    Signed-off-by: Alan Chiu <achiu@lyft.com>

--- test/java/org/chromium/net/CronetUrlRequestTest.java
+++ test/java/org/chromium/net/CronetUrlRequestTest.java
@@ -54,7 +54,7 @@ import org.robolectric.RobolectricTestRunner;
 /**
  * Test functionality of CronetUrlRequest.
  */
-@Ignore("ignoreing due to https://github.com/envoyproxy/envoy-mobile/pull/1710")
+@Ignore("ignoring due to https://github.com/envoyproxy/envoy-mobile/pull/1710")
 @RunWith(RobolectricTestRunner.class)
 public class CronetUrlRequestTest {
 
