fbae4b714da6bcbf616aa6d06f0ca7bb94deebc2
Luis Fernando Pino Duque <luis@engflow.com>
2021-09-15 01:39:08 +0200

    Debug
    Signed-off-by: Luis Fernando Pino Duque <luis@engflow.com>

--- test/kotlin/integration/SetEventTrackerTest.kt
+++ test/kotlin/integration/SetEventTrackerTest.kt
@@ -16,43 +16,6 @@ class SetEventTrackerTest {
   }
 
   @Test
-  fun `set eventTracker`() {
-    val countDownLatch = CountDownLatch(1)
-    val engine = EngineBuilder()
-      .setEventTracker { events ->
-        for (entry in events) {
-          assertThat(entry.key).isEqualTo("foo")
-          assertThat(entry.value).isEqualTo("bar")
-        }
-        countDownLatch.countDown()
-      }
-      .addNativeFilter(
-        "envoy.filters.http.test_event_tracker",
-        "{\"@type\":\"type.googleapis.com/envoymobile.extensions.filters.http.test_event_tracker.TestEventTracker\",\"attributes\":{\"foo\":\"bar\"}}"
-      )
-      .build()
-
-    val client = engine.streamClient()
-
-    val requestHeaders = RequestHeadersBuilder(
-      method = RequestMethod.GET,
-      scheme = "https",
-      authority = "example.com",
-      path = "/test"
-    )
-      .build()
-
-    client
-      .newStreamPrototype()
-      .start()
-      .sendHeaders(requestHeaders, true)
-
-    countDownLatch.await(30, TimeUnit.SECONDS)
-    engine.terminate()
-    assertThat(countDownLatch.count).isEqualTo(0)
-  }
-
-  @Test
   fun `engine should continue to run if no eventTracker is set and event is emitted`() {
     val countDownLatch = CountDownLatch(1)
     val engine = EngineBuilder()
