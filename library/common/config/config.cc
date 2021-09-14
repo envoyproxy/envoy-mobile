e03517fa97501536fd9a4b12ead5e84974d9691e
Jose Nino <jnino@lyft.com>
2021-09-14 11:02:18 -0700

    updated config
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/common/config/config.cc
+++ library/common/config/config.cc
@@ -68,24 +68,6 @@ const std::string config_header = R"(
       address:
         socket_address: { address: *statsd_host, port_value: *statsd_port }
 
-!ignore protocol_defs: &base_protocol_options_defs
-    envoy.extensions.upstreams.http.v3.HttpProtocolOptions:
-      "@type": type.googleapis.com/envoy.extensions.upstreams.http.v3.HttpProtocolOptions
-      auto_config:
-        http2_protocol_options:
-          connection_keepalive:
-            connection_idle_interval: *h2_connection_keepalive_idle_interval
-            timeout: *h2_connection_keepalive_timeout
-        http_protocol_options:
-          header_key_format:
-            stateful_formatter:
-              name: preserve_case
-              typed_config:
-                "@type": type.googleapis.com/envoy.extensions.http.header_formatters.preserve_case.v3.PreserveCaseFormatterConfig
-      upstream_http_protocol_options:
-        auto_sni: true
-        auto_san_validation: true
-
 !ignore protocol_defs: &http1_protocol_options_defs
     envoy.extensions.upstreams.http.v3.HttpProtocolOptions:
       "@type": type.googleapis.com/envoy.extensions.upstreams.http.v3.HttpProtocolOptions
@@ -372,7 +354,23 @@ R"(
     upstream_connection_options: *upstream_opts
     circuit_breakers: *circuit_breakers_settings
     outlier_detection: *base_outlier_detection
-    typed_extension_protocol_options: *base_protocol_options_defs
+    typed_extension_protocol_options: &base_protocol_options_defs
+      envoy.extensions.upstreams.http.v3.HttpProtocolOptions:
+        "@type": type.googleapis.com/envoy.extensions.upstreams.http.v3.HttpProtocolOptions
+        auto_config:
+          http2_protocol_options:
+            connection_keepalive:
+              connection_idle_interval: *h2_connection_keepalive_idle_interval
+              timeout: *h2_connection_keepalive_timeout
+          http_protocol_options:
+            header_key_format:
+              stateful_formatter:
+                name: preserve_case
+                typed_config:
+                  "@type": type.googleapis.com/envoy.extensions.http.header_formatters.preserve_case.v3.PreserveCaseFormatterConfig
+        upstream_http_protocol_options:
+          auto_sni: true
+          auto_san_validation: true
   - name: base_wlan_h2
     http2_protocol_options: {}
     connect_timeout: *connect_timeout
