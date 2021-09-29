c27d9bf38a26c5fb1beec7def8734487cca95b1c
Mike Schore <mike.schore@gmail.com>
2021-09-29 08:54:26 +0800

    remove some logs that would be too noisy
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- library/common/network/configurator.cc
+++ library/common/network/configurator.cc
@@ -77,7 +77,6 @@ envoy_network_t Configurator::getPreferredNetwork() { return preferred_network_.
 bool Configurator::overrideInterface(envoy_network_t) { return false; }
 
 void Configurator::refreshDns(envoy_network_t network) {
-  ENVOY_LOG_EVENT(debug, "network_configuration_refresh_dns_attempt", std::to_string(network));
   // refreshDns is intended to be queued on Envoy's event loop, whereas preferred_network_ is
   // updated synchronously. In the event that multiple refreshes become queued on the event loop,
   // this avoids triggering a refresh for a non-current network.
@@ -87,11 +86,7 @@ void Configurator::refreshDns(envoy_network_t network) {
     ENVOY_LOG_EVENT(debug, "network_configuration_dns_flipflop", std::to_string(network));
     return;
   }
-  // TODO(goaway): track event here or are there existing signals we can use?
-  if (!dns_cache_manager_) {
-    ENVOY_LOG_EVENT(warn, "network_configuration_dcm_missing", std::to_string(network));
-    return;
-  }
+
   if (auto dns_cache = dns_cache_manager_->lookUpCacheByName(BaseDnsCache)) {
     ENVOY_LOG_EVENT(debug, "network_configuration_refresh_dns", std::to_string(network));
     dns_cache->forceRefreshHosts();
