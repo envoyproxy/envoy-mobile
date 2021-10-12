cb3e22b9ff7faad3aef5f1a72f07f388119abc4b
Chidera Olibie <colibie@google.com>
2021-10-12 18:38:30 +0000

    ci testing
    Signed-off-by: Chidera Olibie <colibie@google.com>

--- test/common/integration/quic_test_server.cc
+++ test/common/integration/quic_test_server.cc
@@ -83,7 +83,9 @@ common_tls_context:
 
 QuicTestServer::QuicTestServer()
     : api_(Api::createApiForTest(stats_store_, time_system_)),
-      version_(Network::Address::IpVersion::v4), port_(34210), upstream_config_(time_system_) {
+      version_(Network::Address::IpVersion::v4),
+      upstream_config_(time_system_),
+      port_(34210) {
   ON_CALL(factory_context_, api()).WillByDefault(testing::ReturnRef(*api_));
   ON_CALL(factory_context_, scope()).WillByDefault(testing::ReturnRef(stats_store_));
 }
