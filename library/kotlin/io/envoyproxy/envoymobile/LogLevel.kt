a23cec1b45188551941e194fa8f6870a17f86dc1
Jingwei Hao <jingwei.hao@gmail.com>
2021-10-14 13:51:24 -0700

    update
    Signed-off-by: Jingwei Hao <jingwei.hao@gmail.com>

--- library/kotlin/io/envoyproxy/envoymobile/LogLevel.kt
+++ library/kotlin/io/envoyproxy/envoymobile/LogLevel.kt
@@ -3,7 +3,7 @@ package io.envoyproxy.envoymobile
 /**
  * Available logging levels for an Envoy instance. Note some levels may be compiled out.
  */
-enum class LogLevel(internal val levelStr: String, val levelInt: Int) {
+enum class LogLevel(internal val level: String, val levelInt: Int) {
   TRACE("trace", 0),
   DEBUG("debug", 1),
   INFO("info", 2),
