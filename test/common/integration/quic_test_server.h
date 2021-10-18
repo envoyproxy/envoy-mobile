f1a9a7534f56e8612faddbd3976d72df391d3403
Chidera Olibie <colibie@google.com>
2021-10-18 19:39:10 +0000

    some more cleanup
    Signed-off-by: Chidera Olibie <colibie@google.com>

--- test/common/integration/quic_test_server.h
+++ test/common/integration/quic_test_server.h
@@ -1,17 +1,13 @@
 #pragma once
 
-#include "source/common/stats/isolated_store_impl.h"
 #include "source/extensions/transport_sockets/tls/ssl_socket.h"
 
 // test_runner setups
-#include "source/common/common/logger.h"
 #include "source/exe/process_wide.h"
 
-#include "absl/strings/str_format.h"
 #include "envoy/extensions/transport_sockets/quic/v3/quic_transport.pb.h"
 #include "test/mocks/server/transport_socket_factory_context.h"
 #include "test/integration/autonomous_upstream.h"
-#include "test/test_common/network_utility.h"
 
 namespace Envoy {
 class QuicTestServer {
