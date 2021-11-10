a6cf1b78503a9db75a29a361e51b065b88b131fb
Jose Nino <jnino@lyft.com>
2021-11-10 10:34:21 -0800

    accidental delete
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/kotlin/io/envoyproxy/envoymobile/EngineBuilder.kt
+++ library/kotlin/io/envoyproxy/envoymobile/EngineBuilder.kt
@@ -40,6 +40,7 @@ open class EngineBuilder(
   private var h2ConnectionKeepaliveTimeoutSeconds = 10
   // 10Mb default (10 * 1024 * 1024)
   private var h2StreamBufferLimitBytes = 10485760
+  private var statsFlushSeconds = 60
   private var streamIdleTimeoutSeconds = 15
   private var perTryIdleTimeoutSeconds = 15
   private var appVersion = "unspecified"
