232bb61d17899b5a22de7a6fc3412d8a24aa6233
Jose Nino <jnino@lyft.com>
2021-12-02 15:44:29 -0800

    iOS
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- library/objective-c/EnvoyConfiguration.m
+++ library/objective-c/EnvoyConfiguration.m
@@ -106,9 +106,6 @@
                                                          withString:customRoutes];
   templateYAML = [templateYAML stringByReplacingOccurrencesOfString:@"#{custom_filters}"
                                                          withString:customFilters];
-  // No template is currently needed for Apple-based DNS resolver.
-  templateYAML = [templateYAML stringByReplacingOccurrencesOfString:@"#{dns_resolver_config}"
-                                                         withString:@""];
 
   NSMutableString *definitions =
       [[NSMutableString alloc] initWithString:@"!ignore platform_defs:\n"];
@@ -123,6 +120,9 @@
   [definitions
       appendFormat:@"- &dns_query_timeout %lus\n", (unsigned long)self.dnsQueryTimeoutSeconds];
   [definitions appendFormat:@"- &dns_preresolve_hostnames %@\n", self.dnsPreresolveHostnames];
+  [definitions appendFormat:@"- &dns_resolver_name envoy.network.dns_resolver.apple\n"];
+  // No additional values are currently needed for Apple-based DNS resolver.
+  [definitions appendFormat:@"- &dns_resolver_config {}\n"];
   [definitions appendFormat:@"- &enable_interface_binding %@\n",
                             self.enableInterfaceBinding ? @"true" : @"false"];
   [definitions appendFormat:@"- &h2_connection_keepalive_idle_interval %.*fs\n", 3,
