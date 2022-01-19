e6d6b26bbd3ba7c471c2f193eac57a8c7f70cff0
Charles Le Borgne <cleborgne@google.com>
2022-01-19 18:06:53 +0000

    Add comments to JNI interface
    Signed-off-by: Charles Le Borgne <cleborgne@google.com>

--- test/common/integration/quic_test_server_interface.h
+++ test/common/integration/quic_test_server_interface.h
@@ -8,10 +8,21 @@
 extern "C" { // functions
 #endif
 
+/**
+ * Starts the server. Can only have one server active per JVM. This is blocking until the port can
+ * start accepting requests.
+ */
 void start_server();
 
+/**
+ * Shutdowns the server. Can be restarted later. This is blocking until the server has freed all
+ * resources.
+ */
 void shutdown_server();
 
+/**
+ * Returns the port that got attributed. Can only be called once the server has been started.
+ */
 int get_server_port();
 
 #ifdef __cplusplus
