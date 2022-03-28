02166d971f9d9bd55fcef049d8af9a67f002e875
Snow Pettersen <snowp@lyft.com>
2022-03-28 16:57:20 -0400

    format test
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/swift/integration/FlatBufferTest.swift
+++ test/swift/integration/FlatBufferTest.swift
@@ -3,9 +3,9 @@ import FlatBuffers
 import XCTest
 
 final class FlatBufferTest: XCTestCase {
-  func testCreateFlatBuffer() {
-	  // This test simply verifies that we can import both the generated types as well as upstream FlatBuffers.
-	  let _ = Test_SomeTypeT()
-	  let _ = FlatBufferBuilder()
-  }
+    func testCreateFlatBuffer() {
+        // This test simply verifies that we can import both the generated types as well as upstream FlatBuffers.
+        _ = Test_SomeTypeT()
+        _ = FlatBufferBuilder()
+    }
 }
