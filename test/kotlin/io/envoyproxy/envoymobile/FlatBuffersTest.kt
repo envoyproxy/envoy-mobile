ed9febff0891f83544231848e9fa98aa80462b24
Snow Pettersen <snowp@lyft.com>
2022-03-28 19:49:36 -0400

    kotlin format
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/kotlin/io/envoyproxy/envoymobile/FlatBuffersTest.kt
+++ test/kotlin/io/envoyproxy/envoymobile/FlatBuffersTest.kt
@@ -1,10 +1,8 @@
 package io.envoyproxy.envoymobile
 
+import Test.SomeType
 import com.google.flatbuffers.FlatBufferBuilder
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.mockito.Mockito.mock
-import Test.SomeType;
 
 class FlatBuffersTest {
 
@@ -12,7 +10,7 @@ class FlatBuffersTest {
   fun `flatbuffers construction`() {
     // This test doesn't really test any functionality, just demonstrates that we are able to utilize flatbuffers pending
     // real usage within the code base.
-    val s  = SomeType()
+    val s = SomeType()
     val f = FlatBufferBuilder()
   }
 }
