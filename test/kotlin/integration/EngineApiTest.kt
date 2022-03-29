43ae8ca7c5b62e86c80dda88e28a12f287c278ca
JP Simard <jp@jpsim.com>
2022-03-29 17:05:56 -0400

    Revert "Re-add disabled parts of `EngineApiTest.kt`"
    This reverts commit 6e6b81b4c80e755485e6db2b671c273a6e21520b.
    
    Signed-off-by: JP Simard <jp@jpsim.com>

--- test/kotlin/integration/EngineApiTest.kt
+++ test/kotlin/integration/EngineApiTest.kt
@@ -1,13 +1,10 @@
 package test.kotlin.integration
 
-import io.envoyproxy.envoymobile.Element
 import io.envoyproxy.envoymobile.Engine
 import io.envoyproxy.envoymobile.EngineBuilder
 import io.envoyproxy.envoymobile.LogLevel
 import io.envoyproxy.envoymobile.engine.JniLibrary
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Test
 
@@ -37,10 +34,9 @@ class EngineApiTest {
       }
       .build()
 
-    assertThat(countDownLatch.await(30, TimeUnit.SECONDS)).isTrue()
-
-    engine!!.pulseClient().counter(Element("foo"), Element("bar")).increment(1)
-
-    assertThat(engine?.dumpStats()).contains("pulse.foo.bar: 1")
+    // TODO(jpsim): Re-enable
+    // assertThat(countDownLatch.await(30, TimeUnit.SECONDS)).isTrue()
+    // engine!!.pulseClient().counter(Element("foo"), Element("bar")).increment(1)
+    // assertThat(engine?.dumpStats()).contains("pulse.foo.bar: 1")
   }
 }
