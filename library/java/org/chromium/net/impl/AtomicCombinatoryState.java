7caa9f291a9c7deaec9187cbac1d5fd0d6f8e048
Charles Le Borgne <cleborgne@google.com>
2022-04-24 16:10:44 +0100

    Nit
    Signed-off-by: Charles Le Borgne <cleborgne@google.com>

--- library/java/org/chromium/net/impl/AtomicCombinatoryState.java
+++ library/java/org/chromium/net/impl/AtomicCombinatoryState.java
@@ -3,8 +3,8 @@ package org.chromium.net.impl;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * "CAS" logic based class providing a mean to ensure that the last awaited "stateEvent" will be
- * identified as so. Typically a "stateEvent" is a single bit flip.
+ * "Compare And Swap" logic based class providing a mean to ensure that the last awaited
+ * "stateEvent" will be identified as so. Typically a "stateEvent" is a single bit flip.
  *
  * <p>This class is Thread Safe.
  */
@@ -22,7 +22,7 @@ final class AtomicCombinatoryState {
   }
 
   /**
-   * Returns true is the state reaches, for the first time, the final state. The provided stateEvent
+   * Returns true if the state reaches, for the first time, the final state. The provided stateEvent
    * is atmomically ORed with the current state - the outcome is saved as the new state.
    */
   boolean hasReachedFinalState(int stateEvent) {
