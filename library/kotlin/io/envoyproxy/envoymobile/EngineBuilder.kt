4a938b23dd59617269115214e172c10aafe8acc5
Rafał Augustyniak <Augustyniak@users.noreply.github.com>
2022-05-12 16:16:31 -0400

    Update library/kotlin/io/envoyproxy/envoymobile/EngineBuilder.kt
    Co-authored-by: JP Simard <jp@jpsim.com>
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- library/kotlin/io/envoyproxy/envoymobile/EngineBuilder.kt
+++ library/kotlin/io/envoyproxy/envoymobile/EngineBuilder.kt
@@ -256,7 +256,7 @@ open class EngineBuilder(
 
   /**
    * Add a rate at which to ping h2 connections on new stream creation if the connection has
-   * sat idle. Defaults to 1 millisecond which effectively enables h2 ping functionlity
+   * sat idle. Defaults to 1 millisecond which effectively enables h2 ping functionality
    * and results in a connection ping on every new stream creation.
    *
    * @param h2ConnectionKeepaliveIdleIntervalMilliseconds rate in milliseconds.
