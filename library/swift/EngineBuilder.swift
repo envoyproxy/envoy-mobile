ca4cf35d035bde7017c5da0a2b302c7119bb94b6
Rafal Augustyniak <raugustyniak@lyft.com>
2022-05-12 16:53:59 -0400

    lint fixes
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- library/swift/EngineBuilder.swift
+++ library/swift/EngineBuilder.swift
@@ -204,7 +204,7 @@ open class EngineBuilder: NSObject {
 
   /// Add a rate at which to ping h2 connections on new stream creation if the connection has
   /// sat idle. Defaults to 1 millisecond which effectively enables h2 ping functionality
-  /// and results in a connection ping on every new stream creation. Set it to 
+  /// and results in a connection ping on every new stream creation. Set it to
   /// 100000000 milliseconds to effectively disable the ping.
   ///
   /// - parameter h2ConnectionKeepaliveIdleIntervalMilliseconds: Rate in milliseconds.
