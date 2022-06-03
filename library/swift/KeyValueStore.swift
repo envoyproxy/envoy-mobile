931b84211a6d85fa80908a73647a411eaa82e245
Mike Schore <mike.schore@gmail.com>
2022-06-03 19:57:47 +0800

    include missing protocol file
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

---
+++ library/swift/KeyValueStore.swift
@@ -0,0 +1,32 @@
+@_implementationOnly import EnvoyEngine
+import Foundation
+
+/// `KeyValueStore` is an interface that may be implemented to provide access to an arbitrary
+/// key-value store implementation that may be made accessible to native Envoy Mobile code.
+
+
+public protocol KeyValueStore {
+  func readValue(forKey key: String) -> String?
+  func saveValue(_ value: String, toKey key: String)
+  func removeKey(_ key: String)
+}
+
+internal class KeyValueStoreImpl : EnvoyKeyValueStore {
+  internal let implementation: KeyValueStore
+
+  init(implementation: KeyValueStore) {
+    self.implementation = implementation
+  }
+
+  func readValue(forKey key: String) -> String? {
+    return implementation.readValue(forKey: key)
+  }
+
+  func saveValue(_ value: String, toKey key: String) {
+    implementation.saveValue(value, toKey: key)
+  }
+
+  func removeKey(_ key: String) {
+    implementation.removeKey(key)
+  }
+}
