2f6cd587f95a326e44129af16d0467b828555654
JP Simard <jp@jpsim.com>
2022-06-14 19:31:02 -0400

    api: return opaque engine handle in `onEngineRunning`
    To support interop between the Swift/Kotlin and C++ APIs.
    
    Signed-off-by: JP Simard <jp@jpsim.com>

--- library/objective-c/EnvoyEngineImpl.m
+++ library/objective-c/EnvoyEngineImpl.m
@@ -18,7 +18,7 @@ static void ios_on_engine_running(void *context) {
   @autoreleasepool {
     EnvoyEngineImpl *engineImpl = (__bridge EnvoyEngineImpl *)context;
     if (engineImpl.onEngineRunning) {
-      engineImpl.onEngineRunning();
+      engineImpl.onEngineRunning(engineImpl.handle);
     }
   }
 }
@@ -435,7 +435,11 @@ static void ios_track_event(envoy_map map, const void *context) {
   EnvoyNetworkMonitor *_networkMonitor;
 }
 
-- (instancetype)initWithRunningCallback:(nullable void (^)())onEngineRunning
+- (NSInteger)handle {
+  return (NSInteger)_engineHandle;
+}
+
+- (instancetype)initWithRunningCallback:(nullable void (^)(NSInteger))onEngineRunning
                                  logger:(nullable void (^)(NSString *))logger
                            eventTracker:(nullable void (^)(EnvoyEvent *))eventTracker
                   networkMonitoringMode:(int)networkMonitoringMode {
