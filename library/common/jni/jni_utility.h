aa79c8df69b04c92e3cd7c28908c985e74c71d62
Rafal Augustyniak <raugustyniak@lyft.com>
2022-08-19 17:03:19 +0200

    add mention of android
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- library/common/jni/jni_utility.h
+++ library/common/jni/jni_utility.h
@@ -22,6 +22,9 @@ void set_class_loader(jobject class_loader);
  * application's context and should be associated with project's code - Java classes
  * defined by the project. For finding classes of Java built in-types use
  * `env->FindClass(...)` method instead as it is lighter to use.
+ * 
+ * The method works on Android targets only as the `set_class_loader` method is not
+ * called by JVM-only targets.
  *
  * @param class_name, the name of the class to find (i.e. "org.chromium.net.AndroidNetworkLibrary").
  *
