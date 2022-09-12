03c7dc1f30fb56762cbd548dc268d38d06a4eba2
Rafal Augustyniak <raugustyniak@lyft.com>
2022-09-12 18:24:08 -0400

    clean up test
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- test/kotlin/integration/PerformHTTPRequestUsingProxy.kt
+++ test/kotlin/integration/PerformHTTPRequestUsingProxy.kt
@@ -1,44 +1,27 @@
 package test.kotlin.integration
 
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
+import android.content.Context
 import android.net.ConnectivityManager
 import android.net.ProxyInfo
-import org.robolectric.Shadows.shadowOf
-import org.mockito.Mock
-import org.mockito.Mockito
-// import org.mockito.Mockito.mock
-// import org.mockito.Mockito.never
-// import org.mockito.Mockito.spy
-// import org.mockito.Mockito.times
-// import org.mockito.Mockito.verify
-// import org.mockito.Mockito.when
 
 import io.envoyproxy.envoymobile.Custom
-import io.envoyproxy.envoymobile.LogLevel
 import io.envoyproxy.envoymobile.AndroidEngineBuilder
-import io.envoyproxy.envoymobile.EnvoyError
-import io.envoyproxy.envoymobile.FilterDataStatus
-import androidx.test.core.app.ApplicationProvider
-import io.envoyproxy.envoymobile.FilterHeadersStatus
-import io.envoyproxy.envoymobile.FilterTrailersStatus
-import io.envoyproxy.envoymobile.FinalStreamIntel
 import io.envoyproxy.envoymobile.RequestHeadersBuilder
 import io.envoyproxy.envoymobile.RequestMethod
-import io.envoyproxy.envoymobile.ResponseFilter
 import io.envoyproxy.envoymobile.ResponseHeaders
-import io.envoyproxy.envoymobile.ResponseTrailers
 import io.envoyproxy.envoymobile.StreamIntel
-import io.envoyproxy.envoymobile.UpstreamHttpProtocol
 import io.envoyproxy.envoymobile.engine.JniLibrary
-import java.nio.ByteBuffer
-import android.content.Context;
 
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
 
 private val remotePort = (10001..11000).random()
 private val config =
@@ -47,7 +30,7 @@ static_resources:
   listeners:
   - name: base_api_listener
     address:
-      socket_address: { protocol: TCP, address: 127.0.0.1, port_value: 10002 }
+      socket_address: { protocol: TCP, address: 127.0.0.1, port_value: 10000 }
     api_listener:
       api_listener:
         "@type": "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.EnvoyMobileHttpConnectionManager"
@@ -93,14 +76,28 @@ static_resources:
     address:
       socket_address:
         address: 127.0.0.1
-        port_value: 9999
+        port_value: $remotePort
     filter_chains:
       - filters:
-          - name: envoy.filters.network.tcp_proxy
-            typed_config:
-              "@type": type.googleapis.com/envoy.extensions.filters.network.tcp_proxy.v3.TcpProxy
-              stat_prefix: destination
-              cluster: cluster_0
+        - name: envoy.filters.network.http_connection_manager
+          typed_config:
+            "@type": type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager
+            stat_prefix: remote_hcm
+            route_config:
+              name: remote_route
+              virtual_hosts:
+              - name: remote_service
+                domains: ["*"]
+                routes:
+                - match: { prefix: "/" }
+                  direct_response: { status: 200, body: { inline_string: "not found" } }
+                  request_headers_to_remove:
+                  - x-forwarded-proto
+                  - x-envoy-mobile-cluster
+            http_filters:
+              - name: envoy.router
+                typed_config:
+                  "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
   clusters:
   - name: base
     connect_timeout: 10s
@@ -119,77 +116,55 @@ static_resources:
           name: envoy.transport_sockets.raw_buffer
           typed_config:
             "@type": type.googleapis.com/envoy.extensions.transport_sockets.raw_buffer.v3.RawBuffer
-  - name: cluster_0
-    connect_timeout: 30s
-    type: LOGICAL_DNS
-    dns_lookup_family: V4_ONLY
-    load_assignment:
-      cluster_name: cluster_0
-      endpoints:
-        - lb_endpoints:
-            - endpoint:
-                address:
-                  socket_address:
-                    address: example.com
-                    port_value: 80
 """
 
 @RunWith(RobolectricTestRunner::class)
-class SetProxyTest {
+class PerformHTTPRequestUsingProxy {
   init {
     JniLibrary.loadTestLibrary()
   }
 
-  private val startExpectation = CountDownLatch(1)
-  private val runExpectation = CountDownLatch(1)
+  private val onEngineRunningLatch = CountDownLatch(1)
+  private val onRespondeHeadersLatch = CountDownLatch(1)
 
   @Test
-  fun `connects through proxy to an HTTP domain`() {
-    val context: Context = ApplicationProvider.getApplicationContext()
-
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
+  fun `performs an HTTP request through a proxy`() {
     val mockContext = Mockito.mock(Context::class.java)
     Mockito.`when`(mockContext.getApplicationContext()).thenReturn(mockContext)
     val mockConnectivityManager = Mockito.mock(ConnectivityManager::class.java)
     Mockito.`when`(mockContext.getSystemService(Mockito.anyString())).thenReturn(mockConnectivityManager)
-    Mockito.`when`(mockConnectivityManager.getDefaultProxy()).thenReturn(ProxyInfo.buildDirectProxy("127.0.0.1", 9999))
+    Mockito.`when`(mockConnectivityManager.getDefaultProxy()).thenReturn(ProxyInfo.buildDirectProxy("127.0.0.1", remotePort))
 
     val builder = AndroidEngineBuilder(mockContext, Custom(config))
-    builder
-        .addLogLevel(LogLevel.TRACE)
+    val engine = builder
         .enableProxySupport(true)
-        .enableHappyEyeballs(true)
-        .setOnEngineRunning { startExpectation.countDown() }
+        .setOnEngineRunning { onEngineRunningLatch.countDown() }
+        .build()
 
-    val engine = builder.build()
-    val client = engine.streamClient()
-
-    startExpectation.await(10, TimeUnit.SECONDS)
-    assertThat(startExpectation.count).isEqualTo(0)
+    onEngineRunningLatch.await(10, TimeUnit.SECONDS)
+    assertThat(onEngineRunningLatch.count).isEqualTo(0)
 
     val requestHeaders = RequestHeadersBuilder(
       method = RequestMethod.GET,
       scheme = "http",
-      authority = "example.com",
+      authority = "foo.com",
       path = "/"
     )
       .build()
 
-
-    client.newStreamPrototype()
+    engine
+      .streamClient()
+      .newStreamPrototype()
       .setOnResponseHeaders { responseHeaders, _, _ ->
         val status = responseHeaders.httpStatus ?: 0L
         assertThat(status).isEqualTo(200)
-        runExpectation.countDown()
+        onRespondeHeadersLatch.countDown()
       }
       .start(Executors.newSingleThreadExecutor())
       .sendHeaders(requestHeaders, true)
 
-    runExpectation.await(10, TimeUnit.SECONDS)
-
+    onRespondeHeadersLatch.await(10, TimeUnit.SECONDS)
     engine.terminate()
-
-    assertThat(runExpectation.count).isEqualTo(0)
+    assertThat(onRespondeHeadersLatch.count).isEqualTo(0)
   }
 }
