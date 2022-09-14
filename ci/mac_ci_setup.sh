f20be8ac7c4c7159bdcd9b0e9109a1056e158c99
Rafal Augustyniak <raugustyniak@lyft.com>
2022-09-13 21:39:27 -0400

    one one more
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- ci/mac_ci_setup.sh
+++ ci/mac_ci_setup.sh
@@ -57,14 +57,14 @@ pip3 install slackclient
 if [[ "${1:-}" == "--android" ]]; then
   # Download and set up ndk 21 after GitHub update
   # https://github.com/actions/virtual-environments/issues/5595
-#   ANDROID_HOME=$ANDROID_SDK_ROOT
-#   SDKMANAGER="${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager"
-#   $SDKMANAGER --uninstall "ndk-bundle"
-#   echo "y" | $SDKMANAGER "ndk;21.4.7075529"
-  ANDROID_ROOT=/usr/local/lib/android
-  ANDROID_SDK_ROOT=${ANDROID_ROOT}/sdk
-  SDKMANAGER=${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager
+  ANDROID_HOME=$ANDROID_SDK_ROOT
+  SDKMANAGER="${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager"
+  $SDKMANAGER --uninstall "ndk-bundle"
   echo "y" | $SDKMANAGER "ndk;21.4.7075529"
+#   ANDROID_ROOT=/usr/local/lib/android
+#   ANDROID_SDK_ROOT=${ANDROID_ROOT}/sdk
+#   SDKMANAGER=${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager
+#   echo "y" | $SDKMANAGER "ndk;21.4.7075529"
   ln -sfn $ANDROID_SDK_ROOT/ndk/21.4.7075529 "${ANDROID_SDK_ROOT}/ndk-bundle"
 
   # Download and set up build-tools 30.0.3, 31.0.0 is missing dx.jar.
