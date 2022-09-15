2b63cabe6e798ccbc686c1a3928bc4b9eab53cf8
Rafal Augustyniak <raugustyniak@lyft.com>
2022-09-15 00:10:04 -0400

    trying to fix
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -8,7 +8,6 @@ sdk_install_target="/github/home/.android"
 
 mkdir "$sdk_install_target"
 pushd "$sdk_install_target"
-
 if [ ! -d ./sdk/cmdline-tools/latest ]; then
 	mkdir -p sdk/
 	cmdline_file="commandlinetools-linux-7583922_latest.zip"
@@ -17,18 +16,17 @@ if [ ! -d ./sdk/cmdline-tools/latest ]; then
 	mkdir -p sdk/cmdline-tools/latest
 	mv cmdline-tools/* sdk/cmdline-tools/latest
 fi
+popd
 
-export ANDROID_HOME="$(realpath "$sdk_install_target/sdk")"
+export ANDROID_HOME="$sdk_install_target/sdk"
 export ANDROID_SDK_ROOT=$ANDROID_HOME
 
 SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
-$SDKMANAGER --uninstall "ndk-bundle"
 
+$SDKMANAGER --uninstall "ndk-bundle"
 echo "y" | $SDKMANAGER "ndk;21.4.7075529"
 $SDKMANAGER --install "platforms;android-30"
 ln -sfn $ANDROID_SDK_ROOT/ndk/21.4.7075529 "${ANDROID_SDK_ROOT}/ndk-bundle"
-
-# Download and set up build-tools 30.0.3, 31.0.0 is missing dx.jar.
 $SDKMANAGER --install "build-tools;30.0.2"
 
 ANDROID_NDK_HOME=$ANDROID_HOME/ndk/21.4.7075529
