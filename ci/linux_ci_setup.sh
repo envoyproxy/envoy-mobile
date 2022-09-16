2852ca7069ea559a43ea2fbb80e6f11d8af03587
Rafal Augustyniak <raugustyniak@lyft.com>
2022-09-16 10:03:00 -0400

    simplify exports
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -15,21 +15,17 @@ if [ ! -d ./sdk/cmdline-tools/latest ]; then
 	mv cmdline-tools/* sdk/cmdline-tools/latest
 fi
 
-export ANDROID_HOME="$(realpath "$sdk_install_target/sdk")"
-export ANDROID_SDK_ROOT=$ANDROID_HOME
-
+ANDROID_HOME="$(realpath "$sdk_install_target/sdk")"
 SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
 
 echo "y" | $SDKMANAGER --install "ndk;21.4.7075529"
 $SDKMANAGER --install "platforms;android-30"
 $SDKMANAGER --install "build-tools;30.0.2"
 
-export PATH=$ANDROID_HOME/cmdline-tools/latest/bin/:$ANDROID_SDK_ROOT/tools/:$ANDROID_SDK_ROOT/platform-tools/:$PATH
-
-export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/21.4.7075529
+echo "ANDROID_HOME=$ANDROID_HOME" >> $GITHUB_ENV
+echo "ANDROID_SDK_ROOT=$ANDROID_HOME" >> $GITHUB_ENV
 echo "ANDROID_NDK_HOME=$ANDROID_HOME/ndk/21.4.7075529" >> $GITHUB_ENV
 
-echo "PATH=$PATH"
 echo "export ANDROID_SDK_ROOT=$ANDROID_HOME"
 echo "export ANDROID_HOME=$ANDROID_HOME"
 echo "export ANDROID_NDK_HOME=$ANDROID_NDK_HOME"
