90d5a212d4a175f8a25bc7263a670e136ba7db89
Rafal Augustyniak <raugustyniak@lyft.com>
2022-09-16 10:15:14 -0400

    code review: clean up
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- ci/linux_ci_setup.sh
+++ ci/linux_ci_setup.sh
@@ -5,6 +5,8 @@ set -e
 ./bazelw version
 
 sdk_install_target="/github/home/.android"
+
+mkdir "$sdk_install_target"
 pushd "$sdk_install_target"
 if [ ! -d ./sdk/cmdline-tools/latest ]; then
 	mkdir -p sdk/
@@ -15,22 +17,13 @@ if [ ! -d ./sdk/cmdline-tools/latest ]; then
 	mv cmdline-tools/* sdk/cmdline-tools/latest
 fi
 
-export ANDROID_HOME="$(realpath "$sdk_install_target/sdk")"
-export ANDROID_SDK_ROOT=$ANDROID_HOME
-
+ANDROID_HOME="$(realpath "$sdk_install_target/sdk")"
 SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
 
 echo "y" | $SDKMANAGER --install "ndk;21.4.7075529"
 $SDKMANAGER --install "platforms;android-30"
 $SDKMANAGER --install "build-tools;30.0.2"
 
-ln -sfn $ANDROID_SDK_ROOT/ndk/21.4.7075529 "${ANDROID_SDK_ROOT}/ndk-bundle"
-export PATH=$ANDROID_HOME/cmdline-tools/latest/bin/:$ANDROID_SDK_ROOT/tools/:$ANDROID_SDK_ROOT/platform-tools/:$PATH
-
-export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/21.4.7075529
+echo "ANDROID_HOME=$ANDROID_HOME" >> $GITHUB_ENV
+echo "ANDROID_SDK_ROOT=$ANDROID_HOME" >> $GITHUB_ENV
 echo "ANDROID_NDK_HOME=$ANDROID_HOME/ndk/21.4.7075529" >> $GITHUB_ENV
-
-echo "PATH=$PATH"
-echo "export ANDROID_SDK_ROOT=$ANDROID_HOME"
-echo "export ANDROID_HOME=$ANDROID_HOME"
-echo "export ANDROID_NDK_HOME=$ANDROID_NDK_HOME"
