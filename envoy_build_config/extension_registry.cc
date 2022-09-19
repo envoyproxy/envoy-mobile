c59c0025daa0260e94eaa9b29424a911eb0ca9f0
Rafal Augustyniak <raugustyniak@lyft.com>
2022-09-19 12:01:39 -0400

    fix method name v3
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- envoy_build_config/extension_registry.cc
+++ envoy_build_config/extension_registry.cc
@@ -60,7 +60,7 @@ void ExtensionRegistry::registerFactories() {
   Envoy::Extensions::TransportSockets::RawBuffer::forceRegisterUpstreamRawBufferSocketFactory();
   Envoy::Extensions::TransportSockets::Tls::forceRegisterUpstreamSslSocketFactory();
   Envoy::Extensions::TransportSockets::Http11Connect::
-      forceRegisterUpstreamHttp11ConnectSocketFactory();
+    forceRegisterUpstreamHttp11ConnectSocketConfigFactory();
   Envoy::Extensions::TransportSockets::Tls::forceRegisterDefaultCertValidatorFactory();
   Envoy::Extensions::Upstreams::Http::Generic::forceRegisterGenericGenericConnPoolFactory();
   Envoy::Upstream::forceRegisterLogicalDnsClusterFactory();
