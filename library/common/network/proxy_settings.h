6a7d454a0ce61be973fa685fa56f274914b4777f
Alyssa Wilk <alyssar@chromium.org>
2022-09-20 09:02:44 -0400

    WIP
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- library/common/network/proxy_settings.h
+++ library/common/network/proxy_settings.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "source/common/network/utility.h"
 
 namespace Envoy {
@@ -22,48 +20,18 @@ struct ProxySettings {
    * @param port The proxy port.
    */
   ProxySettings(const std::string& host, const uint16_t port)
-      : address_(Envoy::Network::Utility::parseInternetAddressNoThrow(host, port)) {}
-
-  /**
-   * @brief Parses given host and domain and creates proxy settings. Returns nullptr
-   *        for an empty host and a port equal to 0 as they are passed to c++ native layer
-   *        as a synonym of the lack of proxy settings configured on a device.
-   *
-   * @param host The proxy host defined as a hostname or an IP address. Some platforms
-   *             (i.e., Android) allow users to specify proxy using either one of these.
-   * @param port The proxy port.
-   * @return The created proxy settings, nullptr if the passed host is an empty string and
-   *         port is equal to 0.
-   */
-  static const ProxySettingsConstSharedPtr parseHostAndPort(const std::string& host,
-                                                            const uint16_t port) {
-    if (host == "" && port == 0) {
-      return nullptr;
-    }
-    return std::make_shared<ProxySettings>(host, port);
-  }
+      : address_(Envoy::Network::Utility::parseInternetAddressNoThrow(host, port)),
+        hostname_(host) {}
 
   /**
    * @brief Returns an address of a proxy. This method returns nullptr for proxy settings
-   *        that are initialized with anything other than an IP address.
+   *        that are initialized with a host represted using a hostname.
    *
    * @return Address of a proxy or nullptr if proxy address is incorrect or host is
    *         defined using a hostname and not an IP address.
    */
   const Envoy::Network::Address::InstanceConstSharedPtr& address() const { return address_; }
-
-  /**
-   * @brief Returns a human readable representation of the proxy settings represented
-   *        by the receiver
-   *
-   * @return const A human readable representation of the receiver.
-   */
-  const std::string asString() const {
-    if (address_ != nullptr) {
-      return address_->asString();
-    }
-    return "no_proxy_configured";
-  }
+  const std::string& hostname() const { return hostname_; }
 
   bool operator==(ProxySettings const& rhs) const {
     if (this->address() == nullptr || rhs.address() == nullptr) {
@@ -77,6 +45,7 @@ struct ProxySettings {
 
 private:
   Envoy::Network::Address::InstanceConstSharedPtr address_;
+  std::string hostname_;
 };
 
 } // namespace Network
