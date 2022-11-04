b883c10dfd7de7902d7feb52ef795e35ec38b4f3
Ryan Hamilton <rch@google.com>
2022-11-04 12:42:30 -0700

    iOS => Apple
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- library/common/network/apple_platform_cert_verifier.h
+++ library/common/network/apple_platform_cert_verifier.h
@@ -7,7 +7,7 @@ extern "C" {
 #endif
 
 /**
- * Registers the iOS platform cert verifier API.
+ * Registers the Apple platform cert verifier API.
  */
 void register_apple_platform_cert_verifier();
 
