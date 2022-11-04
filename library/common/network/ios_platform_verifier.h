5c79ed9a5254a058bc4ba8686e7238c9c355faf9
Ryan Hamilton <rch@google.com>
2022-11-04 09:37:44 -0700

    Break out ios_platform_verifier
    Signed-off-by: Ryan Hamilton <rch@google.com>

---
+++ library/common/network/ios_platform_verifier.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Registers the iOS platform cert verifier API.
+ */
+void register_ios_platform_verifier();
+
+#ifdef __cplusplus
+}
+#endif
