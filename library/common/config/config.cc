be90d240bbaa7cb928315cbfc150f1f5ab6ecf84
Renjie Tang <renjietang@chromium.org>
2022-11-10 14:08:29 -0800

    fix config
    Signed-off-by: Renjie Tang <renjietang@chromium.org>

--- library/common/config/config.cc
+++ library/common/config/config.cc
@@ -86,7 +86,8 @@ const char* platform_cert_validation_context_template = R"(
 - &validation_context
   custom_validator_config:
     name: "envoy_mobile.cert_validator.platform_bridge_cert_validator"
-    "@type": type.googleapis.com/envoy_mobile.extensions.cert_validator.platform_bridge.PlatformBridgeCertValidator
+    typed_config:
+      "@type": type.googleapis.com/envoy_mobile.extensions.cert_validator.platform_bridge.PlatformBridgeCertValidator
   trust_chain_verification: *trust_chain_verification
 )";
 
