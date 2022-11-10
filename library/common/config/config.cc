35cb2ea0418328f971384ab7d15058456886b8a9
Renjie Tang <renjietang@chromium.org>
2022-11-10 13:44:30 -0800

    add proto definition to PlatformBridgeCertValidator
    Signed-off-by: Renjie Tang <renjietang@chromium.org>

--- library/common/config/config.cc
+++ library/common/config/config.cc
@@ -86,6 +86,7 @@ const char* platform_cert_validation_context_template = R"(
 - &validation_context
   custom_validator_config:
     name: "envoy_mobile.cert_validator.platform_bridge_cert_validator"
+    "@type": type.googleapis.com/envoy_mobile.extensions.cert_validator.platform_bridge.PlatformBridgeCertValidator
   trust_chain_verification: *trust_chain_verification
 )";
 
